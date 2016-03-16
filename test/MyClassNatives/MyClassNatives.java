71b14cd473998263a6e3ec6835cf8a7116aac315
Pavle Batuta <pavle.batuta@imgtec.com>
2016-03-16 11:31:46 +0100

    [MIPS64] JNI Compiler: compiler independent test
    Fixed MIPS64 stackArgsSignExtendedTest that did not work properly
    when code was built with clang. The the test is now written in an
    asm stub and should be completely compiler indepedent.
    
    Test: mma test-art-target-gtest-jni_compiler_test
    
    Change-Id: Idefef3a8414dd8b88a28d2d8862a724bc72402a7

--- test/MyClassNatives/MyClassNatives.java
+++ test/MyClassNatives/MyClassNatives.java
@@ -139,8 +139,8 @@ class MyClassNatives {
         float f9, int i10, float f10);
 
     // Normal native
-    native static void stackArgsSignExtendedMips64(int i1, int i2, int i3, int i4, int i5, int i6,
-        int i7, int i8);
+    native static long getStackArgSignExtendedMips64(int i1, int i2, int i3, int i4, int i5, int i6,
+        int stack_arg);
 
     // Normal native
     static native double logD(double d);
@@ -273,8 +273,8 @@ class MyClassNatives {
         float f9, int i10, float f10);
 
     @FastNative
-    native static void stackArgsSignExtendedMips64_Fast(int i1, int i2, int i3, int i4, int i5, int i6,
-        int i7, int i8);
+    native static long getStackArgSignExtendedMips64_Fast(int i1, int i2, int i3, int i4, int i5, int i6,
+        int stack_arg);
 
     @FastNative
     static native double logD_Fast(double d);
@@ -316,8 +316,8 @@ class MyClassNatives {
         float f9, int i10, float f10);
 
     @CriticalNative
-    native static void stackArgsSignExtendedMips64_Critical(int i1, int i2, int i3, int i4, int i5, int i6,
-        int i7, int i8);
+    native static long getStackArgSignExtendedMips64_Critical(int i1, int i2, int i3, int i4, int i5, int i6,
+        int stack_arg);
 
     @CriticalNative
     static native double logD_Critical(double d);
