bd11690caaa32575e77493a6c0da15bdc4ba35a8
Mingyao Yang <mingyao@google.com>
2016-08-02 11:02:54 -0700

    Class Hierarchy Analysis (CHA)
    The class linker now tracks whether a method has a single implementation
    and if so, the JIT compiler will try to devirtualize a virtual call for
    the method into a direct call. If the single-implementation assumption
    is violated due to additional class linking, compiled code that makes the
    assumption is invalidated. Deoptimization is triggered for compiled code
    live on stack. Instead of patching return pc's on stack, a CHA guard is
    added which checks a hidden should_deoptimize flag for deoptimization.
    This approach limits the number of deoptimization points.
    
    This CL does not devirtualize abstract/interface method invocation.
    
    Slides on CHA:
    https://docs.google.com/a/google.com/presentation/d/1Ax6cabP1vM44aLOaJU3B26n5fTE9w5YU-1CRevIDsBc/edit?usp=sharing
    
    Change-Id: I18bf716a601b6413b46312e925a6ad9e4008efa4
    Test: ART_TEST_JIT=true m test-art-host/target-run-test test-art-host-gtest

--- compiler/optimizing/code_generator_arm64.cc
+++ compiler/optimizing/code_generator_arm64.cc
@@ -1279,6 +1279,12 @@ void CodeGeneratorARM64::GenerateFrameEntry() {
         frame_size - GetCoreSpillSize());
     GetAssembler()->SpillRegisters(GetFramePreservedFPRegisters(),
         frame_size - FrameEntrySpillSize());
+
+    if (GetGraph()->HasShouldDeoptimizeFlag()) {
+      // Initialize should_deoptimize flag to 0.
+      Register zero_reg = Register(VIXLRegCodeFromART(WZR), kWRegSize);
+      __ Str(zero_reg, MemOperand(sp, GetStackOffsetOfShouldDeoptimizeFlag()));
+    }
   }
 }
 
@@ -3239,6 +3245,17 @@ void InstructionCodeGeneratorARM64::VisitDeoptimize(HDeoptimize* deoptimize) {
                         /* condition_input_index */ 0,
                         slow_path->GetEntryLabel(),
                         /* false_target */ nullptr);
+}
+
+void LocationsBuilderARM64::VisitShouldDeoptimizeFlag(HShouldDeoptimizeFlag* flag) {
+  LocationSummary* locations = new (GetGraph()->GetArena())
+      LocationSummary(flag, LocationSummary::kNoCall);
+  locations->SetOut(Location::RequiresRegister());
+}
+
+void InstructionCodeGeneratorARM64::VisitShouldDeoptimizeFlag(HShouldDeoptimizeFlag* flag) {
+  __ Ldr(OutputRegister(flag),
+         MemOperand(sp, codegen_->GetStackOffsetOfShouldDeoptimizeFlag()));
 }
 
 static inline bool IsConditionOnFloatingPointValues(HInstruction* condition) {
