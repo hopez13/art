538044d113da2b29fb080ea51c9571a8c0b116cd
Narayan Kamath <narayan@google.com>
2016-08-03 12:46:58 +0100

    interpreter: support for non-exact invokes.
    Performs primitive argument conversions as well as boxing and unboxing
    operations. Support for return value conversions will be added in a
    follow up change.
    
    Test: make test-art-host
    
    Change-Id: I2e3348ff64a5826e477f87c12a7d5c390eb3a653

--- runtime/interpreter/interpreter_common.cc
+++ runtime/interpreter/interpreter_common.cc
@@ -24,6 +24,7 @@
 #include "jit/jit.h"
 #include "jvalue.h"
 #include "method_handles.h"
+#include "method_handles-inl.h"
 #include "mirror/array-inl.h"
 #include "mirror/class.h"
 #include "mirror/method_handle_impl.h"
@@ -474,24 +475,6 @@ void UnexpectedOpcode(const Instruction* inst, const ShadowFrame& shadow_frame) 
   UNREACHABLE();
 }
 
-// Assign register 'src_reg' from shadow_frame to register 'dest_reg' into new_shadow_frame.
-static inline void AssignRegister(ShadowFrame* new_shadow_frame, const ShadowFrame& shadow_frame,
-                                  size_t dest_reg, size_t src_reg)
-    REQUIRES_SHARED(Locks::mutator_lock_) {
-  // Uint required, so that sign extension does not make this wrong on 64b systems
-  uint32_t src_value = shadow_frame.GetVReg(src_reg);
-  mirror::Object* o = shadow_frame.GetVRegReference<kVerifyNone>(src_reg);
-
-  // If both register locations contains the same value, the register probably holds a reference.
-  // Note: As an optimization, non-moving collectors leave a stale reference value
-  // in the references array even after the original vreg was overwritten to a non-reference.
-  if (src_value == reinterpret_cast<uintptr_t>(o)) {
-    new_shadow_frame->SetVRegReference(dest_reg, o);
-  } else {
-    new_shadow_frame->SetVReg(dest_reg, src_value);
-  }
-}
-
 void AbortTransactionF(Thread* self, const char* fmt, ...) {
   va_list args;
   va_start(args, fmt);
@@ -518,6 +501,17 @@ static inline bool DoCallCommon(ArtMethod* called_method,
                                 uint16_t number_of_inputs,
                                 uint32_t (&arg)[Instruction::kMaxVarArgRegs],
                                 uint32_t vregC) ALWAYS_INLINE;
+
+// Separate declaration is required solely for the attributes.
+template <bool is_range> REQUIRES_SHARED(Locks::mutator_lock_)
+static inline bool DoCallPolymorphic(ArtMethod* called_method,
+                                     mirror::MethodType* callsite_type,
+                                     mirror::MethodType* target_type,
+                                     Thread* self,
+                                     ShadowFrame& shadow_frame,
+                                     JValue* result,
+                                     uint32_t (&arg)[Instruction::kMaxVarArgRegs],
+                                     uint32_t vregC) ALWAYS_INLINE;
 
 void ArtInterpreterToCompiledCodeBridge(Thread* self,
                                         ArtMethod* caller,
@@ -687,22 +681,119 @@ inline bool DoInvokePolymorphic(Thread* self, ShadowFrame& shadow_frame,
     // NOTE: handle_kind == kInvokeStatic needs no special treatment here. We
     // can directly make the call. handle_kind == kInvokeSuper doesn't have any
     // particular use and can probably be dropped.
+    self->EndAssertNoThreadSuspension(old_cause);
+
     if (callsite_type->IsExactMatch(handle_type)) {
-      self->EndAssertNoThreadSuspension(old_cause);
       return DoCallCommon<is_range, do_access_check>(
           called_method, self, shadow_frame, result, number_of_inputs,
           arg, receiver_vregC);
+    } else {
+      return DoCallPolymorphic<is_range>(
+          called_method, callsite_type, handle_type, self, shadow_frame,
+          result, arg, receiver_vregC);
     }
-
-    self->EndAssertNoThreadSuspension(old_cause);
-    UNIMPLEMENTED(FATAL) << "Non exact invokes are not implemented yet.";
-    return false;
   } else {
     // TODO(narayan): Implement field getters and setters.
     self->EndAssertNoThreadSuspension(old_cause);
     UNIMPLEMENTED(FATAL) << "Field references in method handles are not implemented yet.";
     return false;
   }
+}
+
+// Calculate the number of ins for a proxy or native method, where we
+// can't just look at the code item.
+static inline size_t GetInsForProxyOrNativeMethod(ArtMethod* method)
+    REQUIRES_SHARED(Locks::mutator_lock_) {
+  DCHECK(method->IsNative() || method->IsProxyMethod());
+
+  method = method->GetInterfaceMethodIfProxy(kRuntimePointerSize);
+  size_t num_ins = 0;
+  // Separate accounting for the receiver, which isn't a part of the
+  // shorty.
+  if (!method->IsStatic()) {
+    ++num_ins;
+  }
+
+  uint32_t shorty_len = 0;
+  const char* shorty = method->GetShorty(&shorty_len);
+  for (size_t i = 1; i < shorty_len; ++i) {
+    const char c = shorty[i];
+    ++num_ins;
+    if (c == 'J' || c == 'D') {
+      ++num_ins;
+    }
+  }
+
+  return num_ins;
+}
+
+template <bool is_range>
+static inline bool DoCallPolymorphic(ArtMethod* called_method,
+                                     mirror::MethodType* callsite_type,
+                                     mirror::MethodType* target_type,
+                                     Thread* self,
+                                     ShadowFrame& shadow_frame,
+                                     JValue* result,
+                                     uint32_t (&arg)[Instruction::kMaxVarArgRegs],
+                                     uint32_t vregC) {
+  // TODO(narayan): Wire in the String.init hacks.
+
+  // Compute method information.
+  const DexFile::CodeItem* code_item = called_method->GetCodeItem();
+
+  // Number of registers for the callee's call frame. Note that for non-exact
+  // invokes, we always derive this information from the callee method. We
+  // cannot guarantee during verification that the number of registers encoded
+  // in the invoke is equal to the number of ins for the callee. This is because
+  // some transformations (such as boxing a long -> Long or wideining an
+  // int -> long will change that number.
+  uint16_t num_regs;
+  size_t first_dest_reg;
+  if (LIKELY(code_item != nullptr)) {
+    num_regs = code_item->registers_size_;
+    first_dest_reg = num_regs - code_item->ins_size_;
+    // Parameter registers go at the end of the shadow frame.
+    DCHECK_NE(first_dest_reg, (size_t)-1);
+  } else {
+    // No local regs for proxy and native methods.
+    DCHECK(called_method->IsNative() || called_method->IsProxyMethod());
+    num_regs = GetInsForProxyOrNativeMethod(called_method);
+    first_dest_reg = 0;
+  }
+
+  // Allocate shadow frame on the stack.
+  const char* old_cause = self->StartAssertNoThreadSuspension("DoCallPolymorphic");
+  ShadowFrameAllocaUniquePtr shadow_frame_unique_ptr =
+      CREATE_SHADOW_FRAME(num_regs, &shadow_frame, called_method, /* dex pc */ 0);
+  ShadowFrame* new_shadow_frame = shadow_frame_unique_ptr.get();
+  self->EndAssertNoThreadSuspension(old_cause);
+
+  if (!PerformArgumentConversions<is_range>(self, callsite_type, target_type,
+                                            shadow_frame, vregC, first_dest_reg,
+                                            arg, new_shadow_frame, result)) {
+    DCHECK(self->IsExceptionPending());
+    result->SetL(0);
+    return false;
+  }
+
+  // Do the call now.
+  if (LIKELY(Runtime::Current()->IsStarted())) {
+    ArtMethod* target = new_shadow_frame->GetMethod();
+    if (ClassLinker::ShouldUseInterpreterEntrypoint(
+        target,
+        target->GetEntryPointFromQuickCompiledCode())) {
+      ArtInterpreterToInterpreterBridge(self, code_item, new_shadow_frame, result);
+    } else {
+      ArtInterpreterToCompiledCodeBridge(
+          self, shadow_frame.GetMethod(), code_item, new_shadow_frame, result);
+    }
+  } else {
+    UnstartedRuntime::Invoke(self, code_item, new_shadow_frame, result, first_dest_reg);
+  }
+
+  // TODO(narayan): Perform return value conversions.
+
+  return !self->IsExceptionPending();
 }
 
 template <bool is_range,
