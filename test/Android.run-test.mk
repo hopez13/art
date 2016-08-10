fb3f7fb4c2847028fdf7b30c3f89c526df7a687c
Roland Levillain <rpl@google.com>
2016-08-10 17:29:00 +0100

    Add support for Baker read barriers in SystemArrayCopy intrinsics.
    Test: ART host and target (ARM, ARM64) tests.
    Bug: 29516905
    Bug: 29506760
    Bug: 12687968
    Change-Id: I85431368d09965687a0301ae2eb3c991f276ce5d

--- test/Android.run-test.mk
+++ test/Android.run-test.mk
@@ -555,11 +555,8 @@ TEST_ART_BROKEN_INTERPRETER_READ_BARRIER_RUN_TESTS :=
 # Tests that should fail in the read barrier configuration with the Optimizing compiler (AOT).
 # 484: Baker's fast path based read barrier compiler instrumentation generates code containing
 #      more parallel moves on x86, thus some Checker assertions may fail.
-# 537: Expects an array copy to be intrinsified on x86-64, but calling-on-slowpath intrinsics are
-#      not yet handled in the read barrier configuration.
 TEST_ART_BROKEN_OPTIMIZING_READ_BARRIER_RUN_TESTS := \
-  484-checker-register-hints \
-  537-checker-arraycopy
+  484-checker-register-hints
 
 # Tests that should fail in the read barrier configuration with JIT (Optimizing compiler).
 TEST_ART_BROKEN_JIT_READ_BARRIER_RUN_TESTS :=
