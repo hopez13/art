24ba1d76e912035e1e56c9a0abf3c5072209c8f8
David Brazdil <dbrazdil@google.com>
2016-09-06 14:04:48 +0100

    Collect verifier dependencies
    MethodVerifier tests whether a DEX method is valid w.r.t. the classes
    in class path. Since the APK does not change across OTA updates, it
    is not necessary to analyze the bytecode again with MethodVerifier,
    as long as its dependencies on the class path (which may have changed)
    are satisfied.
    
    This patch introduces VerifierDeps, a class path dependency collector,
    and adds hooks into MethodVerifier where classes/methods/fields are
    resolved and where assignability of types is tested.
    
    Test: m test-art-host-gtest-verifier_deps_test
    Bug: 30937355
    Change-Id: Iee0b321d772a5c7d1cb471aaa6e13918310b7e2f

--- compiler/common_compiler_test.cc
+++ compiler/common_compiler_test.cc
@@ -203,6 +203,7 @@ void CommonCompilerTest::SetUpRuntimeOptions(RuntimeOptions* options) {
   method_inliner_map_.reset(new DexFileToMethodInlinerMap);
   callbacks_.reset(new QuickCompilerCallbacks(verification_results_.get(),
                                               method_inliner_map_.get(),
+                                              nullptr /* verifier_deps */,
                                               CompilerCallbacks::CallbackMode::kCompileApp));
 }
 
