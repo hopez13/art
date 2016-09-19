cc9d8f93c64ea8275a0d18d92dd6d83c5f782672
Alexey Alexandrov <aalexand@google.com>
2016-09-19 09:33:49 -0700

    Generate SHA-1 build ID for host-generated *.oat files.
    For host-generated *.oat files, generate a SHA-1 build ID based on the
    file content and write it to .note.gnu.build-id ELF section.  This
    should allow various developer tools like profilers correlate the data
    captured for files like boot.oat on the device with the corresponding
    known version of the file during an offline analysis.
    
    Bug: 31292208
    Test: Verified that boot.oat contains the build ID section now.
    Test: make test-art-host
    Change-Id: Ie5e89da2ef87e34c27c0237ab34ddc7d2dc0aa3b

--- dex2oat/dex2oat.cc
+++ dex2oat/dex2oat.cc
@@ -337,6 +337,13 @@ NO_RETURN static void Usage(const char* fmt, ...) {
   UsageError("      debug information necessary to print backtraces. (disabled by default)");
   UsageError("");
   UsageError("  --no-generate-mini-debug-info: Do not generate backtrace info.");
+#ifdef SUPPORT_BUILD_ID_GENERATION
+  UsageError("");
+  UsageError("  --generate-build-id: Generate GNU-compatible linker build ID ELF section with");
+  UsageError("      SHA-1 of the file content (and thus stable across identical builds)");
+  UsageError("");
+  UsageError("  --no-generate-build-id: Do not generate the build ID ELF section.");
+#endif
   UsageError("");
   UsageError("  --debuggable: Produce code debuggable with Java debugger.");
   UsageError("");
