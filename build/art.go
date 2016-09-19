28dce0da42d7c6e8c5bb44bd4139c7f7230c8551
Colin Cross <ccross@android.com>
2016-09-19 12:25:31 -0700

    Increase host frame size limit for SANITIZE_HOST
    When SANITIZE_HOST=address, art/test/137-cfi/cfi.cc fails with:
    error: stack frame size of 1944 bytes in function 'Java_Main_unwindInProcess'
    Increase the frame size when SANITIZE_HOST is set, the same way it is
    increased for device builds.
    
    Test: m -j libarttest SANITIZE_HOST=address
    Change-Id: I08d8492e6629159b0e0306cb9499debbe6bb865c

--- build/art.go
+++ build/art.go
@@ -109,6 +109,9 @@ func deviceFlags(ctx android.BaseContext) []string {
 func hostFlags(ctx android.BaseContext) []string {
 	var cflags []string
 	hostFrameSizeLimit := 1736
+	if len(ctx.AConfig().SanitizeHost()) > 0 {
+		hostFrameSizeLimit = 6400
+	}
 	cflags = append(cflags,
 		fmt.Sprintf("-Wframe-larger-than=%d", hostFrameSizeLimit),
 		fmt.Sprintf("-DART_FRAME_SIZE_LIMIT=%d", hostFrameSizeLimit),
