042389482b474e9db21b75a178cd6f5a9f30e77d
David Sehr <sehr@google.com>
2016-10-19 13:20:45 -0700

    Remove mutex dependency on art::Runtime
    Breaks the cyclic dependency between mutex and the runtime.  This allows
    the use of mutexes without instantiating a runtime.
    
    Bug: 22322814
    Test: test-art
    Change-Id: Ia642e515937068d385e5bb1e10bbd3e50a6e36d2

--- runtime/runtime.cc
+++ runtime/runtime.cc
@@ -508,6 +508,12 @@ bool Runtime::ParseOptions(const RuntimeOptions& raw_options,
   return true;
 }
 
+// Callback to allow mutex to check whether the runtime is shutting down.
+static bool IsSafeToCallAbort() NO_THREAD_SAFETY_ANALYSIS {
+  Runtime* runtime = Runtime::Current();
+  return runtime != nullptr && runtime->IsStarted() && !runtime->IsShuttingDownLocked();
+}
+
 bool Runtime::Create(RuntimeArgumentMap&& runtime_options) {
   // TODO: acquire a static mutex on Runtime to avoid racing.
   if (Runtime::instance_ != nullptr) {
@@ -521,6 +527,7 @@ bool Runtime::Create(RuntimeArgumentMap&& runtime_options) {
     instance_ = nullptr;
     return false;
   }
+  Locks::SetClientCallback(IsSafeToCallAbort);
   return true;
 }
 
