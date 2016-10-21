01e8d7fa7ce029e373853c6176909e09135c6eff
Igor Murashkin <iam@google.com>
2016-10-21 12:29:59 -0700

    tools: Fix buildbot-build.sh on aosp angler
    Don't use $TARGET_PRODUCT to figure out build directory, this is sometimes wrong and it should've
    at least used $TARGET_DEVICE instead.
    
    Go a step further and get $TARGET_OUT/$HOST_OUT from the build
    system which will always return the right value.
    
    Bug: 32333814
    Change-Id: I0cb1128aeea66017628ea8a0bfd1164332d857b2

--- tools/buildbot-build.sh
+++ tools/buildbot-build.sh
@@ -14,14 +14,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if [ ! -d art ]; then
+if [ ! -f build/envsetup.sh ]; then
   echo "Script needs to be run at the root of the android tree"
   exit 1
 fi
 
-out_dir=${OUT_DIR-out}
-java_libraries_dir=${out_dir}/target/common/obj/JAVA_LIBRARIES
-common_targets="vogar core-tests apache-harmony-jdwp-tests-hostdex jsr166-tests mockito-target ${out_dir}/host/linux-x86/bin/jack"
+# Get all the android functions like "get_build_var"
+source "build/envsetup.sh"
+
+# Get the out directories from the build, which already respects a custom $OUT_DIR if one was set.
+out_dir="$(get_build_var OUT_DIR)"
+HOST_OUT="$(get_build_var HOST_OUT)"
+TARGET_OUT="$(get_build_var TARGET_OUT)"
+
+common_targets="vogar core-tests apache-harmony-jdwp-tests-hostdex jsr166-tests mockito-target $HOST_OUT/bin/jack"
 mode="target"
 j_arg="-j$(nproc)"
 showcommands=
@@ -57,13 +63,13 @@ fi
 
 if [[ $mode == "host" ]]; then
   make_command="make $j_arg $showcommands build-art-host-tests $common_targets"
-  make_command+=" ${out_dir}/host/linux-x86/lib/libjavacoretests.so "
-  make_command+=" ${out_dir}/host/linux-x86/lib64/libjavacoretests.so"
+  make_command+=" $HOST_OUT/lib/libjavacoretests.so "
+  make_command+=" $HOST_OUT/lib64/libjavacoretests.so"
 elif [[ $mode == "target" ]]; then
   make_command="make $j_arg $showcommands build-art-target-tests $common_targets"
   make_command+=" libjavacrypto libjavacoretests libnetd_client linker toybox toolbox sh"
-  make_command+=" ${out_dir}/host/linux-x86/bin/adb libstdc++ "
-  make_command+=" ${out_dir}/target/product/${TARGET_PRODUCT}/system/etc/public.libraries.txt"
+  make_command+=" $HOST_OUT/bin/adb libstdc++ "
+  make_command+=" $TARGET_OUT/etc/public.libraries.txt"
 fi
 
 echo "Executing $make_command"
