14c72c710fc169a5c4f5ee96d18c71de95dbb9b1
David Sehr <sehr@google.com>
2016-11-04 15:48:34 -0700

    WIP: inlining a function that does not return
    So far: a regression test that reproduces the failure discovered by the
    scanner.  This illustrates that the issue with LinearOrder failing is
    due to incorrect loop information being computed for blocks that are
    not on a path to the inlined function's return.  They should not be
    considered part of the caller's enclosing loop, but are today.
    
    Bug: 32547653
    Test: run-test --host 478-checker-inline-noreturn
    Change-Id: I9694a1cb861430051c801d07f7ce29752332cba5

---
+++ test/478-checker-inline-noreturn/src/Main.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/*
+ * A compile only test that checks that inlining a function that does not return works with
+ * the LinearOrder computation.
+ */
+
+public class Main {
+
+  public static void assertIntEquals(int expected, int result) {
+    if (expected != result) {
+      throw new Error("Expected: " + expected + ", found: " + result);
+    }
+  }
+
+  public static int Inline(int x, int y) {
+    int result;
+    if (x <= y) {
+      result = 42;
+    } else {
+      while (true);
+    }
+    return result;
+  }
+
+  public static int NestedLoop(int max_x, int max_y) {
+    int total = 0;
+    for (int x = 0; x < max_x; ++x) {
+      total += Inline(x, max_y);
+    }
+    return total;
+  }
+
+  public static void main(String[] args) {
+    assertIntEquals(42, NestedLoop(1, 1));
+  }
+}
