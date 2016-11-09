2b2f8eec3f52cadcf36442eb84e2cd02c94ba3a1
Orion Hodson <oth@google.com>
2016-11-09 08:32:42 +0000

    MethodHandles: Enable return value conversions.
    Enables return values conversions and simplify the type conversion
    logic. Ensures check for WrongMethodTypeExceptions before invoking a
    method handle.
    
    Test: m test-art-host-run-test-956-methodhandles
    Bug: 30550796
    
    Change-Id: I8add3ad0a19b43010946b4fb9f1c0f0949287860

--- runtime/mirror/method_type.h
+++ runtime/mirror/method_type.h
@@ -52,9 +52,19 @@ class MANAGED MethodType : public Object {
   static void ResetClass() REQUIRES_SHARED(Locks::mutator_lock_);
   static void VisitRoots(RootVisitor* visitor) REQUIRES_SHARED(Locks::mutator_lock_);
 
-  // Returns true iff. |other| is an exact match for this method type, i.e
+  // Returns true iff. |this| is an exact match for method type |target|, i.e
   // iff. they have the same return types and parameter types.
-  bool IsExactMatch(mirror::MethodType* other) REQUIRES_SHARED(Locks::mutator_lock_);
+  bool IsExactMatch(mirror::MethodType* target) REQUIRES_SHARED(Locks::mutator_lock_);
+
+  // Returns true iff. |this| can be converted to match |target| method type, i.e
+  // iff. they have convertible return types and parameter types.
+  bool IsNonExactMatch(mirror::MethodType* target) REQUIRES_SHARED(Locks::mutator_lock_);
+
+  // Returns true iff. |this| is a match for |target|. The comparison
+  // is made with IsExactMatch() if |exact| is true, targetwise with IsNonExactMatch().
+  bool IsMatch(bool exact, mirror::MethodType* target) REQUIRES_SHARED(Locks::mutator_lock_) {
+    return exact ? IsExactMatch(target) : IsNonExactMatch(target);
+  }
 
   // Returns the pretty descriptor for this method type, suitable for display in
   // exception messages and the like.
