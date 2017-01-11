bionic-linux/android_art

ART: DEX file support for method types and method handles.
Introduces HeaderExtensions for new header sections.

Introduces sections for method types and method handles for invoke-custom.

Change-Id: Ic7b6c1c031e871522078bbaf1584f48f79d0db44

BUG: 33191717, 30550796
Test

runtime/dex_file.h
@@ -77,8 +77,8 @@ class DexFile {
     uint32_t file_size_;  // size of entire file
     uint32_t header_size_;  // offset to start of next section
     uint32_t endian_tag_;
-    uint32_t link_size_;  // unused
-    uint32_t link_off_;  // unused
+    uint32_t extensions_size_;  // number of HeaderExtensions since v38 (was link_size_)
+    uint32_t extensions_off_;  // file offset of HeaderExtensions array since v38 (was link_off_)
     uint32_t map_off_;  // unused
     uint32_t string_ids_size_;  // number of StringIds
     uint32_t string_ids_off_;  // file offset of StringIds array
@@ -98,8 +98,26 @@ class DexFile {
     // Decode the dex magic version
     uint32_t GetVersion() const;
 
+    // Returns true if version 38 or above.
+    bool VersionSupportsHeaderExtensions() const;
+
    private:
     DISALLOW_COPY_AND_ASSIGN(Header);
+  };
+
+  // Enumeration of header extension types (introduced in version 38).
+  enum HeaderExtensionType : uint16_t {
+    kDexHeaderExtensionMethodType   = 0x0000,
+    kDexHeaderExtensionMethodHandle = 0x0001,
+    kDexHeaderExtensionLast = kDexHeaderExtensionMethodHandle,
+  };
+
+  // Raw extension_header_item
+  struct HeaderExtension {
+    uint16_t type_;
+    uint16_t reserved_;
+    uint32_t size_;
+    uint32_t off_;
   };
 
   // Map item type codes.
@@ -389,6 +407,31 @@ class DexFile {
 
   struct AnnotationValue;
 
+  struct MethodTypeItem {
+    uint16_t proto_id_idx_;
+   private:
+    DISALLOW_COPY_AND_ASSIGN(MethodTypeItem);
+  };
+
+  enum {
+    kPutStatic = 0x00,         // a setter for a given static field.
+    kGetStatic = 0x01,          // a getter for a given static field.
+    kPutInstance = 0x02,       // a setter for a given instance field.
+    kGetInstance = 0x03,       // a getter for a given instance field.
+    kInvokeStatic = 0x04,      // an invoker for a given static method
+    kInvokeInstance = 0x05,    // invoke_instance : an invoker for a given instance method. This can
+                               // be any non-static method on any class (or interface) except for
+                               // “<init>”.
+    kInvokeConstructor = 0x06  // an invoker for a given constructor.
+  };
+
+  struct MethodHandleItem {
+    uint16_t method_handle_type_;
+    uint16_t field_or_method_idx_;
+   private:
+    DISALLOW_COPY_AND_ASSIGN(MethodHandleItem);
+  };
+
   // Returns the checksum of a file for comparison with GetLocationChecksum().
   // For .dex files, this is the header checksum.
   // For zip files, this is the classes.dex zip entry CRC32 checksum.
@@ -493,6 +536,17 @@ class DexFile {
 
   // Returns true if the byte string after the magic is the correct value.
   static bool IsVersionValid(const uint8_t* magic);
+
+  // Returns the number of extension headers in the .dex file.
+  size_t NumHeaderExtensions() const {
+    return header_->VersionSupportsHeaderExtensions() ? header_->extensions_size_ : 0;
+  }
+
+  // Returns the |extension_idx| header extension present.
+  const HeaderExtension& GetHeaderExtension(uint16_t extension_idx) const;
+
+  // Returns the name of the header extension of |extension_type|.
+  static std::string GetHeaderExtensionName(uint16_t extension_type);
 
   // Returns the number of string identifiers in the .dex file.
   size_t NumStringIds() const {
@@ -1431,7 +1485,9 @@ class EncodedStaticFieldValueIterator {
     kArray = 0x1c,
     kAnnotation = 0x1d,
     kNull = 0x1e,
-    kBoolean = 0x1f
+    kBoolean = 0x1f,
+    kMethodType = 0x15,
+    kMethodHandle = 0x16,
   };
 
   ValueType GetValueType() const { return type_; }

hopez13/art

runtime/dex_file.h
@@ -77,8 +77,8 @@ class DexFile {
     uint32_t file_size_;  // size of entire file
     uint32_t header_size_;  // offset to start of next section
     uint32_t endian_tag_;
-    uint32_t link_size_;  // unused
-    uint32_t link_off_;  // unused
+    uint32_t extensions_size_;  // number of HeaderExtensions since v38 (was link_size_)
+    uint32_t extensions_off_;  // file offset of HeaderExtensions array since v38 (was link_off_)
     uint32_t map_off_;  // unused
     uint32_t string_ids_size_;  // number of StringIds
     uint32_t string_ids_off_;  // file offset of StringIds array
@@ -98,8 +98,26 @@ class DexFile {
     // Decode the dex magic version
     uint32_t GetVersion() const;
 
+    // Returns true if version 38 or above.
+    bool VersionSupportsHeaderExtensions() const;
+
    private:
     DISALLOW_COPY_AND_ASSIGN(Header);
+  };
+
+  // Enumeration of header extension types (introduced in version 38).
+  enum HeaderExtensionType : uint16_t {
+    kDexHeaderExtensionMethodHandles = 0x0000,
+    kDexHeaderExtensionCallSites     = 0x0001,
+    kDexHeaderExtensionLast          = kDexHeaderExtensionCallSites,
+  };
+
+  // Raw extension_header_item
+  struct HeaderExtension {
+    uint16_t type_;
+    uint16_t reserved_;
+    uint32_t size_;
+    uint32_t off_;
   };
 
   // Map item type codes.
@@ -389,6 +407,40 @@ class DexFile {
 
   struct AnnotationValue;
 
+  enum {
+    kPutStatic         = 0x00, // a setter for a given static field.
+    kGetStatic         = 0x01, // a getter for a given static field.
+    kPutInstance       = 0x02, // a setter for a given instance field.
+    kGetInstance       = 0x03, // a getter for a given instance field.
+    kInvokeStatic      = 0x04, // an invoker for a given static method
+    kInvokeInstance    = 0x05, // invoke_instance : an invoker for a given instance method. This can
+                               // be any non-static method on any class (or interface) except for
+                               // “<init>”.
+    kInvokeConstructor = 0x06  // an invoker for a given constructor.
+  };
+
+  struct MethodHandle {
+    uint16_t method_handle_type_;
+    uint16_t field_or_method_idx_;
+   private:
+    DISALLOW_COPY_AND_ASSIGN(MethodHandle);
+  };
+
+  struct MethodHandleItem {
+    uint16_t method_handle_type_;
+    uint16_t reserved1_;
+    uint16_t field_or_method_idx_;
+    uint16_t reserved2_;
+   private:
+    DISALLOW_COPY_AND_ASSIGN(MethodHandleItem);
+  };
+
+  struct CallSite {
+    uint32_t data_off_;  // Offset into data section pointing to encoded array items.
+   private:
+    DISALLOW_COPY_AND_ASSIGN(CallSite);
+  };
+
   // Returns the checksum of a file for comparison with GetLocationChecksum().
   // For .dex files, this is the header checksum.
   // For zip files, this is the classes.dex zip entry CRC32 checksum.
@@ -493,6 +545,17 @@ class DexFile {
 
   // Returns true if the byte string after the magic is the correct value.
   static bool IsVersionValid(const uint8_t* magic);
+
+  // Returns the number of extension headers in the .dex file.
+  size_t NumHeaderExtensions() const {
+    return header_->VersionSupportsHeaderExtensions() ? header_->extensions_size_ : 0;
+  }
+
+  // Returns the |extension_idx| header extension present.
+  const HeaderExtension& GetHeaderExtension(uint16_t extension_idx) const;
+
+  // Returns the name of the header extension of |extension_type|.
+  static std::string GetHeaderExtensionName(uint16_t extension_type);
 
   // Returns the number of string identifiers in the .dex file.
   size_t NumStringIds() const {
@@ -1431,7 +1494,8 @@ class EncodedStaticFieldValueIterator {
     kArray = 0x1c,
     kAnnotation = 0x1d,
     kNull = 0x1e,
-    kBoolean = 0x1f
+    kBoolean = 0x1f,
+    kMethodHandle = 0x15,
   };
 
   ValueType GetValueType() const { return type_; }