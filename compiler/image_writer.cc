9465ea04f60f2132185fc69209502ee320231d81
Vladimir Marko <vmarko@google.com>
2017-01-18 15:22:59 +0000

    Fix inserting classes to initiating loader's class table.
    Fix two cases that were missed in
        https://android-review.googlesource.com/312285
    
    First, copy all class references to app image class table,
    including boot image classes where the class loader used for
    AOT is only the initiating loader, not the defining loader.
    
    Second, add array classes to the initiating loader's class
    table.
    
    Without these fixes, ClassLinker::LookupResolvedType() was
    actually relying on the type being in the dex cache because
    in some cases the slow path would not be able to find it.
    
    Test: m test-art-host
    Bug: 30627598
    Change-Id: I4fa6405ccf52dd6a705f9a2c9f0e5bc59f97e63b

--- compiler/image_writer.cc
+++ compiler/image_writer.cc
@@ -1167,9 +1167,11 @@ mirror::Object* ImageWriter::TryAssignBinSlot(WorkStack& work_stack,
       oat_index = GetOatIndexForDexCache(dex_cache);
       ImageInfo& image_info = GetImageInfo(oat_index);
       {
-        // Note: This table is only accessed from the image writer, avoid locking to prevent lock
-        // order violations from root visiting.
-        image_info.class_table_->InsertWithoutLocks(as_klass);
+        if (!compile_app_image_) {
+          // Note: Avoid locking to prevent lock order violations from root visiting;
+          // image_info.class_table_ is only accessed from the image writer.
+          image_info.class_table_->InsertWithoutLocks(as_klass);
+        }
       }
       for (LengthPrefixedArray<ArtField>* cur_fields : fields) {
         // Total array length including header.
@@ -1265,7 +1267,14 @@ mirror::Object* ImageWriter::TryAssignBinSlot(WorkStack& work_stack,
       // class loader.
       mirror::ClassLoader* class_loader = obj->AsClassLoader();
       if (class_loader->GetClassTable() != nullptr) {
+        DCHECK(compile_app_image_);
+        DCHECK(class_loaders_.empty());
         class_loaders_.insert(class_loader);
+        ImageInfo& image_info = GetImageInfo(oat_index);
+        // Note: Avoid locking to prevent lock order violations from root visiting;
+        // image_info.class_table_ table is only accessed from the image writer
+        // and class_loader->GetClassTable() is iterated but not modified.
+        image_info.class_table_->CopyWithoutLocks(class_loader->GetClassTable());
       }
     }
     AssignImageBinSlot(obj, oat_index);
