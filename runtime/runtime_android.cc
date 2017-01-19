4907464ffc637b5823fdb61e7a4c3693aacf468a
Roland Levillain <rpl@google.com>
2017-01-19 20:04:27 +0000

    Optionally dump ART crash information to logcat when running on Android.
    This logging feature does not use debuggerd.  It is only
    enabled when the environment variable ANDROID_ROOT is set
    to something different than "/system".
    
    Test: Observe device logcat when dalvikvm crashes
    Bug: 32466479
    Change-Id: If59ce838fdb5b9bb6638b73fcd074b7065275c6c

--- runtime/runtime_android.cc
+++ runtime/runtime_android.cc
@@ -15,20 +15,25 @@
  */
 
 #include <signal.h>
-#include <string.h>
 #include <sys/utsname.h>
-#include <inttypes.h>
+
+#include <cinttypes>
+#include <string>
+
+#include "android-base/stringprintf.h"
 
 #include "base/logging.h"
 #include "base/mutex.h"
+#include "runtime_common.h"
 #include "thread-inl.h"
 #include "utils.h"
 
 namespace art {
 
-static constexpr bool kUseSignalHandler = false;
+using android::base::StringPrintf;
 
 struct sigaction old_action;
+
 void HandleUnexpectedSignal(int signal_number, siginfo_t* info, void* raw_context) {
   static bool handling_unexpected_signal = false;
   if (handling_unexpected_signal) {
@@ -39,9 +44,38 @@ void HandleUnexpectedSignal(int signal_number, siginfo_t* info, void* raw_contex
     _exit(1);
   }
   handling_unexpected_signal = true;
+
   gAborting++;  // set before taking any locks
   MutexLock mu(Thread::Current(), *Locks::unexpected_signal_lock_);
 
+  bool has_address = (signal_number == SIGILL || signal_number == SIGBUS ||
+                      signal_number == SIGFPE || signal_number == SIGSEGV);
+
+  OsInfo os_info;
+  const char* cmd_line = GetCmdLine();
+  if (cmd_line == nullptr) {
+    cmd_line = "<unset>";  // Because no-one called InitLogging.
+  }
+  pid_t tid = GetTid();
+  std::string thread_name(GetThreadName(tid));
+  UContext thread_context(raw_context);
+  Backtrace thread_backtrace(raw_context);
+
+  LOG(FATAL_WITHOUT_ABORT)
+      << "*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***\n"
+      << StringPrintf("Fatal signal %d (%s), code %d (%s)",
+                      signal_number, GetSignalName(signal_number),
+                      info->si_code,
+                      GetSignalCodeName(signal_number, info->si_code))
+      << (has_address ? StringPrintf(" fault addr %p", info->si_addr) : "") << std::endl
+      << "OS: " << Dumpable<OsInfo>(os_info) << std::endl
+      << "Cmdline: " << cmd_line << std::endl
+      << "Thread: " << tid << " \"" << thread_name << "\"" << std::endl
+      << "Registers:\n" << Dumpable<UContext>(thread_context) << std::endl
+      << "Backtrace:\n" << Dumpable<Backtrace>(thread_backtrace) << std::endl;
+  if (kIsDebugBuild && signal_number == SIGSEGV) {
+    PrintFileToLog("/proc/self/maps", LogSeverity::FATAL_WITHOUT_ABORT);
+  }
   Runtime* runtime = Runtime::Current();
   if (runtime != nullptr) {
     // Print this out first in case DumpObject faults.
@@ -52,7 +86,10 @@ void HandleUnexpectedSignal(int signal_number, siginfo_t* info, void* raw_contex
 }
 
 void Runtime::InitPlatformSignalHandlers() {
-  if (kUseSignalHandler) {
+  // Enable the signal handler dumping crash information to the logcat
+  // when the Android root is not "/system".
+  const char* android_root = getenv("ANDROID_ROOT");
+  if (android_root != 0 && strcmp(android_root, "/system") != 0) {
     struct sigaction action;
     memset(&action, 0, sizeof(action));
     sigemptyset(&action.sa_mask);
@@ -61,8 +98,18 @@ void Runtime::InitPlatformSignalHandlers() {
     action.sa_flags |= SA_SIGINFO;
     // Use the alternate signal stack so we can catch stack overflows.
     action.sa_flags |= SA_ONSTACK;
+
     int rc = 0;
+    rc += sigaction(SIGABRT, &action, &old_action);
+    rc += sigaction(SIGBUS, &action, &old_action);
+    rc += sigaction(SIGFPE, &action, &old_action);
+    rc += sigaction(SIGILL, &action, &old_action);
+    rc += sigaction(SIGPIPE, &action, &old_action);
     rc += sigaction(SIGSEGV, &action, &old_action);
+#if defined(SIGSTKFLT)
+    rc += sigaction(SIGSTKFLT, &action, &old_action);
+#endif
+    rc += sigaction(SIGTRAP, &action, &old_action);
     CHECK_EQ(rc, 0);
   }
 }
