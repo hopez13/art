f38b94d746c8a824ce29ad480cd78e32b9ebf1ab
Roland Levillain <rpl@google.com>
2017-01-19 20:04:27 +0000

    Optionally dump ART crash information to logcat when running on Android.
    This logging feature does not use debuggerd.  It is only
    enabled when the environment variable ANDROID_ROOT is set
    to something different than "/system".
    
    Test: Observe device logcat when dalvikvm crashes
    Bug: 32466479
    Change-Id: If59ce838fdb5b9bb6638b73fcd074b7065275c6c

--- runtime/runtime_android.cc
+++ runtime/runtime_android.cc
@@ -14,55 +14,52 @@
  * limitations under the License.
  */
 
-#include <signal.h>
-#include <string.h>
-#include <sys/utsname.h>
-#include <inttypes.h>
+#include "runtime.h"
 
-#include "base/logging.h"
-#include "base/mutex.h"
-#include "thread-inl.h"
-#include "utils.h"
+#include <signal.h>
+#include <sys/utsname.h>
+
+#include <cinttypes>
+#include <string>
+
+#include "runtime_common.h"
 
 namespace art {
 
-static constexpr bool kUseSignalHandler = false;
-
 struct sigaction old_action;
-void HandleUnexpectedSignal(int signal_number, siginfo_t* info, void* raw_context) {
-  static bool handling_unexpected_signal = false;
-  if (handling_unexpected_signal) {
-    LogHelper::LogLineLowStack(__FILE__,
-                               __LINE__,
-                               ::android::base::FATAL_WITHOUT_ABORT,
-                               "HandleUnexpectedSignal reentered\n");
-    _exit(1);
-  }
-  handling_unexpected_signal = true;
-  gAborting++;  // set before taking any locks
-  MutexLock mu(Thread::Current(), *Locks::unexpected_signal_lock_);
 
-  Runtime* runtime = Runtime::Current();
-  if (runtime != nullptr) {
-    // Print this out first in case DumpObject faults.
-    LOG(FATAL_WITHOUT_ABORT) << "Fault message: " << runtime->GetFaultMessage();
-  }
+void HandleUnexpectedSignalAndroid(int signal_number, siginfo_t* info, void* raw_context) {
+  HandleUnexpectedSignalCommon(signal_number, info, raw_context, /* running_on_linux */ false);
+
   // Run the old signal handler.
   old_action.sa_sigaction(signal_number, info, raw_context);
 }
 
 void Runtime::InitPlatformSignalHandlers() {
-  if (kUseSignalHandler) {
+  // Enable the signal handler dumping crash information to the logcat
+  // when the Android root is not "/system".
+  const char* android_root = getenv("ANDROID_ROOT");
+  if (android_root != nullptr && strcmp(android_root, "/system") != 0) {
     struct sigaction action;
     memset(&action, 0, sizeof(action));
     sigemptyset(&action.sa_mask);
-    action.sa_sigaction = HandleUnexpectedSignal;
+    action.sa_sigaction = HandleUnexpectedSignalAndroid;
     // Use the three-argument sa_sigaction handler.
     action.sa_flags |= SA_SIGINFO;
     // Use the alternate signal stack so we can catch stack overflows.
     action.sa_flags |= SA_ONSTACK;
+
     int rc = 0;
+    rc += sigaction(SIGABRT, &action, &old_action);
+    rc += sigaction(SIGBUS, &action, &old_action);
+    rc += sigaction(SIGFPE, &action, &old_action);
+    rc += sigaction(SIGILL, &action, &old_action);
+    rc += sigaction(SIGPIPE, &action, &old_action);
     rc += sigaction(SIGSEGV, &action, &old_action);
+#if defined(SIGSTKFLT)
+    rc += sigaction(SIGSTKFLT, &action, &old_action);
+#endif
+    rc += sigaction(SIGTRAP, &action, &old_action);
     CHECK_EQ(rc, 0);
   }
 }
