8ec5ed663e30eb1af38d4b4560908634116f498a
Nicolas Geoffray <ngeoffray@google.com>
2017-02-10 10:59:22 +0000

    Add a DCHECK in thread::GetPeer.
    Motivated  by https://android-review.googlesource.com/#/c/333205/.
    
    Test: test-art-host run-jdwp-test.sh
    Change-Id: I173c060324aa0dc39144db55e3a97e672c012ba8

--- runtime/openjdkjvmti/ti_stack.cc
+++ runtime/openjdkjvmti/ti_stack.cc
@@ -328,7 +328,7 @@ jvmtiError StackUtil::GetAllStackTraces(jvmtiEnv* env,
 
     // For the time being, set the thread to null. We don't have good ScopedLocalRef
     // infrastructure.
-    DCHECK(self->GetPeer() != nullptr);
+    DCHECK(self->GetPeerFromOtherThread() != nullptr);
     stack_info.thread = nullptr;
     stack_info.state = JVMTI_THREAD_STATE_SUSPENDED;
 
