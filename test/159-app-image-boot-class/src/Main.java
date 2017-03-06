5c696709196ccb55260aa5683f0e9930acc40e45
Mathieu Chartier <mathieuc@google.com>
2017-03-06 12:35:10 -0800

    Support boot class path classes in app images
    Record references to non image boot class path classes in app images.
    When the app image is loaded, these references are fixed up if the
    class is already loaded.
    
    Added basic unit test.
    
    Test: test-art-host -j32
    
    Bug: 34927277
    Bug: 34928633
    
    Change-Id: I8fe653b1bebc4e7e9d9566c78b2705347f5b8757

---
+++ test/159-app-image-boot-class/src/Main.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import sun.misc.SharedSecrets;
+
+class Main {
+    static class ExtendsBootClass extends sun.misc.SharedSecrets {
+        public static int abc = 0;
+    }
+  
+    public static void main(String[] args) {
+        System.loadLibrary(args[0]);
+        try {
+            Class classLoaderVersion = Class.forName("sun.misc.SharedSecrets");
+            Class superVersion = new ExtendsBootClass().getClass().getSuperclass();
+            Class directVersion = sun.misc.SharedSecrets.class; 
+            System.out.println("Classes match: " + (classLoaderVersion == superVersion));
+            System.out.println("Classes match: " + (superVersion == directVersion));
+            // Should be in the boot class loader
+            System.out.println("Is boot class loader class: " +
+                  (classLoaderVersion.getClassLoader() == String.class.getClassLoader()));
+            // Should not be in the boot image.
+            System.out.println("Is boot image class: " + bootImageContains(classLoaderVersion));
+            // Should not be in the app image.
+            System.out.println("Is app image class: " + appImageContains(classLoaderVersion));
+            // Extender should be in app image.
+            System.out.println("Extends is app image class: " +
+                  appImageContains(ExtendsBootClass.class));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static native boolean bootImageContains(Object klass);
+    public static native boolean appImageContains(Object klass);
+}
