bcd82efaa9a57b2c0eaa144bf4ff037af467985b
Shubham Ajmera <shubhamajmera@google.com>
2017-03-23 13:58:23 -0700

    Add test.py script to run gtest and run-test
    By default, it will run both run-test and gtest
    If either of the two are explicilty asked for, it will
    not run the other one.
    
    Options:
    --run-test : To run run-test
    --gtest : To run gtest
    -j : Number of jobs
    --host: for host tests
    --target: for target tests
    All the other arguments will be passed to the run-test testrunner.
    
    Test: ./art/test.py --host --target -j12 --run-test --gtest
    Change-Id: I2fc4ebb900fb72db83165c6ae1615b8bdcfc33f4

---
+++ test.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+#
+# Copyright 2017, The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# --run-test : To run run-test
+# --gtest : To run gtest
+# -j : Number of jobs
+# --host: for host tests
+# --target: for target tests
+# All the other arguments will be passed to the run-test testrunner.
+import sys
+import subprocess
+import os
+import argparse
+
+test_set = set()
+test_args = []
+ANDROID_BUILD_TOP = os.environ.get('ANDROID_BUILD_TOP', os.getcwd())
+
+for arg in sys.argv[1:]:
+  if arg == '--run-test':
+    test_set.add('run-test')
+  elif arg == '--gtest':
+    test_set.add('gtest')
+  else:
+    if arg is '--target' or arg is '--host':
+      test_args.add(arg[2:])
+    test_args.append(arg)
+
+if not test_set or 'run-test' in test_set:
+  testrunner = os.path.join('./',
+                       ANDROID_BUILD_TOP,
+                       'art/test/testrunner/testrunner.py')
+  test_runner_cmd = [testrunner] + test_args
+  print test_runner_cmd
+  if subprocess.call(test_runner_cmd):
+    sys.exit(1)
+
+if not test_set or 'gtest' in test_set:
+  build_target = ''
+  if '--host' in test_args:
+    build_target += ' test-art-host-gtest'
+  if '--target' in test_args:
+    build_target += ' test-art-target-gtest'
+
+  build_command = 'make'
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-j', type=int, dest='n_threads')
+  options, unknown = parser.parse_known_args(test_args)
+  if options.n_threads:
+    n_thread = max(1, options.n_threads)
+  build_command += ' -j' + str(n_thread)
+
+  build_command += ' -C ' + ANDROID_BUILD_TOP
+  build_command += ' ' + 'test-art-host-gtest'
+  # Add 'dist' to avoid Jack issues b/36169180.
+  build_command += ' dist'
+
+  print build_command
+
+  if subprocess.call(build_command.split()):
+      sys.exit(1)
+
+sys.exit(0)
