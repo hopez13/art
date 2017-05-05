bc008b5af0dfdb6489d4a0467759771eeea44e52
Jeff Hao <jeffhao@google.com>
2017-05-05 16:59:29 -0700

    Stop interpreter from accessing code items of compiled code.
    The ArtInterpreterToCompiledCodeBridge accesses the code item in a
    number of places to handle argument marshalling. However, the code item
    of a compiled method should have no need to be accessed by the runtime
    at all, since the code has been compiled. By removing these accesses,
    there is a drop in the memory footprint of the dex file, since these
    code items remain untouched by the runtime.
    
    Bug: 35800981
    Test: mm test-art-host
    Change-Id: I147a9267ec022547b384374e1449d20bcab1ead2

--- runtime/interpreter/interpreter.cc
+++ runtime/interpreter/interpreter.cc
@@ -264,7 +264,9 @@ static inline JValue Execute(
 
           // Pop the shadow frame before calling into compiled code.
           self->PopShadowFrame();
-          ArtInterpreterToCompiledCodeBridge(self, nullptr, code_item, &shadow_frame, &result);
+          // Calculate the offset of the first input reg. The input registers are in the high regs.
+          uint16_t arg_offset = code_item->registers_size_ - code_item->ins_size_;
+          ArtInterpreterToCompiledCodeBridge(self, nullptr, &shadow_frame, arg_offset, &result);
           // Push the shadow frame back as the caller will expect it.
           self->PushShadowFrame(&shadow_frame);
 
