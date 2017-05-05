2003aa149ba21fa7d9c861f8071d15d48171af9a
Nicolas Geoffray <ngeoffray@google.com>
2017-05-05 10:27:58 +0100

    Bump oat version after compiler filter name changes.
    bug:37988580
    Test: build
    Change-Id: I87aebe69b85168be9d1ecc2e31da675e818a5692

--- runtime/oat.h
+++ runtime/oat.h
@@ -32,8 +32,8 @@ class InstructionSetFeatures;
 class PACKED(4) OatHeader {
  public:
   static constexpr uint8_t kOatMagic[] = { 'o', 'a', 't', '\n' };
-  // Revert concurrent graying for immune spaces.
-  static constexpr uint8_t kOatVersion[] = { '1', '2', '2', '\0' };
+  // New compiler filter names.
+  static constexpr uint8_t kOatVersion[] = { '1', '2', '3', '\0' };
 
   static constexpr const char* kImageLocationKey = "image-location";
   static constexpr const char* kDex2OatCmdLineKey = "dex2oat-cmdline";
