a2544f450d044b35b6e1c6785236b7add6d7b201
Calin Juravle <calin@google.com>
2017-05-23 14:24:55 -0700

    Do not deopt when using AOT inline caches
    Generating a deopt does not ensure that we will actually capture the new
    types; and the danger is that we could be stuck in a loop with "forever"
    deoptimizations. For example, if the method never becomes hot again, the
    inline cache will not be updated and the AOT code will keep
    deoptimizing.
    
    This is comprise because we will most likely never update the inline
    cache (unless there's another reason to deopt). So we might be stuck
    with a sub-optimal inline cache. As a TODO, we could be smarter when
    capturing inline caches to mitigate this. (e.g. by having different
    thresholds for new and old methods).
    
    Delete test 644 which was testing a regression involving deopts which is
    no longer applicable.
    
    Bug: 38412648
    Test: m test-art-host
    Change-Id: Ib84559e84f07ec976feeb3a3120fa486d5bee3bf

--- compiler/optimizing/inliner.cc
+++ compiler/optimizing/inliner.cc
@@ -470,6 +470,23 @@ static Handle<mirror::ObjectArray<mirror::Class>> AllocateInlineCacheHolder(
   return inline_cache;
 }
 
+bool HInliner::UseOnlyPolymorphicInliningWithNoDeopt() {
+  // If we are compiling AOT or OSR, pretend the call using inline caches is polymorphic and
+  // do not generate a deopt.
+  //
+  // For AOT: Generating a deopt does not ensure that we will actually capture the new types;
+  //          and the danger is that we could be stuck in a loop with "forever" deoptimizations.
+  //          For example, if the method never becomes hot again, the inline cache will not be
+  //          updated and the AOT code will keep deoptimizing.
+  // TODO(calin): This is comprise because we will most likely never update the inline cache
+  //              (unless there's another reason to deopt). So we might be stuck with
+  //              a sub-optimal inline cache.
+  //              We could be smarter when capturing inline caches to mitigate this.
+  //              (e.g. by having different thresholds for new and old methods).
+  //
+  // For OSR: We may come from the interpreter and it may have seen different receiver types.
+  return Runtime::Current()->IsAotCompiler() || outermost_graph_->IsCompilingOsr();
+}
 bool HInliner::TryInlineFromInlineCache(const DexFile& caller_dex_file,
                                         HInvoke* invoke_instruction,
                                         ArtMethod* resolved_method)
@@ -503,9 +520,7 @@ bool HInliner::TryInlineFromInlineCache(const DexFile& caller_dex_file,
 
     case kInlineCacheMonomorphic: {
       MaybeRecordStat(kMonomorphicCall);
-      if (outermost_graph_->IsCompilingOsr()) {
-        // If we are compiling OSR, we pretend this call is polymorphic, as we may come from the
-        // interpreter and it may have seen different receiver types.
+      if (UseOnlyPolymorphicInliningWithNoDeopt()) {
         return TryInlinePolymorphicCall(invoke_instruction, resolved_method, inline_cache);
       } else {
         return TryInlineMonomorphicCall(invoke_instruction, resolved_method, inline_cache);
@@ -926,14 +941,11 @@ bool HInliner::TryInlinePolymorphicCall(HInvoke* invoke_instruction,
 
       // If we have inlined all targets before, and this receiver is the last seen,
       // we deoptimize instead of keeping the original invoke instruction.
-      bool deoptimize = all_targets_inlined &&
+      bool deoptimize = !UseOnlyPolymorphicInliningWithNoDeopt() &&
+          all_targets_inlined &&
           (i != InlineCache::kIndividualCacheSize - 1) &&
           (classes->Get(i + 1) == nullptr);
 
-      if (outermost_graph_->IsCompilingOsr()) {
-        // We do not support HDeoptimize in OSR methods.
-        deoptimize = false;
-      }
       HInstruction* compare = AddTypeGuard(receiver,
                                            cursor,
                                            bb_cursor,
