5ecd4015e10848115b7a1c8b0638ff264ce13f24
Andreas Gampe <agampe@google.com>
2017-05-31 19:46:03 -0700

    ART: Allow unlimited PushLocalFrame
    The local reference table is resizable. Allow arbitrary capacity
    requests and only fail when the request cannot be satisfied.
    
    Bug: 62223672
    Test: m test-art-host
    Change-Id: I05183098359c5a33473701e9a0d2a4d6c81bde58

--- runtime/jni_internal_test.cc
+++ runtime/jni_internal_test.cc
@@ -1908,9 +1908,6 @@ TEST_F(JniInternalTest, PushLocalFrame_10395422) {
 
   // Negative capacities are not allowed.
   ASSERT_EQ(JNI_ERR, env_->PushLocalFrame(-1));
-
-  // And it's okay to have an upper limit. Ours is currently 512.
-  ASSERT_EQ(JNI_ERR, env_->PushLocalFrame(8192));
 }
 
 TEST_F(JniInternalTest, PushLocalFrame_PopLocalFrame) {
@@ -1963,6 +1960,11 @@ TEST_F(JniInternalTest, PushLocalFrame_PopLocalFrame) {
 }
 
 TEST_F(JniInternalTest, PushLocalFrame_LimitAndOverflow) {
+  // This tests leads to errors in the log.
+  ScopedLogSeverity sls(LogSeverity::FATAL);
+
+  ScopedObjectAccess soa(env_);
+
   // Try a very large value that should fail.
   ASSERT_NE(JNI_OK, env_->PushLocalFrame(std::numeric_limits<jint>::max()));
   ASSERT_TRUE(env_->ExceptionCheck());
@@ -1976,6 +1978,13 @@ TEST_F(JniInternalTest, PushLocalFrame_LimitAndOverflow) {
   env_->ExceptionClear();
   EXPECT_EQ(env_->PopLocalFrame(nullptr), nullptr);
 #endif
+}
+
+TEST_F(JniInternalTest, PushLocalFrame_b62223672) {
+  ScopedObjectAccess soa(env_);
+  // The 512 entry limit has been lifted, try a larger value.
+  ASSERT_EQ(JNI_OK, env_->PushLocalFrame(1024));
+  EXPECT_EQ(env_->PopLocalFrame(nullptr), nullptr);
 }
 
 TEST_F(JniInternalTest, NewGlobalRef_nullptr) {
