f0f1776fdf5a211e91fa3b0b2bf9a0083fd13350
Chang Xing <chxing@google.com>
2017-06-08 17:16:12 -0700

    Add an init encoded fields threshold to compiler
    Add a check for the number of encoded fields when initializing app
    images and fail the initialization if too much encoded fields are
    presented. This helps against the adversary applications and reduce
    compiling time on test case 056.
    
    Bug: 62337922
    Test: art-host-test -j64
    Change-Id: I9f24fbe5ada473ec415459a36b0c22d2f504af62

--- compiler/driver/compiler_driver.cc
+++ compiler/driver/compiler_driver.cc
@@ -87,6 +87,9 @@ static constexpr bool kTimeCompileMethod = !kIsDebugBuild;
 // Print additional info during profile guided compilation.
 static constexpr bool kDebugProfileGuidedCompilation = false;
 
+// Max encoded fields allowed for initializing app image. 5000 is large enough so hardcoded.
+static constexpr uint32_t kMaxEncodedFields = 5000;
+
 static double Percentage(size_t x, size_t y) {
   return 100.0 * (static_cast<double>(x)) / (static_cast<double>(x + y));
 }
@@ -2273,11 +2276,18 @@ class InitializeClassVisitor : public CompilationVisitor {
         }
         // Otherwise it's in app image but superclasses can't be initialized, no need to proceed.
         old_status = klass->GetStatus();
+
+        bool not_too_many_encoded_fields = true;
+        const size_t num_static_fields = klass->NumStaticFields();
+        if (num_static_fields > kMaxEncodedFields && !is_boot_image) {
+          not_too_many_encoded_fields = false;
+        }
         // If the class was not initialized, we can proceed to see if we can initialize static
-        // fields.
+        // fields. Limit the max number of encoded fields.
         if (!klass->IsInitialized() &&
             (is_app_image || is_boot_image) &&
             is_superclass_initialized &&
+            not_too_many_encoded_fields &&
             manager_->GetCompiler()->IsImageClass(descriptor)) {
           bool can_init_static_fields = false;
           if (is_boot_image) {
