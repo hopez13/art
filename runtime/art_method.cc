72771489633ac66a555c56522c9abb4734024787
Roland Levillain <rpl@google.com>
2017-06-21 13:04:14 +0100

    Only look for method optimization annotations in the bootstrap class loader.
    When checking for an optimization annotation (FastNative or
    CriticalNative) on a method, do not resolve the method's annotations'
    classes as a side effect -- instead, look them up in the bootstrap
    class loader's resolved types. This is to prevent exceptions from being
    thrown (during class resolution) in JNI transitions.
    
    This change does not affect annotation lookup rules in the context of
    reflection.
    
    Bug: 38454151
    Bug: 34659969
    Change-Id: Ia88d5e0e02b5cb37ecff57f0bc1d40a6807b9be0

--- runtime/art_method.cc
+++ runtime/art_method.cc
@@ -402,16 +402,15 @@ bool ArtMethod::IsOverridableByDefaultMethod() {
 }
 
 bool ArtMethod::IsAnnotatedWithFastNative() {
-  return IsAnnotatedWith(WellKnownClasses::dalvik_annotation_optimization_FastNative,
-                         DexFile::kDexVisibilityBuild);
+  return IsAnnotatedWithOptimization(WellKnownClasses::dalvik_annotation_optimization_FastNative);
 }
 
 bool ArtMethod::IsAnnotatedWithCriticalNative() {
-  return IsAnnotatedWith(WellKnownClasses::dalvik_annotation_optimization_CriticalNative,
-                         DexFile::kDexVisibilityBuild);
+  return IsAnnotatedWithOptimization(
+      WellKnownClasses::dalvik_annotation_optimization_CriticalNative);
 }
 
-bool ArtMethod::IsAnnotatedWith(jclass klass, uint32_t visibility) {
+bool ArtMethod::IsAnnotatedWith(jclass klass, uint32_t visibility, bool allow_class_resolution) {
   Thread* self = Thread::Current();
   ScopedObjectAccess soa(self);
   StackHandleScope<1> shs(self);
@@ -420,10 +419,16 @@ bool ArtMethod::IsAnnotatedWith(jclass klass, uint32_t visibility) {
   DCHECK(annotation->IsAnnotation());
   Handle<mirror::Class> annotation_handle(shs.NewHandle(annotation));
 
-  // Note: Resolves any method annotations' classes as a side-effect.
+  // Note: If `allow_class_resolution` is true, resolve any of the
+  // method's annotations' classes as a side effect.
   // -- This seems allowed by the spec since it says we can preload any classes
   //    referenced by another classes's constant pool table.
-  return annotations::IsMethodAnnotationPresent(this, annotation_handle, visibility);
+  return annotations::IsMethodAnnotationPresent(
+      this, annotation_handle, visibility, allow_class_resolution);
+}
+
+bool ArtMethod::IsAnnotatedWithOptimization(jclass klass) {
+  return IsAnnotatedWith(klass, DexFile::kDexVisibilityBuild, /* allow_class_resolution */ false);
 }
 
 static uint32_t GetOatMethodIndexFromMethodIndex(const DexFile& dex_file,
