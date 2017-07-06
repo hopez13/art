543fb094ddbba0eefcb27500f56aa81a675837a2
Bharadwaj Kalandhabhatta <bharadwajsk@google.com>
2017-07-06 10:05:31 -0700

    Added more functionality to dex file tracking.
    Added functions that poison parts of String Data Items of Dex Files.
    
    Bug: 37754950
    Test: export ART_DEX_FILE_ACCESS_TRACKING=true && m -j && m -j
    SANITIZE_TARGET=address SANITIZE_LITE=true test-art-host
    
    Change-Id: Icfd3db36e52beef4631f3ffd221d0a019cdd5308

--- runtime/dex_file_tracking_registrar.h
+++ runtime/dex_file_tracking_registrar.h
@@ -54,6 +54,13 @@ class DexFileTrackingRegistrar {
   void SetAllInsnsRegistration(bool should_poison);
   // This function finds the code item of a class based on class name.
   void SetCodeItemRegistration(const char* class_name, bool should_poison);
+  void SetAllCodeItemStartRegistration(bool should_poison);
+
+  // Set of functions concerning String Data Items of dex_file_
+  void SetAllStringDataRegistration(bool should_poison);
+  // Sets the first byte of size value and data section of all string data
+  // items.
+  void SetAllStringDataStartRegistration(bool should_poison);
 
   // Contains tuples of all ranges of memory that need to be explicitly
   // (un)poisoned by the memory tool.
