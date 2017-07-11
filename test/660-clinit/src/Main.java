cf39c7d071fb6e318f1010843e0d6394a9f2a046
Chang Xing <chxing@google.com>
2017-07-11 11:58:01 -0700

    Support clinit for app image during compilation
    Enabled compiler driver to start initialization process for classes with
    static class initializers if debuggable flag is not setted because this
    might change the behaviour of interpreter (although invisible to user),
    e.g. the memory heap at startup will contains initialization results.
    
    Updated two testcases to reflect the change of heap memory before
    classes are initialized.
    
    Added one testcase 660-clinit to test whether class initializers are
    executed.
    
    This CL have to be submitted after /c/432328, /c/432154, /c/433242 and
    /c/433342 because without that four this will break the build. Besides,
    the other four CL will be tested in this CL.
    
    Test: make test-art-host -j64
    Change-Id: Ia25b9e18bdcd0fa3619be0058d459651f3b9a492

---
+++ test/660-clinit/src/Main.java
@@ -0,0 +1,202 @@
+/*
+ * Copyright 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    System.loadLibrary(args[0]);
+
+    if (!checkAppImageLoaded()) {
+      System.out.println("AppImage not loaded.");
+    }
+
+    if (!checkAppImageContains(ClInit.class)) {
+      System.out.println("ClInit class is not in app image!");
+    }
+
+    ShouldInit(ClInit.class);
+    ShouldInit(A.class);
+    ShouldNotInit(D.class);
+    ShouldInit(E.class);
+    ShouldNotInit(ClinitE.class);
+
+    ShouldNotInit(G.class);
+    ShouldNotInit(Gs.class);
+
+    Gs gs = new Gs();
+
+    A x = new A();
+    System.out.println("A.a: " + A.a);
+
+    ClInit c = new ClInit();
+    int aa = c.a;
+
+    System.out.println("X: " + c.getX());
+    System.out.println("Y: " + c.getY());
+    System.out.println("str: " + c.str);
+    System.out.println("ooo: " + c.ooo);
+    System.out.println("Z: " + c.getZ());
+    System.out.println("A: " + c.getA());
+    System.out.println("AA: " + aa);
+    if (c.arr.get(0) + c.arr.get(1) != 1000) {
+      System.out.println("arr not right.");
+    }
+
+    if (c.a != 101) {
+      System.out.println("a != 101");
+    }
+
+    try {
+      ClinitE e = new ClinitE();
+    } catch (Error err) { }
+
+    return;
+  }
+
+  static void ShouldInit(Class<?> klass) {
+    if (checkInitialized(klass) == false) {
+      System.out.println(klass.getName() + " should be initialized!");
+    }
+  }
+
+  static void ShouldNotInit(Class<?> klass) {
+    if (checkInitialized(klass) == true) {
+      System.out.println(klass.getName() + " should not be initialized!");
+    }
+  }
+
+  public static native boolean checkAppImageLoaded();
+  public static native boolean checkAppImageContains(Class<?> klass);
+  public static native boolean checkInitialized(Class<?> klass);
+}
+
+enum Day {
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
+    THURSDAY, FRIDAY, SATURDAY
+}
+
+class ClInit {
+
+  static String ooo = "OoooooO";
+  static String str;
+  static int z;
+  int x, y;
+  public static volatile int a = 100;
+  static ArrayList<Integer> arr;
+  static HashMap<Integer, String> map;
+
+  static {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Hello ");
+    sb.append("World!");
+    str = sb.toString();
+  }
+
+  static {
+    z = 0xFF;
+    z += 0xFF00;
+    z += 0xAA0000;
+  }
+
+  static {
+    arr = new ArrayList<Integer>(3);
+    arr.add(new Integer(1));
+    arr.add(new Integer(999));
+
+    map = new HashMap<>();
+    map.put(1, "1.00");
+    map.put(2, "2.00");
+    map.put(3, "3.00");
+  }
+
+  {
+    for(int i = 0; i < 100; i++) {
+      x += i;
+    }
+  }
+
+  {
+    y = this.x;
+    for(int i = 0; i < 40; i++) {
+      y += i;
+    }
+  }
+
+  ClInit() {
+
+  }
+
+  int getX() {
+    return x;
+  }
+
+  int getZ() {
+    return z;
+  }
+
+  int getY() {
+    return y;
+  }
+
+  int getA() {
+    return a;
+  }
+}
+
+class A {
+  public static int a = 2;
+  static {
+    a = 5;
+  }
+}
+
+class D {
+  static int d;
+  static {
+    d = E.e; // fail here
+  }
+}
+
+class E {
+  public static final int e;
+  static {
+    e = 100;
+  }
+}
+
+class G {
+  static G g;
+  static int i;
+  static {
+    g = new Gs();
+    i = A.a;
+  }
+}
+
+class Gs extends G { }
+
+class ClinitE {
+  static {
+    if (Math.sin(3) < 0.5) {
+      // throw anyway, can't initialized
+      throw new ExceptionInInitializerError("Can't initialize this class!");
+    }
+  }
+}
+
+
