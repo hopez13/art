2a9f143284ddc94f0f720b0df3ef260362c85267
Chang Xing <chxing@google.com>
2017-07-13 17:51:52 -0700

    Fix unexpected circular dependency check
    The PruneObjectReferenceVisitor, responsible for traversal instances of
    a class, to avoid circular dependenceis which makes infinite recursion,
    should only record an object received if it's not a ClassClass, because
    the processing of ClassClass objects will be forwarded to
    PruneAppImageClassInternal, which will first check circular
    dependencies.
    
    Test: make test-art-host -j64
    Change-Id: Ib056d3fcd02135992b09f7d28176da3a1173d42e

--- test/660-clinit/src/Main.java
+++ test/660-clinit/src/Main.java
@@ -33,6 +33,10 @@ public class Main {
     expectNotPreInit(G.class); // should fail
     expectNotPreInit(Gs.class); // should fail
 
+    expectNotPreInit(Add.class);
+    expectNotPreInit(Mul.class);
+    expectNotPreInit(ObjectRef.class);
+
     A x = new A();
     System.out.println("A.a: " + A.a);
 
@@ -163,3 +167,22 @@ class G {
 // Instantiation of Gs will fail because we try to invoke G's <init>
 // but G's status will be StatusVerified. INVOKE_DIRECT will not initialize class.
 class Gs extends G { }  // fail because super class can't be initialized
+
+// pruned because holding reference to non-image class
+class ObjectRef {
+  static Class<?> klazz[] = new Class<?>[]{Add.class, Mul.class};
+}
+
+// non-image
+class Add {
+  static int exec(int a, int b) {
+    return a + b;
+  }
+}
+
+// non-image
+class Mul {
+  static int exec(int a, int b) {
+    return a * b;
+  }
+}
