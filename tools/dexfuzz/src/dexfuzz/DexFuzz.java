f11af18aa3fcef6fad8ccc9fb6e74a632e6293d2
Sumnima Joshi <sumnima@google.com>
2017-07-14 11:52:23 -0700

    A Mutation that changes the length of an array.
    This mutation takes an array and changes its length.
    
    Test: The mutation did not show up while running dexfuzz.
    Change-Id: I5078b678673af7a839208c79113f1c1f26090baa

--- tools/dexfuzz/src/dexfuzz/DexFuzz.java
+++ tools/dexfuzz/src/dexfuzz/DexFuzz.java
@@ -34,7 +34,8 @@ import dexfuzz.listeners.UpdatingConsoleListener;
  */
 public class DexFuzz {
   private static int majorVersion = 1;
-  private static int minorVersion = 3;
+  // Last version update: added array size mutator.
+  private static int minorVersion = 4;
   private static int seedChangeVersion = 0;
 
   /**
