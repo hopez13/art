347dbe01534c3ab6539746c9f0c3215941cba6d5
Calin Juravle <calin@google.com>
2017-07-18 16:51:51 -0700

    Change kMultiDexSeparator from ':' to '!'
    The ':' separator is commonly used to separate class path elements. That
    means that we cannot easily encode multidex location in a classpath
    without complicating the parsing logic unnecessarily (e.g. when encoding
    classpaths in the oat file).
    
    For easy parsing and understanding kMultiDexSeparator and
    kClassPathSeparator should have different values. ':' is a wide spread
    classpath separator so this CL changes the value of kMultiDexSeparator to
    '!' which is also commonly used to denote an object inside a given
    container.
    
    Test: m test-art-host
    Bug: 38138251
    Change-Id: I30995c553d9131478c6c071b27327df6d2de06a7

--- runtime/oat.h
+++ runtime/oat.h
@@ -32,7 +32,7 @@ class InstructionSetFeatures;
 class PACKED(4) OatHeader {
  public:
   static constexpr uint8_t kOatMagic[] = { 'o', 'a', 't', '\n' };
-  // Last oat version changed reason: update classpath key format.
+  // Last oat version changed reason: update kMultiDexSeparator from ':' to '!'.
   static constexpr uint8_t kOatVersion[] = { '1', '2', '8', '\0' };
 
   static constexpr const char* kImageLocationKey = "image-location";
