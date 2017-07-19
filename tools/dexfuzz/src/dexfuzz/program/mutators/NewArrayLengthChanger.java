58eef6e140f60962df49d9050027760aa20e6fd3
Sumnima Joshi <sumnima@google.com>
2017-07-19 10:36:51 -0700

    Assign temporary register to the size of an array.
    Temporary register was added so that the value of existing registers
    could remain the same.
    
    Test: Dexfuzz was run.
    Before
    000144: 1300 0800                  |0000: const/16 v0, #int 8 // #8
    000148: 2300 0300                  |0002: new-array v0, v0, [I // type@0003
    00014c: 2600 0400 0000             |0004: fill-array-data v0, 00000008 // +00000004
    000152: 1100                       |0007: return-object v0
    
    After
    000144: 0900 0100 0200             |0000: move-object/16 v1, v2
    00014a: 1300 1052                  |0003: const/16 v0, #int 21008 // #5210
    00014e: 1302 1800                  |0005: const/16 v2, #int 24 // #18
    000152: 2320 0300                  |0007: new-array v0, v2, [I // type@0003
    000156: 2600 0500 0000             |0009: fill-array-data v0, 0000000e // +00000005
    00015c: 1102                       |000c: return-object v2
    
    Change-Id: I0d8380ab3facd6256bc46931929d32763bad4476

--- tools/dexfuzz/src/dexfuzz/program/mutators/NewArrayLengthChanger.java
+++ tools/dexfuzz/src/dexfuzz/program/mutators/NewArrayLengthChanger.java
@@ -28,8 +28,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-// This mutation might change the length of an array but can also change the
-// value of the register in every place it is used.
 public class NewArrayLengthChanger extends CodeMutator {
   /**
    * Every CodeMutator has an AssociatedMutation, representing the
@@ -116,20 +114,38 @@ public class NewArrayLengthChanger extends CodeMutator {
     MutatableCode mutatableCode = mutation.mutatableCode;
     MInsn newArrayInsn = newArrayLengthInsns.get(mutation.newArrayToChangeIdx);
     int newArrayInsnIdx = mutatableCode.getInstructionIndex(newArrayInsn);
+    if (newArrayInsnIdx < 0) {
+      newArrayInsnIdx = scanNewArray(mutatableCode);
+    }
+    if (newArrayInsnIdx == -1) {
+      return;
+    }
 
     MInsn newInsn = new MInsn();
     newInsn.insn = new Instruction();
     newInsn.insn.info = Instruction.getOpcodeInfo(Opcode.CONST_16);
+    mutatableCode.allocateTemporaryVRegs(1);
+    newArrayInsn.insn.vregB = mutatableCode.getTemporaryVReg(0);
     newInsn.insn.vregA = (int) newArrayInsn.insn.vregB;
     // New length chosen randomly between 1 to 100.
     newInsn.insn.vregB = rng.nextInt(100);
     mutatableCode.insertInstructionAt(newInsn, newArrayInsnIdx);
     Log.info("Changed the length of the array to " + newInsn.insn.vregB);
     stats.incrementStat("Changed length of new array");
+    mutatableCode.finishedUsingTemporaryVRegs();
   }
 
   private boolean isNewArray(MInsn mInsn) {
     Opcode opcode = mInsn.insn.info.opcode;
     return opcode == Opcode.NEW_ARRAY;
+  }
+
+  private int scanNewArray(MutatableCode mutatableCode) {
+    for (MInsn mInsn : mutatableCode.getInstructions()){
+      if (isNewArray(mInsn)){
+        return mutatableCode.getInstructionIndex(mInsn);
+      }
+    }
+    return -1;
   }
 }
