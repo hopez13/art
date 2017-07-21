67acdee0b32efd3b9608a44f1604758321b6f1da
Chang Xing <chxing@google.com>
2017-07-21 12:21:26 -0700

    Support clinit for app image during compilation
    Enabled compiler driver to start initialization process for classes with
    static class initializers if debuggable flag is not setted because this
    might change the behaviour of interpreter (although invisible to user),
    e.g. the memory heap at startup will contains initialization results.
    
    Updated two testcases to reflect the change of heap memory before
    classes are initialized.
    
    Enabled testcase 660-clinit to test whether class initializers are
    executed.
    
    This CL have to be submitted after /c/432328, /c/432154, /c/433242 and
    /c/433342 because without that four this will break the build. Besides,
    the other four CL will be tested in this CL.
    
    Test: make test-art-host -j64
    Change-Id: Ia25b9e18bdcd0fa3619be0058d459651f3b9a492

--- compiler/driver/compiler_driver.cc
+++ compiler/driver/compiler_driver.cc
@@ -2314,10 +2314,13 @@ class InitializeClassVisitor : public CompilationVisitor {
             CHECK(is_app_image);
             // The boot image case doesn't need to recursively initialize the dependencies with
             // special logic since the class linker already does this.
+            // Optimization will be disabled in debuggable build, because in debuggable mode we
+            // want the <clinit> behavior to be observable for the debugger, so we don't do the
+            // <clinit> at compile time.
             can_init_static_fields =
+                !manager_->GetCompiler()->GetCompilerOptions().GetDebuggable() &&
                 !soa.Self()->IsExceptionPending() &&
-                is_superclass_initialized &&
-                NoClinitInDependency(klass, soa.Self(), &class_loader);
+                is_superclass_initialized;
             // TODO The checking for clinit can be removed since it's already
             // checked when init superclass. Currently keep it because it contains
             // processing of intern strings. Will be removed later when intern strings
@@ -2331,6 +2334,11 @@ class InitializeClassVisitor : public CompilationVisitor {
             // a ReaderWriterMutex but we're holding the mutator lock so we fail mutex sanity
             // checks in Thread::AssertThreadSuspensionIsAllowable.
             Runtime* const runtime = Runtime::Current();
+            // Resolve the exception type before enabling the transaction in case the transaction
+            // aborts and cannot resolve the type.
+            manager_->GetClassLinker()->FindClass(Thread::Current(),
+                                                  Transaction::kAbortExceptionSignature,
+                                                  class_loader);
             // Run the class initializer in transaction mode.
             runtime->EnterTransactionMode(is_app_image, klass.Get());
             bool success = manager_->GetClassLinker()->EnsureInitialized(soa.Self(), klass, true,
@@ -2368,10 +2376,12 @@ class InitializeClassVisitor : public CompilationVisitor {
               }
             }
 
-            if (!success) {
+            if (!success && is_boot_image) {
               // On failure, still intern strings of static fields and seen in <clinit>, as these
               // will be created in the zygote. This is separated from the transaction code just
               // above as we will allocate strings, so must be allowed to suspend.
+              // We only need to intern strings for boot image because classes that failed to be
+              // initialized will not appear in app image.
               if (&klass->GetDexFile() == manager_->GetDexFile()) {
                 InternStrings(klass, class_loader);
               } else {
