ef190356867d0891797123ba40feffdf3bd5777e
Shubham Ajmera <shubhamajmera@google.com>
2017-07-26 13:20:41 -0700

    Add script to generate CMakeLists files used for CLion setup
    Test: manual
    Change-Id: Ifd619b80395571e0d9f81767df065a8fb0febdbb

---
+++ tools/generate_cmake_lists.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+#
+# Copyright 2017, The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""
+The primary objective of this file is to generate CMakeLists files
+for Clion setup.
+"""
+
+import sys
+import os
+import subprocess
+
+def _get_android_build_top():
+  path_to_top = os.environ.get('ANDROID_BUILD_TOP')
+  if not path_to_top:
+    # nothing set. try to guess it based on the relative path of this env.py file.
+    this_file_path = os.path.realpath(__file__)
+    path_to_top = os.path.join(os.path.dirname(this_file_path), '../..')
+    path_to_top = os.path.realpath(path_to_top)
+
+  if not os.path.exists(os.path.join(path_to_top, 'build/envsetup.sh')):
+    raise AssertionError("env.py must be located inside an android source tree")
+
+  return path_to_top
+
+project_name = "native"
+if (len(sys.argv) > 1):
+  project_name = sys.argv[1]
+
+os.environ['SOONG_GEN_CMAKEFILES']='1'
+os.environ['SOONG_GEN_CMAKEFILES_DEBUG']='1'
+
+ANDROID_BUILD_TOP = _get_android_build_top()
+
+subprocess.check_output(('make -j64 -C %s') % (ANDROID_BUILD_TOP), shell=True)
+
+out_art_cmakelists_dir = os.path.join(ANDROID_BUILD_TOP,
+                                      'out/development/ide/clion/art')
+
+cmake_sub_dirs = set()
+for root, dirs, files in os.walk(out_art_cmakelists_dir):
+  for name in files:
+    if name == 'CMakeLists.txt':
+      if (os.path.samefile(root, out_art_cmakelists_dir)):
+        continue
+      if 'arm64' not in root:
+        # arm64 is chosen randomly. We need to filter out only one architecture
+        # for CMakeLists to avoid collisions.
+        continue
+      cmake_sub_dir = cmake_sub_dirs.add(root.replace(out_art_cmakelists_dir,
+                                                      '.'))
+
+
+# Now generating CMakeLists.txt
+
+f = open(os.path.join(out_art_cmakelists_dir, 'CMakeLists.txt'), 'w')
+f.write('cmake_minimum_required(VERSION 3.6)\n')
+f.write('project(%s)\n' % (project_name))
+
+
+for dr in cmake_sub_dirs:
+  f.write('add_subdirectory(%s)\n' % (dr))
