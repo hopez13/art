5f6c52656ac1c43318eebc02c7fb147b89f39298
Bharadwaj Kalandhabhatta <bharadwajsk@google.com>
2017-08-10 19:32:58 -0700

    Changed Profile and Dexlayout to use Class Init
    Added information to the profile which will take runtime class
    initialization into account. Added functionality to Dexlayout which will
    use this new information to push classes that were initialized together
    and strings associated with these classes together. Added bug fixes to
    other scripts.
    
    Bug: 37754950
    Test: mm test-art-host
    Change-Id: I2bc1ce24636ca51aac099e2f9d9dea90f900edd3

--- runtime/jit/profile_compilation_info_test.cc
+++ runtime/jit/profile_compilation_info_test.cc
@@ -83,10 +83,11 @@ class ProfileCompilationInfoTest : public CommonRuntimeTest {
   bool AddClass(const std::string& dex_location,
                 uint32_t checksum,
                 dex::TypeIndex type_index,
-                ProfileCompilationInfo* info) {
+                ProfileCompilationInfo* info,
+                bool initialized = false) {
     DexCacheResolvedClasses classes(dex_location, dex_location, checksum, kMaxMethodIds);
     classes.AddClass(type_index);
-    return info->AddClasses({classes});
+    return info->AddClasses({classes}, initialized);
   }
 
   uint32_t GetFd(const ScratchFile& file) {
@@ -400,6 +401,35 @@ TEST_F(ProfileCompilationInfoTest, SaveMaxMethods) {
   for (uint16_t i = 0; i < std::numeric_limits<uint16_t>::max(); i++) {
     ASSERT_TRUE(AddClass("dex_location1", /* checksum */ 1, dex::TypeIndex(i), &saved_info));
     ASSERT_TRUE(AddClass("dex_location2", /* checksum */ 2, dex::TypeIndex(i), &saved_info));
+  }
+
+  ASSERT_TRUE(saved_info.Save(GetFd(profile)));
+  ASSERT_EQ(0, profile.GetFile()->Flush());
+
+  // Check that we get back what we saved.
+  ProfileCompilationInfo loaded_info;
+  ASSERT_TRUE(profile.GetFile()->ResetOffset());
+  ASSERT_TRUE(loaded_info.Load(GetFd(profile)));
+  ASSERT_TRUE(loaded_info.Equals(saved_info));
+}
+
+TEST_F(ProfileCompilationInfoTest, SaveMaxMethodsAndInitializedClasses) {
+  ScratchFile profile;
+
+  ProfileCompilationInfo saved_info;
+  // Save the maximum number of methods
+  for (uint16_t i = 0; i < std::numeric_limits<uint16_t>::max(); i++) {
+    ASSERT_TRUE(AddMethod("dex_location1", /* checksum */ 1, /* method_idx */ i, &saved_info));
+    ASSERT_TRUE(AddMethod("dex_location2", /* checksum */ 2, /* method_idx */ i, &saved_info));
+  }
+  // Save the maximum number of classes
+  for (uint16_t i = 0; i < std::numeric_limits<uint16_t>::max(); i++) {
+    ASSERT_TRUE(AddClass("dex_location1", /* checksum */ 1, dex::TypeIndex(i), &saved_info));
+    ASSERT_TRUE(AddClass(
+        "dex_location1", /* checksum */ 1, dex::TypeIndex(i), &saved_info, /* initialized */ true));
+    ASSERT_TRUE(AddClass("dex_location2", /* checksum */ 2, dex::TypeIndex(i), &saved_info));
+    ASSERT_TRUE(AddClass(
+        "dex_location2", /* checksum */ 2, dex::TypeIndex(i), &saved_info, /* initialized */ true));
   }
 
   ASSERT_TRUE(saved_info.Save(GetFd(profile)));
