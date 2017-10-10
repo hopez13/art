4e92adb09e55cbdadb124ab641079594db360fab
Orion Hodson <oth@google.com>
2017-10-10 14:47:32 +0100

    ART: Intrinsify polymorphic signature methods
    Adds VarHandle accessor method to list of intrinsics.
    
    Adds code to interpreter to ensure intrinsics with polymorphic
    signatures are initialized.
    
    Rename most uses of InvokePolymorphic to InvokeMethodHandle (and
    similar changes) to be clear that the particular code path applies to
    MethodHandle instances rather than VarHandle.
    
    Change-Id: Ib74865124a1e986badc0a7c4bb3d782af07225d4
    Bug: 65872996
    Test: art/test.py --host

--- runtime/intrinsics_enum.h
+++ runtime/intrinsics_enum.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 The Android Open Source Project
+ * Copyright (C) 2016 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,25 +14,22 @@
  * limitations under the License.
  */
 
-#ifndef ART_RUNTIME_INVOKE_TYPE_H_
-#define ART_RUNTIME_INVOKE_TYPE_H_
-
-#include <iosfwd>
+#ifndef ART_RUNTIME_INTRINSICS_ENUM_H_
+#define ART_RUNTIME_INTRINSICS_ENUM_H_
 
 namespace art {
 
-enum InvokeType : uint32_t {
-  kStatic,       // <<static>>
-  kDirect,       // <<direct>>
-  kVirtual,      // <<virtual>>
-  kSuper,        // <<super>>
-  kInterface,    // <<interface>>
-  kPolymorphic,  // <<polymorphic>>
-  kMaxInvokeType = kPolymorphic
+enum class Intrinsics {
+#define OPTIMIZING_INTRINSICS(Name, ...) \
+  k ## Name,
+#include "intrinsics_list.h"
+  kNone,
+  INTRINSICS_LIST(OPTIMIZING_INTRINSICS)
+#undef INTRINSICS_LIST
+#undef OPTIMIZING_INTRINSICS
 };
-
-std::ostream& operator<<(std::ostream& os, const InvokeType& rhs);
+std::ostream& operator<<(std::ostream& os, const Intrinsics& intrinsic);
 
 }  // namespace art
 
-#endif  // ART_RUNTIME_INVOKE_TYPE_H_
+#endif  // ART_RUNTIME_INTRINSICS_ENUM_H_
