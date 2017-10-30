1c4fbdf2fe88670ac6660675bf68d48730caae05
Roland Levillain <rpl@google.com>
2017-10-30 13:28:28 +0000

    Move the ART Buildbot target harness to a chroot-based environment.
    Instead of having the ART Buildbot install ART into a local path
    (set with ART_TEST_ANDROID_ROOT) and hijack the linker (using
    CUSTOM_TARGET_LINKER), prepare and use a chroot environment (set
    with ART_TEST_CHROOT).
    
    This means ART and its dependencies can be built and used normally
    (in particular, when building from the master-art Android manifest)
    and still be executed as a standalone runtime outside of /system,
    for testing purposes.
    
    TODO: Provide a cleaner upgrade path, where the old local-path
    model (based on ART_TEST_ANDROID_ROOT) is still used on the ART
    Buidbot, but allowing a switch to the new chroot-based model when
    ART_TEST_CHROOT is defined. We can then decide to clean up scripts and
    remove references to ART_TEST_ANDROID_ROOT.
    
    TODO: Remove all uses of CUSTOM_TARGET_LINKER in ART (and elsewhere),
    maybe in another CL.
    
    TODO: Update building and testing recipes on the ART Buildbot.
    
    Test: Have the ART Buildbot build and run ART on device with chroot.
    Bug: 34729697
    Bug: 68125496
    Change-Id: I08f1acd0d2813584f703fedb84e69df954cbdbda

---
+++ tools/cleanup-buildbot-device.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Copyright (C) 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+green='\033[0;32m'
+nc='\033[0m'
+
+# Setup as root, as device cleanup requires it.
+adb root
+adb wait-for-device
+
+# Check that ART_TEST_CHROOT is correctly defined.
+[[ -n "$ART_TEST_CHROOT" ]] || { echo "$ART_TEST_CHROOT is not defined"; exit 1; }
+[[ "x$ART_TEST_CHROOT" = x/* ]] || { echo "$ART_TEST_CHROOT is not an absolute path"; exit 1; }
+
+echo -e "${green}Clean up /system in chroot${nc}"
+adb shell rm -rf "$ART_TEST_CHROOT/system"
+
+echo -e "${green}Clean up some subdirs in /data in chroot${nc}"
+adb shell rm -rf \
+  "$ART_TEST_CHROOT/data/art-test" \
+  "$ART_TEST_CHROOT/data/nativetest" \
+  "$ART_TEST_CHROOT/data/nativetest64" \
+  "$ART_TEST_CHROOT/data/run-test" \
+  "$ART_TEST_CHROOT/data/dalvik-cache/*" \
+  "$ART_TEST_CHROOT/data/misc/trace/*"
