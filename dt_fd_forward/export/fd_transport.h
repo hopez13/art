07c953a9b2c338c9bd53b26da809a0978e549ad4
Alex Light <allight@google.com>
2017-11-07 16:03:39 -0800

    Add fd-forwarding transport lib
    dt_fd_forward is a jdwpTransport that takes as an address a local
    socket file-descriptor and uses it to allow some other piece of code
    or program to control the actual attach and communication process.
    
    tools/dt_fds_forward/dt_fds_forward.py is a python program that can be
    used as a controller for this comms system. This is useful for
    testing.
    
    The overall goal of this transport is to use it to allow us to safely
    multiplex the out-bound data with DDMS data. This is needed to let us
    match current DDMS behavior which transmits packets in the blind on
    the same channel JDWP traffic is sent on.
    
    Test: ./tools/dt_fds_forward.py  \
            ./test/run-test --host --dev 001-HelloWorld
    Test: jdb -attach localhost:12345
    
    Test: ./tools/dt_fds_forward.py  \
            --debug-lib $JAVA_HOME/jre/lib/amd64/libjdwp.so \
            -- \
            ./test/run-test --host --jvm --dev 001-HelloWorld
    Test: jdb -attach localhost:12345
    
    Bug: 62821960
    Bug: 69169846
    
    Change-Id: I654db6c6991c006933e1e1f0a4e41c13f795f9a8

---
+++ dt_fd_forward/export/fd_transport.h
@@ -0,0 +1,45 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef ART_DT_FD_FORWARD_EXPORT_FD_TRANSPORT_H_
+#define ART_DT_FD_FORWARD_EXPORT_FD_TRANSPORT_H_
+
+#include <stdint.h>
+
+namespace dt_fd_forward {
+
+// The file-descriptors sent over a socket to the dt_fd_forward transport.
+struct FdSet {
+  // A fd that can be read from which provides the JDWP data.
+  int read_fd_;
+
+  // A fd that can be written to in order to provide JDWP responses and events.
+  int write_fd_;
+
+  // A eventfd that can be locked to ensure that writes to write_fd_ are atomic. This must be held
+  // when writing to write_fd_. This allows the proxy to insert packets into the response stream
+  // without having to parse it.
+  int write_lock_fd_;
+};
+
+// This message is sent over the fd associated with the transport when we are listening for fds.
+static constexpr char kListenStartMessage[] = "dt_fd_forward:START-LISTEN";
+
+// This message is sent over the fd associated with the transport when we stop listening for fds.
+static constexpr char kListenEndMessage[] = "dt_fd_forward:END-LISTEN";
+
+}  // namespace dt_fd_forward
+
+#endif  // ART_DT_FD_FORWARD_EXPORT_FD_TRANSPORT_H_
