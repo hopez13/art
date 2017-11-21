be2e6ab8f0094382595208551bf3490fc366db18
Alex Light <allight@google.com>
2017-11-21 14:05:04 -0800

    Add remaining DDMS messages into DdmPublishChunk
    This ensures that one can get notified of every DDMS chunk that would
    be published.
    
    Add a test to ensure that appropriate ddms events are sent.
    
    Test: ./test.py --host -j50
    Bug: 62821960
    Change-Id: I0c4a60b75615c206a27aff17df853b53cf5e8c96

--- test/1940-ddms-ext/src-art/art/Test1940.java
+++ test/1940-ddms-ext/src-art/art/Test1940.java
@@ -30,6 +30,12 @@ public class Test1940 {
   public static final int MY_DDMS_TYPE = 0xDEADBEEF;
   public static final int MY_DDMS_RESPONSE_TYPE = 0xFADE7357;
 
+  public static final boolean PRINT_ALL_CHUNKS = false;
+
+  public static interface DdmHandler {
+    public void HandleChunk(int type, byte[] data);
+  }
+
   public static final class TestError extends Error {
     public TestError(String s) { super(s); }
   }
@@ -69,11 +75,33 @@ public class Test1940 {
 
   public static final ChunkHandler SINGLE_HANDLER = new MyDdmHandler();
 
+  public static DdmHandler CURRENT_HANDLER;
+
   public static void HandlePublish(int type, byte[] data) {
-    System.out.println("Chunk published: " + printChunk(new Chunk(type, data, 0, data.length)));
+    CURRENT_HANDLER.HandleChunk(type, data);
+  }
+
+  // TYPE Thread Create
+  public static final int TYPE_THCR = 0x54484352;
+  // Type Thread name
+  public static final int TYPE_THNM = 0x54484E4D;
+  // Type Thread death.
+  public static final int TYPE_THDE = 0x54484445;
+
+  // Type Heap info
+  public static final int TYPE_HPIF = 0x48504946;
+
+  public static boolean IsFromThread(Thread t, byte[] data) {
+    // DDMS always puts the thread-id as the first 4 bytes.
+    ByteBuffer b = ByteBuffer.wrap(data);
+    b.order(ByteOrder.BIG_ENDIAN);
+    return b.getInt() == (int) t.getId();
   }
 
   public static void run() throws Exception {
+    CURRENT_HANDLER = (type, data) -> {
+      System.out.println("Chunk published: " + printChunk(new Chunk(type, data, 0, data.length)));
+    };
     initializeTest(
         Test1940.class,
         Test1940.class.getDeclaredMethod("HandlePublish", Integer.TYPE, new byte[0].getClass()));
@@ -90,6 +118,54 @@ public class Test1940 {
         MY_DDMS_TYPE, new byte[] { 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10 }, 0, 8);
     System.out.println("Sending chunk: " + printChunk(c));
     DdmServer.sendChunk(c);
+
+    // Test thread chunks are sent.
+    final boolean[] types_seen = new boolean[] { false, false, false };
+    CURRENT_HANDLER = (type, cdata) -> {
+      switch (type) {
+        case TYPE_THCR:
+          types_seen[0] = true;
+          break;
+        case TYPE_THNM:
+          types_seen[1] = true;
+          break;
+        case TYPE_THDE:
+          types_seen[2] = true;
+          break;
+        default:
+          // We don't want to print other types.
+          break;
+      }
+      if (PRINT_ALL_CHUNKS) {
+        System.out.println(
+            "Unknown Chunk published: " + printChunk(new Chunk(type, cdata, 0, cdata.length)));
+      }
+    };
+    DdmVmInternal.threadNotify(true);
+    final Thread thr = new Thread(() -> { return; }, "THREAD");
+    thr.start();
+    thr.join();
+    DdmVmInternal.threadNotify(false);
+    // Make sure we saw at least one of Thread-create, Thread name, & thread death.
+    if (!types_seen[0] || !types_seen[1] || !types_seen[2]) {
+      System.out.println("Didn't see expected chunks for thread creation! got: " +
+          Arrays.toString(types_seen));
+    } else {
+      System.out.println("Saw expected thread events.");
+    }
+
+    // Test heap chunks are sent.
+    CURRENT_HANDLER = (type, cdata) -> {
+      // The actual data is far to noisy for this test.
+      // type = "HPIF"
+      if (type == TYPE_HPIF) {
+        System.out.println("Expected chunk type published: " + type);
+      }
+    };
+    final int HPIF_WHEN_NOW = 1;
+    if (!DdmVmInternal.heapInfoNotify(HPIF_WHEN_NOW)) {
+      System.out.println("Unexpected failure for heapInfoNotify!");
+    }
   }
 
   private static Chunk processChunk(byte[] val) {
