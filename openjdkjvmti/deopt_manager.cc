f59e750977f9d5d76bb3fc6968482296d89a279f
Alex Light <allight@google.com>
2017-12-18 16:42:36 -0800

    Support using adbconnection without JAVA_DEBUGGABLE
    We need to support using a best-effort JDWP when we have the
    ENABLE_JDWP attribute without the JAVA_DEBUGGABLE attribute. This is
    used on eng and userdebug builds. We do this by making the plugin try
    to change the runtime to debuggable if possible and to allow getting
    an ArtTiEnv which is a best-effort version of JVMTI by calling GetEnv
    with (JVMTI_VERSION_1_2 | 0x4000000).
    
    Bug: 62821960
    Test: Manual
    Test: Build, Test debugging system_server and other processes.
    Change-Id: I2233299fceb83c76785e5de09e51eaf18b7922e8

--- openjdkjvmti/deopt_manager.cc
+++ openjdkjvmti/deopt_manager.cc
@@ -68,7 +68,9 @@ bool JvmtiMethodInspectionCallback::IsMethodSafeToJit(art::ArtMethod* method) {
 }
 
 DeoptManager::DeoptManager()
-  : deoptimization_status_lock_("JVMTI_DeoptimizationStatusLock"),
+  : deoptimization_status_lock_("JVMTI_DeoptimizationStatusLock",
+                                static_cast<art::LockLevel>(
+                                    art::LockLevel::kClassLinkerClassesLock + 1)),
     deoptimization_condition_("JVMTI_DeoptimizationCondition", deoptimization_status_lock_),
     performing_deoptimization_(false),
     global_deopt_count_(0),
@@ -89,6 +91,26 @@ void DeoptManager::Shutdown() {
   art::ScopedSuspendAll ssa("remove method Inspection Callback");
   art::RuntimeCallbacks* callbacks = art::Runtime::Current()->GetRuntimeCallbacks();
   callbacks->RemoveMethodInspectionCallback(&inspection_callback_);
+}
+
+void DeoptManager::FinishSetup() {
+  art::Thread* self = art::Thread::Current();
+  art::MutexLock mu(self, deoptimization_status_lock_);
+
+  art::Runtime* runtime = art::Runtime::Current();
+  // See if we need to do anything.
+  if (!runtime->IsJavaDebuggable() &&
+      !runtime->GetInstrumentation()->IsForcedInterpretOnly() &&
+      !runtime->IsNativeDebuggable()) {
+    // See if we can enable all JVMTI functions. If this is false only kArtTiVersion agents can be
+    // retrieved and they will all be best-effort.
+    if (PhaseUtil::GetPhaseUnchecked() == JVMTI_PHASE_ONLOAD) {
+      // We are still early enough to change the compiler options and get full JVMTI support.
+      runtime->AddCompilerOption("--debuggable");
+      runtime->SetJavaDebuggable(true);
+    }
+    runtime->DeoptimizeBootImage();
+  }
 }
 
 bool DeoptManager::MethodHasBreakpoints(art::ArtMethod* method) {
