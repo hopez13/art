fad8821b713fd3ce3a7da24b2d25f18f692a5c0b
Alan Leung <acleung@google.com>
2017-12-20 13:33:21 -0800

    Make D8 Desugaring default.
    Bug: 69329508
    Test: m checkbuild
    Change-Id: I25218efd33885a0b8c11858ccabeb4cfce4302bf

--- build/art.go
+++ build/art.go
@@ -103,7 +103,7 @@ func globalFlags(ctx android.BaseContext) ([]string, []string) {
 		asflags = append(asflags, "-DART_MIPS32_CHECK_ALIGNMENT")
 	}
 
-	if envTrue(ctx, "USE_D8_DESUGAR") {
+	if !envFalse(ctx, "USE_D8_DESUGAR") {
 		cflags = append(cflags, "-DUSE_D8_DESUGAR=1")
 	}
 
