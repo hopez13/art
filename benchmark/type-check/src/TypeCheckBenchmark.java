9c727c6b7558021d91beb9af01994ad8cb68685c
Vladimir Marko <vmarko@google.com>
2018-01-10 18:26:38 +0000

    Compiler changes for bitstring based type checks.
    Boot image size for aosp_taimen-userdebug in AOSP master:
      - before:
        arm boot*.oat: 63604740
        arm64 boot*.oat: 74237864
      - after:
        arm boot*.oat: 63531172 (-72KiB, -0.1%)
        arm64 boot*.oat: 74135008 (-100KiB, -0.1%)
    
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing --jit
    Test: testrunner.py --host -t 670-bitstring-type-check
    Test: Pixel 2 XL boots.
    Test: testrunner.py --target --optimizing --jit
    Test: testrunner.py --target -t 670-bitstring-type-check
    Bug: 64692057
    Bug: 71853552
    Bug: 26687569
    Change-Id: I538d7e036b5a8ae2cc3fe77662a5903d74854562

---
+++ benchmark/type-check/src/TypeCheckBenchmark.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class TypeCheckBenchmark {
+    public void timeCheckCastLevel1ToLevel1(int count) {
+        for (int i = 0; i < count; ++i) {
+            Level1 l1 = (Level1) o1;
+        }
+    }
+
+    public void timeCheckCastLevel2ToLevel1(int count) {
+        for (int i = 0; i < count; ++i) {
+            Level1 l1 = (Level1) o2;
+        }
+    }
+
+    public void timeCheckCastLevel3ToLevel1(int count) {
+        for (int i = 0; i < count; ++i) {
+            Level1 l1 = (Level1) o3;
+        }
+    }
+
+    public void timeCheckCastLevel9ToLevel1(int count) {
+        for (int i = 0; i < count; ++i) {
+            Level1 l1 = (Level1) o9;
+        }
+    }
+
+    public void timeCheckCastLevel9ToLevel2(int count) {
+        for (int i = 0; i < count; ++i) {
+            Level2 l2 = (Level2) o9;
+        }
+    }
+
+    public void timeInstanceOfLevel1ToLevel1(int count) {
+        int sum = 0;
+        for (int i = 0; i < count; ++i) {
+            if (o1 instanceof Level1) {
+                ++sum;
+            }
+        }
+        result = sum;
+    }
+
+    public void timeInstanceOfLevel2ToLevel1(int count) {
+        int sum = 0;
+        for (int i = 0; i < count; ++i) {
+            if (o2 instanceof Level1) {
+                ++sum;
+            }
+        }
+        result = sum;
+    }
+
+    public void timeInstanceOfLevel3ToLevel1(int count) {
+        int sum = 0;
+        for (int i = 0; i < count; ++i) {
+            if (o3 instanceof Level1) {
+                ++sum;
+            }
+        }
+        result = sum;
+    }
+
+    public void timeInstanceOfLevel9ToLevel1(int count) {
+        int sum = 0;
+        for (int i = 0; i < count; ++i) {
+            if (o9 instanceof Level1) {
+                ++sum;
+            }
+        }
+        result = sum;
+    }
+
+    public void timeInstanceOfLevel9ToLevel2(int count) {
+        int sum = 0;
+        for (int i = 0; i < count; ++i) {
+            if (o9 instanceof Level2) {
+                ++sum;
+            }
+        }
+        result = sum;
+    }
+
+    volatile Object o1 = new Level1();
+    volatile Object o2 = new Level2();
+    volatile Object o3 = new Level3();
+    volatile Object o9 = new Level9();
+    int result;
+}
+
+class Level1 { }
+class Level2 extends Level1 { }
+class Level3 extends Level2 { }
+class Level4 extends Level3 { }
+class Level5 extends Level4 { }
+class Level6 extends Level5 { }
+class Level7 extends Level6 { }
+class Level8 extends Level7 { }
+class Level9 extends Level8 { }
--- compiler/optimizing/graph_checker.cc
+++ compiler/optimizing/graph_checker.cc
@@ -25,6 +25,11 @@
 #include "base/bit_vector-inl.h"
 #include "base/scoped_arena_allocator.h"
 #include "base/scoped_arena_containers.h"
+#include "handle.h"
+#include "mirror/class.h"
+#include "obj_ptr-inl.h"
+#include "scoped_thread_state_change-inl.h"
+#include "subtype_check.h"
 
 namespace art {
 
@@ -540,28 +545,79 @@ void GraphChecker::VisitReturnVoid(HReturnVoid* ret) {
   }
 }
 
-void GraphChecker::VisitCheckCast(HCheckCast* check) {
+void GraphChecker::HandleTypeCheckInstruction(HTypeCheckInstruction* check) {
   VisitInstruction(check);
   HInstruction* input = check->InputAt(1);
-  if (!input->IsLoadClass()) {
-    AddError(StringPrintf("%s:%d expects a HLoadClass as second input, not %s:%d.",
-                          check->DebugName(),
-                          check->GetId(),
-                          input->DebugName(),
-                          input->GetId()));
+  if (check->GetTypeCheckKind() == TypeCheckKind::kBitstringCheck) {
+    if (!input->IsNullConstant()) {
+      AddError(StringPrintf("%s:%d (bitstring) expects a HNullConstant as second input, not %s:%d.",
+                            check->DebugName(),
+                            check->GetId(),
+                            input->DebugName(),
+                            input->GetId()));
+    }
+    bool check_values = false;
+    BitString::StorageType expected_path_to_root = 0u;
+    BitString::StorageType expected_mask = 0u;
+    {
+      ScopedObjectAccess soa(Thread::Current());
+      ObjPtr<mirror::Class> klass = check->GetClass().Get();
+      MutexLock subtype_check_lock(Thread::Current(), *Locks::subtype_check_lock_);
+      SubtypeCheckInfo::State state = SubtypeCheck<ObjPtr<mirror::Class>>::GetState(klass);
+      if (state == SubtypeCheckInfo::kAssigned) {
+        expected_path_to_root =
+            SubtypeCheck<ObjPtr<mirror::Class>>::GetEncodedPathToRootForTarget(klass);
+        expected_mask = SubtypeCheck<ObjPtr<mirror::Class>>::GetEncodedPathToRootMask(klass);
+        check_values = true;
+      } else {
+        AddError(StringPrintf("%s:%d (bitstring) references a class with unassigned bitstring.",
+                              check->DebugName(),
+                              check->GetId()));
+      }
+    }
+    if (!check->InputAt(2)->IsIntConstant()) {
+      AddError(StringPrintf("%s:%d (bitstring) expects a HIntConstant as third input, not %s:%d.",
+                            check->DebugName(),
+                            check->GetId(),
+                            check->InputAt(2)->DebugName(),
+                            check->InputAt(2)->GetId()));
+    } else if (check_values && check->GetBitstringPathToRoot() != expected_path_to_root) {
+      AddError(StringPrintf("%s:%d (bitstring) has path_to_root 0x%x, not 0x%x as expected.",
+                            check->DebugName(),
+                            check->GetId(),
+                            check->GetBitstringPathToRoot(),
+                            expected_path_to_root));
+    }
+    if (!check->InputAt(3)->IsIntConstant()) {
+      AddError(StringPrintf("%s:%d (bitstring) expects a HIntConstant as fourth input, not %s:%d.",
+                            check->DebugName(),
+                            check->GetId(),
+                            check->InputAt(3)->DebugName(),
+                            check->InputAt(3)->GetId()));
+    } else if (check_values && check->GetBitstringMask() != expected_mask) {
+      AddError(StringPrintf("%s:%d (bitstring) has mask 0x%x, not 0x%x as expected.",
+                            check->DebugName(),
+                            check->GetId(),
+                            check->GetBitstringMask(),
+                            expected_mask));
+    }
+  } else {
+    if (!input->IsLoadClass()) {
+      AddError(StringPrintf("%s:%d (classic) expects a HLoadClass as second input, not %s:%d.",
+                            check->DebugName(),
+                            check->GetId(),
+                            input->DebugName(),
+                            input->GetId()));
+    }
   }
 }
 
+void GraphChecker::VisitCheckCast(HCheckCast* check) {
+  HandleTypeCheckInstruction(check);
+}
+
 void GraphChecker::VisitInstanceOf(HInstanceOf* instruction) {
-  VisitInstruction(instruction);
-  HInstruction* input = instruction->InputAt(1);
-  if (!input->IsLoadClass()) {
-    AddError(StringPrintf("%s:%d expects a HLoadClass as second input, not %s:%d.",
-                          instruction->DebugName(),
-                          instruction->GetId(),
-                          input->DebugName(),
-                          input->GetId()));
-  }
+  HandleTypeCheckInstruction(instruction);
 }
 
 void GraphChecker::HandleLoop(HBasicBlock* loop_header) {
