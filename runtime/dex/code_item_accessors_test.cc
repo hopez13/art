137db8dfdc0811e5698d1510d82375de518e312c
Mathieu Chartier <mathieuc@google.com>
2018-01-11 14:50:02 -0800

    Use efficient encoding for compact dex code item fields
    Use a format for compact dex code items. In the common case, there
    is only 4 bytes worth of fields.
    4 bits: register size, ins size, outs size, tries size
    11 bits: insns size
    5 bits: if there is a preheader extension for each field.
    
    Preheader may use up to 12 extra bytes. The preheader non empty for
    ~1% of code items. The current preheader encoding is suboptimal size
    wise but is meant to be fast and easy to decode.
    
    Reduces vdex size by an average of 4.3% on golem benchmarks.
    
    TODO: Measure interpreter performance.
    
    Test: test-art-host
    
    Bug: 63756964
    Change-Id: I836fd098063dd1573a3dfb6ac465a6d33d2cb2c9

--- runtime/dex/code_item_accessors_test.cc
+++ runtime/dex/code_item_accessors_test.cc
@@ -96,12 +96,16 @@ TEST(CodeItemAccessorsTest, TestDexInstructionsAccessor) {
   verify_code_item(standard_dex.get(), dex_code_item, dex_code_item->insns_);
 
   CompactDexFile::CodeItem* cdex_code_item =
-      reinterpret_cast<CompactDexFile::CodeItem*>(const_cast<uint8_t*>(compact_dex->Begin()));
-  cdex_code_item->registers_size_ = kRegisterSize;
-  cdex_code_item->ins_size_ = kInsSize;
-  cdex_code_item->outs_size_ = kOutsSize;
-  cdex_code_item->tries_size_ = kTriesSize;
-  cdex_code_item->insns_size_in_code_units_ = kInsnsSizeInCodeUnits;
+      reinterpret_cast<CompactDexFile::CodeItem*>(const_cast<uint8_t*>(compact_dex->Begin() +
+          CompactDexFile::CodeItem::kMaxPreHeaderSize * sizeof(uint16_t)));
+  std::vector<uint16_t> preheader;
+  cdex_code_item->Create(kRegisterSize,
+                         kInsSize,
+                         kOutsSize,
+                         kTriesSize,
+                         kInsnsSizeInCodeUnits,
+                         cdex_code_item->PreHeader());
+
   verify_code_item(compact_dex.get(), cdex_code_item, cdex_code_item->insns_);
 }
 
