ffc236b7e7b8c810d4355251341a46b6f9598053
David Sehr <sehr@google.com>
2018-02-26 16:43:04 -0800

    Header library to remove dependence on runtime/
    Add a new header library to remove libdexfile and others' dependence on
    runtime (typically runtime/base) includes in libdexfile.  Also a small step
    to tease dexlayout and profman away from relying on these as well.
    
    Bug: 22322814
    Test: make -j 50 checkbuild
          make -j 50 test-art-host-gtest
    
    Change-Id: I38e2fe399a75f4bc6318c77a71954c00ea73ec2b

--- dex2oat/linker/oat_writer.h
+++ dex2oat/linker/oat_writer.h
@@ -24,6 +24,8 @@
 
 #include "base/array_ref.h"
 #include "base/dchecked_vector.h"
+#include "base/safe_map.h"
+#include "compiler.h"
 #include "dex/compact_dex_level.h"
 #include "debug/debug_info.h"
 #include "linker/relative_patcher.h"  // For RelativePatcherTargetProvider.
@@ -32,7 +34,6 @@
 #include "mirror/class.h"
 #include "oat.h"
 #include "os.h"
-#include "safe_map.h"
 #include "string_reference.h"
 #include "type_reference.h"
 
