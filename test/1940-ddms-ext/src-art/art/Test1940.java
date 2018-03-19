531e684c0a82f8a98936ebe9f8f777af2039475d
Alex Light <allight@google.com>
2018-03-19 13:47:56 -0700

    Try to prevent stdout/stderr interleaving in test 1940
    In host configurations test 1940 at one point has some std-err output
    from native code. This is filtered out but if it ends up interleaving
    with the std-out output we will end up missing some of the output and
    incorrectly failing the test.
    
    Put in some 'flush' calls around where the printing takes place to
    make this not happen.
    
    Test: ./test.py --host -j50
    Change-Id: Ic42816beeea2f04046b5bb88e17a1f6bde19a58e

--- test/1940-ddms-ext/src-art/art/Test1940.java
+++ test/1940-ddms-ext/src-art/art/Test1940.java
@@ -156,6 +156,9 @@ public class Test1940 {
       res = processChunk(new Chunk(MY_INVALID_DDMS_TYPE, data, 0, 1));
       System.out.println("JVMTI returned chunk: " + printChunk(res));
     } catch (RuntimeException e) {
+      // Force a flush so that we won't get interspersed lines in output.
+      System.err.flush();
+      System.out.flush();
       System.out.println("Got error: " + e.getMessage());
     }
 
