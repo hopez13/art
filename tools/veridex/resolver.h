c79798d45ad483ef837cce6ec3e383676ec6fb11
Nicolas Geoffray <ngeoffray@google.com>
2018-03-21 11:00:14 +0000

    Add resolver to veridex.
    Resolver runs over dex file and cache classes/methods/fields.
    
    Change-Id: I0574891cd72b5aa72f97784c6391912fa8ed8c1c

---
+++ tools/veridex/resolver.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ART_TOOLS_VERIDEX_RESOLVER_H_
+#define ART_TOOLS_VERIDEX_RESOLVER_H_
+
+#include "dex/dex_file.h"
+#include "veridex.h"
+
+namespace art {
+
+class VeridexResolver {
+ public:
+  VeridexResolver(const DexFile& dex_file, TypeMap& type_map)
+      : dex_file_(dex_file),
+        type_map_(type_map) {
+    type_infos_.reset(new VeriClass[dex_file.NumTypeIds()]);
+    memset(type_infos_.get(), 0, dex_file.NumTypeIds() * sizeof(VeriClass));
+    method_infos_.reset(new VeriMethod[dex_file.NumMethodIds()]);
+    memset(method_infos_.get(), 0, dex_file.NumFieldIds() * sizeof(VeriMethod));
+    field_infos_.reset(new VeriField[dex_file.NumFieldIds()]);
+    memset(field_infos_.get(), 0, dex_file.NumFieldIds() * sizeof(VeriField));
+  }
+
+  void Run();
+
+ private:
+  const DexFile& dex_file_;
+  TypeMap& type_map_;
+  std::unique_ptr<VeriClass[]> type_infos_;
+  std::unique_ptr<VeriMethod[]> method_infos_;
+  std::unique_ptr<VeriField[]> field_infos_;
+};
+
+}  // namespace art
+
+#endif  // ART_TOOLS_VERIDEX_RESOLVER_H_
