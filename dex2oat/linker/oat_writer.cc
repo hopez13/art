2e812c84f02c6a734c79a3ebba9e2e68cd33aeb6
Mathieu Chartier <mathieuc@google.com>
2018-04-03 10:22:27 -0700

    Always do cdex conversion for OatWriter dexlayout
    Don't use DexLayout without CompactDex conversion in the case where
    a profile is passed in.
    
    Bug: 77498934
    Test: test-art-host
    Change-Id: I6c0498db71f1324402b8dfe2ab3ef9bbd121b4cc

--- dex2oat/linker/oat_writer.cc
+++ dex2oat/linker/oat_writer.cc
@@ -3673,7 +3673,11 @@ bool OatWriter::LayoutAndWriteDexFile(OutputStream* out, OatDexFile* oat_dex_fil
     return false;
   }
   Options options;
-  options.compact_dex_level_ = compact_dex_level_;
+  // If compact dex is enabled by default, always use it.
+  options.compact_dex_level_ = kDefaultCompactDexLevel;
+  if (compact_dex_level_ != CompactDexLevel::kCompactDexLevelNone) {
+    options.compact_dex_level_ = compact_dex_level_;
+  }
   options.update_checksum_ = true;
   DexLayout dex_layout(options, profile_compilation_info_, /*file*/ nullptr, /*header*/ nullptr);
   const uint8_t* dex_src = nullptr;
