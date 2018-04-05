16e5fbddfd948a84231aeb3a51a650c46f644ef3
Vladimir Marko <vmarko@google.com>
2018-04-05 17:59:39 +0100

    ARM64: Implicit suspend checks using LDR.
    Implement implicit suspend checks in compiled managed code.
    Use a single instruction `ldr x21, [x21, #0]` for the check
    where `x21` points to a field in `Thread` that points to
    itself until we request a checkpoint or suspension and set
    it to null. After the null becomes visible to a running
    thread, it requires two loads to get a segmentation fault
    that is intercepted and redirected to a suspend check.
    
    This involves a trade-off between the speed of a sinle
    suspend check (a single LDR is faster than LDR+TST+BEQ/BNE)
    and time to suspend where we now need to wait for two LDRs
    and incur fault handling overhead. The time to suspend was
    previously measured to be acceptable with the long tail
    being comparable to the explicit suspend check.
    
    TODO: Measure boot image size change.
    TODO: Add Golem benchmark results.
    
    Test: testrunner.py --target --optimizing --jit --interpreter --64
    Bug: 38383823
    Change-Id: I1827689a3fb7f3c38310b87c80c7724bd7364a66

--- compiler/optimizing/code_generator_arm64.cc
+++ compiler/optimizing/code_generator_arm64.cc
@@ -1976,6 +1976,15 @@ void CodeGeneratorARM64::GenerateMemoryBarrier(MemBarrierKind kind) {
 
 void InstructionCodeGeneratorARM64::GenerateSuspendCheck(HSuspendCheck* instruction,
                                                          HBasicBlock* successor) {
+  bool use_implicit_check =
+      codegen_->GetCompilerOptions().GetImplicitSuspendChecks() && !GetGraph()->HasSIMD();
+  if (use_implicit_check) {
+    DCHECK(instruction->IsSuspendCheck());
+    __ Ldr(kImplicitSuspendCheckRegister, MemOperand(kImplicitSuspendCheckRegister));
+    codegen_->RecordPcInfo(instruction, instruction->GetDexPc());
+    return;
+  }
+
   SuspendCheckSlowPathARM64* slow_path =
       down_cast<SuspendCheckSlowPathARM64*>(instruction->GetSlowPath());
   if (slow_path == nullptr) {
@@ -3566,14 +3575,20 @@ void InstructionCodeGeneratorARM64::HandleGoto(HInstruction* got, HBasicBlock* s
   HInstruction* previous = got->GetPrevious();
   HLoopInformation* info = block->GetLoopInformation();
 
+  bool use_implicit_check =
+      codegen_->GetCompilerOptions().GetImplicitSuspendChecks() && !GetGraph()->HasSIMD();
   if (info != nullptr && info->IsBackEdge(*block) && info->HasSuspendCheck()) {
     codegen_->MaybeIncrementHotness(/* is_frame_entry= */ false);
-    GenerateSuspendCheck(info->GetSuspendCheck(), successor);
-    return;
+    if (!use_implicit_check) {
+      GenerateSuspendCheck(info->GetSuspendCheck(), successor);
+      return;
+    }
   }
   if (block->IsEntryBlock() && (previous != nullptr) && previous->IsSuspendCheck()) {
-    GenerateSuspendCheck(previous->AsSuspendCheck(), nullptr);
-    codegen_->MaybeGenerateMarkingRegisterCheck(/* code= */ __LINE__);
+    if (!use_implicit_check) {
+      GenerateSuspendCheck(previous->AsSuspendCheck(), nullptr);
+      codegen_->MaybeGenerateMarkingRegisterCheck(/* code */ __LINE__);
+    }
   }
   if (!codegen_->GoesToNextBlock(block, successor)) {
     __ B(codegen_->GetLabelOf(successor));
@@ -6251,15 +6266,19 @@ void LocationsBuilderARM64::VisitSuspendCheck(HSuspendCheck* instruction) {
 }
 
 void InstructionCodeGeneratorARM64::VisitSuspendCheck(HSuspendCheck* instruction) {
-  HBasicBlock* block = instruction->GetBlock();
-  if (block->GetLoopInformation() != nullptr) {
-    DCHECK(block->GetLoopInformation()->GetSuspendCheck() == instruction);
-    // The back edge will generate the suspend check.
-    return;
-  }
-  if (block->IsEntryBlock() && instruction->GetNext()->IsGoto()) {
-    // The goto will generate the suspend check.
-    return;
+  bool use_implicit_check =
+      codegen_->GetCompilerOptions().GetImplicitSuspendChecks() && !GetGraph()->HasSIMD();
+  if (!use_implicit_check) {
+    HBasicBlock* block = instruction->GetBlock();
+    if (block->GetLoopInformation() != nullptr) {
+      DCHECK(block->GetLoopInformation()->GetSuspendCheck() == instruction);
+      // The back edge will generate the suspend check.
+      return;
+    }
+    if (block->IsEntryBlock() && instruction->GetNext()->IsGoto()) {
+      // The goto will generate the suspend check.
+      return;
+    }
   }
   GenerateSuspendCheck(instruction, nullptr);
   codegen_->MaybeGenerateMarkingRegisterCheck(/* code= */ __LINE__);
