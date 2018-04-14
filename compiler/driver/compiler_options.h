afd41b461fa90e00b8f8ec9cb10c342992e436f5
Andreas Gampe <agampe@google.com>
2018-04-13 17:28:34 -0700

    ART: Add profile-compile-check support
    Add --check-profiled-methods, which verifies that all methods mentioned
    in a profile for a guided compilation are actually compiled instead of
    being punted. As outcome it may log or abort dex2oat.
    
    TODO: Extend dex2oat_test
    
    Bug: 76145463
    Test: mmma art
    Test: m test-art-host
    Change-Id: I956113b55796d0666db9dbfd387105a7d27b0868

--- compiler/driver/compiler_options.h
+++ compiler/driver/compiler_options.h
@@ -43,6 +43,13 @@ class DexFile;
 enum class InstructionSet;
 class InstructionSetFeatures;
 
+// Enum for CheckProfileMethodsCompiled. Outside CompilerOptions so it can be forward-declared.
+enum class ProfileMethodsCheck : uint8_t {
+  kNone,
+  kLog,
+  kAbort,
+};
+
 class CompilerOptions final {
  public:
   // Guide heuristics to determine whether to compile method if profile data not available.
@@ -309,6 +316,10 @@ class CompilerOptions final {
     return count_hotness_in_compiled_code_;
   }
 
+  ProfileMethodsCheck CheckProfiledMethodsCompiled() const {
+    return check_profiled_methods_;
+  }
+
  private:
   bool ParseDumpInitFailures(const std::string& option, std::string* error_msg);
   void ParseDumpCfgPasses(const StringPiece& option, UsageFn Usage);
@@ -386,6 +397,10 @@ class CompilerOptions final {
   // Whether compiled code should increment the hotness count of ArtMethod. Note that the increments
   // won't be atomic for performance reasons, so we accept races, just like in interpreter.
   bool count_hotness_in_compiled_code_;
+
+  // When running profile-guided compilation, check that methods intended to be compiled end
+  // up compiled and are not punted.
+  ProfileMethodsCheck check_profiled_methods_;
 
   RegisterAllocator::Strategy register_allocation_strategy_;
 
