c28b0933ea368a55360fdbe3b4fd1bfd11a0a042
Aart Bik <ajcbik@google.com>
2018-04-17 14:56:29 -0700

    Deopt does not throw
    Rationale:
    "CanThrow" of deopt was possibly misused to prevents some
    optimizations. However, the instruction technically cannot
    throw an exception, and indeed crashed the graph verifier
    for some corner cases. This Cl sets that right.
    
    Bug: 29868356
    
    Test: test-art-host,target
    Change-Id: Icb551d3b2935282a70ad673a0544e4fe01104da1

--- compiler/optimizing/nodes.h
+++ compiler/optimizing/nodes.h
@@ -1272,8 +1272,6 @@ class HBasicBlock : public ArenaObject<kArenaAllocBasicBlock> {
   // the appropriate try entry will be returned.
   const HTryBoundary* ComputeTryEntryOfSuccessors() const;
 
-  bool HasThrowingInstructions() const;
-
   // Returns whether this block dominates the blocked passed as parameter.
   bool Dominates(HBasicBlock* block) const;
 
@@ -3238,7 +3236,9 @@ class HDeoptimize FINAL : public HVariableInputSizeInstruction {
 
   bool NeedsEnvironment() const OVERRIDE { return true; }
 
-  bool CanThrow() const OVERRIDE { return true; }
+  // Even though deoptimize is often used for "exceptional cases" to go back to
+  // the interpreter, it never throws an exception.
+  bool CanThrow() const OVERRIDE { return false; }
 
   DeoptimizationKind GetDeoptimizationKind() const { return GetPackedField<DeoptimizeKindField>(); }
 
