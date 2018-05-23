56f44bf53ba9e980138887fa4a98540f9c34618f
Nicolas Geoffray <ngeoffray@google.com>
2018-05-23 11:22:30 +0100

    Add sdk destinations for appcompat.sh related files.
    Test: m
    Change-Id: I9a41a8f2c5b0bd253e38f955204dc3f91956cc28

--- tools/veridex/Android.mk
+++ tools/veridex/Android.mk
@@ -16,6 +16,9 @@
 
 LOCAL_PATH := $(call my-dir)
 
+# The veridex tool takes stub dex files as input, so we generate both the system and oahl
+# dex stubs.
+
 system_stub_dex := $(TARGET_OUT_COMMON_INTERMEDIATES)/PACKAGING/core_dex_intermediates/classes.dex
 $(system_stub_dex): PRIVATE_MIN_SDK_VERSION := 1000
 $(system_stub_dex): $(call resolve-prebuilt-sdk-jar-path,system_current) | $(ZIP2ZIP) $(DX)
@@ -27,9 +30,18 @@ $(oahl_stub_dex): PRIVATE_MIN_SDK_VERSION := 1000
 $(oahl_stub_dex): $(call get-prebuilt-sdk-dir,current)/org.apache.http.legacy.jar | $(ZIP2ZIP) $(DX)
 	$(transform-classes-d8.jar-to-dex)
 
-.PHONY: appcompat
-
-appcompat: $(system_stub_dex) $(oahl_stub_dex) $(HOST_OUT_EXECUTABLES)/veridex \
+app_compat_lists := \
   ${TARGET_OUT_COMMON_INTERMEDIATES}/PACKAGING/hiddenapi-light-greylist.txt \
   ${TARGET_OUT_COMMON_INTERMEDIATES}/PACKAGING/hiddenapi-dark-greylist.txt \
   ${TARGET_OUT_COMMON_INTERMEDIATES}/PACKAGING/hiddenapi-blacklist.txt
+
+# Phony rule to create all dependencies of the appcompat.sh script.
+.PHONY: appcompat
+appcompat: $(system_stub_dex) $(oahl_stub_dex) $(HOST_OUT_EXECUTABLES)/veridex $(app_compat_lists)
+
+# Make the related files available for prebuilts.
+$(call dist-for-goals,sdk,$(HOST_OUT_EXECUTABLES)/veridex)
+$(call dist-for-goals,sdk,$(LOCAL_PATH)/appcompat.sh)
+$(call dist-for-goals,sdk,$(oahl_stub_dex):org.apache.http.legacy-stubs.dex)
+$(call dist-for-goals,sdk,$(oahl_stub_dex):system-stubs.dex)
+$(call dist-for-goals,sdk,$(app_compat_lists))
