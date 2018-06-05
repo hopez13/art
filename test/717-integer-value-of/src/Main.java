b38158f3e07650563dd09068aff2c6caaed42ab8
Vladimir Marko <vmarko@google.com>
2018-06-05 14:57:24 +0100

    [WIP] Implement Integer.valueOf() intrinsic for PIC.
    And fix the intrinsic for JIT even in case when someone
    messes up the IntegerCache using intrinsics.
    
    TODO: Implement the change. So far there's only a regression
    test that hits
        DCHECK(boxed != nullptr &&
               Runtime::Current()->GetHeap()
                   ->ObjectIsInBootImageSpace(boxed));
    during JIT compilation after messing up the cache.
    
    Test: 717-integer-value-of
    Change-Id: I89b3245a62aba22980c86a99e2af480bfa250af1

---
+++ test/717-integer-value-of/src/Main.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.lang.reflect.Field;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        if (!isDalvik) {
+          // This test is ART-specific. Just fake the expected output.
+          System.out.println("JNI_OnLoad called");
+          return;
+        }
+        System.loadLibrary(args[0]);
+        if (!hasJit()) {
+          return;
+        }
+
+        Class<?> integerCacheClass = Class.forName("java.lang.Integer$IntegerCache");
+        Field cacheField = integerCacheClass.getDeclaredField("cache");
+        cacheField.setAccessible(true);
+        Field lowField = integerCacheClass.getDeclaredField("low");
+        lowField.setAccessible(true);
+
+        Object[] cache = (Object[]) cacheField.get(integerCacheClass);
+        int low = (int) lowField.get(integerCacheClass);
+        Object old42 = cache[42 - low];
+        cache[42 - low] = new Integer(42);
+
+        ensureJitCompiled(Main.class, "get42AsInteger");
+
+        Runtime.getRuntime().gc();
+    }
+
+    public static Integer get42AsInteger() {
+        return Integer.valueOf(42);
+    }
+
+    private native static boolean hasJit();
+    private static native void ensureJitCompiled(Class<?> itf, String method_name);
+
+    private final static boolean isDalvik = System.getProperty("java.vm.name").equals("Dalvik");
+}
