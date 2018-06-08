b5e2700ee1e1c42a7ada1759a0774056045694e1
Alex Light <allight@google.com>
2018-06-08 14:51:09 -0700

    Correctly send MethodExit events from exception handlers
    Due to the way that exceptions are handled it was possible for a
    MethodExit event to be sent multiple times for the same exception.
    This fixes that issue by correctly keeping track of which exception
    events have already been sent and correctly tracking the current
    exception being thrown.
    
    Test: ./test/testrunner/testrunner.py --host --runtime-option=-Xplugin:libtracefast-trampolined.so
    Test: ./test.py --host
    
    Change-Id: I7fa09f0f3f82181430b18805da5ba8daf68d4b89

--- runtime/thread.cc
+++ runtime/thread.cc
@@ -3361,7 +3361,6 @@ void Thread::QuickDeliverException() {
   ClearException();
   QuickExceptionHandler exception_handler(this, false);
   exception_handler.FindCatch(exception);
-  exception_handler.UpdateInstrumentationStack();
   if (exception_handler.GetClearException()) {
     // Exception was cleared as part of delivery.
     DCHECK(!IsExceptionPending());
