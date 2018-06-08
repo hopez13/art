d5ef7cfd12f4226c1783c598f371f2fff1b0b250
Alex Light <allight@google.com>
2018-06-08 15:33:21 -0700

    Make instrumentation trampoline able to jump to jit code
    In order to get the benefit of the instrumentation trampoline it must
    be able to jump to jit code. This patch adds support for doing that
    and adds code to ensure that we will not be jumping back and forth
    between the trampoline and the interpreter all the time if the jit has
    not yet compiled the current method.
    
    Test: ./test/testrunner/testrunner.py --host --runtime-option=-Xplugin:libtracefast-trampolined.so
    Test: ./test/testrunner/testrunner.py --host --runtime-option=-Xplugin:libtracefast-trampolined.so --runtime-option=-Xjitthreshhold:0
    Test: ./test.py --host
    
    Change-Id: Ie6e92ec6367452fe4fde24d520d808b7af91d1b5

--- runtime/jit/jit_code_cache.h
+++ runtime/jit/jit_code_cache.h
@@ -150,6 +150,10 @@ class JitCodeCache {
   // Return true if the code cache contains this pc.
   bool ContainsPc(const void* pc) const;
 
+  // Returns true if either the method's entrypoint is JIT compiled code or it is the
+  // instrumentation entrypoint and we can jump to jit code for this method. For testing use only.
+  bool WillExecuteJitCode(ArtMethod* method) REQUIRES(!lock_);
+
   // Return true if the code cache contains this method.
   bool ContainsMethod(ArtMethod* method) REQUIRES(!lock_);
 
@@ -264,6 +268,12 @@ class JitCodeCache {
   void SetGarbageCollectCode(bool value) {
     garbage_collect_code_ = value;
   }
+
+  bool GetGarbageCollectCode() const {
+    return garbage_collect_code_;
+  }
+
+  const void* FindCompiledCode(ArtMethod* method) REQUIRES(!lock_);
 
  private:
   // Take ownership of maps.
@@ -392,6 +402,8 @@ class JitCodeCache {
   SafeMap<const void*, ArtMethod*> method_code_map_ GUARDED_BY(lock_);
   // Holds osr compiled code associated to the ArtMethod.
   SafeMap<ArtMethod*, const void*> osr_code_map_ GUARDED_BY(lock_);
+  // Holds non-osr compiled code associated to the ArtMethod.
+  SafeMap<ArtMethod*, const void*> non_osr_code_map_ GUARDED_BY(lock_);
   // ProfilingInfo objects we have allocated.
   std::vector<ProfilingInfo*> profiling_infos_ GUARDED_BY(lock_);
 
