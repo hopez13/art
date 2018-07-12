9cac85be3699a853ea6093d6e5fdeb04bdcd711b
Alex Light <allight@google.com>
2018-07-12 11:09:59 -0700

    Allow threads to be marked as unsuspendable by kForUserCode
    There was a possible deadlock between jit-threads being suspended and
    another thread waiting for the jit-thread to finish their work. If the
    jit-thread hit a suspend-point the process would deadlock. This fixes
    this by allowing threads to be marked as unsuspendable by user-code.
    This prevents the issue by marking jit and gc threads as unsuspendable
    by user code. Agents attempting to suspend them using JVMTI will
    succeed (and see the thread as suspended) but internal runtime methods
    will not see the thread as suspended and the thread will not be
    prevented from moving into the kRunnable state. A thread that is
    unsuspendable trying to suspend itself using JVMTI will get
    ERR(INTERNAL) and a log message.
    
    Doing this requires that we rewrite the JVMTI thread suspension code
    somewhat so it will now perform an unconditional kInternal suspension
    prior to trying to suspend the thread kForUserCode. The kInternal
    suspension is then lifted. This ensures that everything is done
    atomically even if the kForUserCode won't stop the thread.
    
    Test: ./test.py --host
    Test: ./art/tools/run-libjdwp-tests.sh --mode=host
    Test: ./art/tools/run-libjdwp-tests.sh \
            --mode=host                    \
            --variant=x64                  \
            --test org.apache.harmony.jpda.tests.jdwp.EventModifiers.InstanceOnlyModifierTest
    
    Bug: 70838465
    Bug: 111348762
    
    Change-Id: I91211641b82416664bf5abd8546efebf4f672f12

--- runtime/thread.cc
+++ runtime/thread.cc
@@ -1241,6 +1241,28 @@ static void UnsafeLogFatalForSuspendCount(Thread* self, Thread* thread) NO_THREA
   LOG(FATAL) << ss.str();
 }
 
+void Thread::SetCanBeSuspendedByUserCode(bool can_be_suspended_by_user_code) {
+  CHECK_EQ(this, Thread::Current()) << "This function may only be called on the current thread.";
+  if (can_be_suspended_by_user_code == CanBeSuspendedByUserCode()) {
+    // Don't need to do anything if nothing is changing.
+    return;
+  }
+  art::MutexLock mu(this, *Locks::user_code_suspension_lock_);
+  art::MutexLock thread_list_mu(this, *Locks::thread_suspend_count_lock_);
+
+  // We want to add the user-code suspend count if we are newly allowing user-code suspends and
+  // remove them if we are disabling them.
+  int adj = can_be_suspended_by_user_code ? GetUserCodeSuspendCount() : -GetUserCodeSuspendCount();
+  // Adjust the global suspend count appropriately. Use kInternal to not change the ForUserCode
+  // count.
+  if (adj != 0) {
+    bool suspend = ModifySuspendCountInternal(this, adj, nullptr, SuspendReason::kInternal);
+    CHECK(suspend) << this << " was unable to modify it's own suspend count!";
+  }
+  // Mark thread as accepting user-code suspensions.
+  can_be_suspended_by_user_code_ = can_be_suspended_by_user_code;
+}
+
 bool Thread::ModifySuspendCountInternal(Thread* self,
                                         int delta,
                                         AtomicInteger* suspend_barrier,
@@ -1261,6 +1283,17 @@ bool Thread::ModifySuspendCountInternal(Thread* self,
     if (UNLIKELY(delta + tls32_.user_code_suspend_count < 0)) {
       LOG(ERROR) << "attempting to modify suspend count in an illegal way.";
       return false;
+    }
+    DCHECK(this == self || this->IsSuspended())
+        << "Only self kForUserCode suspension on an unsuspended thread is allowed: " << this;
+    if (UNLIKELY(!CanBeSuspendedByUserCode())) {
+      VLOG(threads) << this << " is being requested to suspend for user code but that is disabled "
+                    << "the thread will not actually go to sleep.";
+      // Having the user_code_suspend_count still be around is useful but we don't need to actually
+      // do anything since we aren't going to 'really' suspend. Just adjust the
+      // user_code_suspend_count and return.
+      tls32_.user_code_suspend_count += delta;
+      return true;
     }
   }
   if (UNLIKELY(delta < 0 && tls32_.suspend_count <= 0)) {
@@ -2123,7 +2156,8 @@ Thread::Thread(bool daemon)
     : tls32_(daemon),
       wait_monitor_(nullptr),
       tls_mutex_(new Mutex("generic runtime TLS map mutex", LockLevel::kGenericBottomLock)),
-      can_call_into_java_(true) {
+      can_call_into_java_(true),
+      can_be_suspended_by_user_code_(true) {
   wait_mutex_ = new Mutex("a thread wait mutex");
   wait_cond_ = new ConditionVariable("a thread wait condition variable", *wait_mutex_);
   tlsPtr_.instrumentation_stack = new std::deque<instrumentation::InstrumentationStackFrame>;
