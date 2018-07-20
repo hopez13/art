18d2f5419c945588793d697762b1f33a2d75464b
Vladimir Marko <vmarko@google.com>
2018-07-20 16:43:56 +0100

    Rewrite Class init entrypoint to take a Class arg.
    Fixes invalid type index being passed to the entrypoint for
    class init check across dex files when the target type does
    not have a TypeId in the outer compilation unit's DexFile.
    
    The size of the aosp_taimen-userdebug prebuilts:
      - before:
        arm/boot*.oat: 16782748
        arm64/boot*.oat: 19764400
        oat/arm64/services.odex: 20162432
      - after:
        arm/boot*.oat: 16811692 (+28.3KiB, +0.17%)
        arm64/boot*.oat: 19801032 (+35.8KiB, +0.19%)
        oat/arm64/services.odex: 20232208 (+68.1KiB, +0.35%)
    Some of this code size regression shall be mitigated by
    avoiding spills in the slow paths for ClinitCheck. This
    optimization opportunity seems to have been missed when
    introducing the "save everything" entrypoints.
    
    Test: Additional test in 476-clinit-inline-static-invoke
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing
    Test: Pixel 2 XL boots.
    Test: testrunner.py --target --optimizing
    Bug: 111433619
    Change-Id: I2fccd6944480ab4dac514f60d38e72c1014ae7b2

--- test/476-clinit-inline-static-invoke/src/Main.java
+++ test/476-clinit-inline-static-invoke/src/Main.java
@@ -18,6 +18,18 @@ public class Main {
 
   public static void main(String[] args) {
     checkClinitCheckBeforeStaticMethodInvoke();
+
+    // The following is a simple static field getter that can be inlined, referenced
+    // through a subclass with the declaring class having no TypeId in current DexFile.
+    // When we inline this getter, we're left with HLoadClass+HClinitCheck which cannot
+    // be merged back to the InvokeStaticOrDirect for implicit class init check.
+    // The declaring class is in the boot image, so the LoadClass can load it using the
+    // .data.bimg.rel.ro section. However, the ClinitCheck entrypoint was previously
+    // taking a type index of the declaring class and since we did not have a valid
+    // TypeId in the current DexFile, we erroneously provided the type index from the
+    // declaring DexFile and that caused a crash. This was fixed by changing the
+    // ClinitCheck entrypoint to take the Class reference from LoadClass.
+    int dummy = MyTimeZone.getDefaultTimeZoneType();
   }
 
   static void checkClinitCheckBeforeStaticMethodInvoke() {
