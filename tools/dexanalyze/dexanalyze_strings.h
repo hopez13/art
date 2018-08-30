3826f52c6bb88e97572b7dcd2b3f37ff2613d677
Mathieu Chartier <mathieuc@google.com>
2018-08-30 15:52:15 -0700

    String representation performance measurements
    Measure ns for comparison of equal and not equal strings.
    
    Sample results:
    
    Prefix string timings
    Compare equal 27.7578
    Compare not equal 7.91009
    Normal string timings
    Compare equal 11.7484
    Compare not equal 3.36408
    
    Bug: 112311352
    Bug: 77709234
    Test: test-art-host
    
    Change-Id: Id40f8f3f3b12a48961df2e63301ae3359cb98613

--- tools/dexanalyze/dexanalyze_strings.h
+++ tools/dexanalyze/dexanalyze_strings.h
@@ -18,9 +18,10 @@
 #define ART_TOOLS_DEXANALYZE_DEXANALYZE_STRINGS_H_
 
 #include <array>
-#include <vector>
 #include <map>
+#include <vector>
 
+#include "base/leb128.h"
 #include "base/safe_map.h"
 #include "dexanalyze_experiments.h"
 #include "dex/code_item_accessors.h"
@@ -29,6 +30,144 @@
 namespace art {
 namespace dexanalyze {
 
+class PrefixDictionary {
+ public:
+  // Add prefix data and return the offset to the start of the added data.
+  size_t AddPrefixData(const uint8_t* data, size_t len) {
+    const size_t offset = prefix_data_.size();
+    prefix_data_.insert(prefix_data_.end(), data, data + len);
+    return offset;
+  }
+
+  static constexpr size_t kLengthBits = 8;
+  static constexpr size_t kLengthMask = (1u << kLengthBits) - 1;
+
+  // Return the prefix offset and length.
+  ALWAYS_INLINE void GetOffset(uint32_t prefix_index, uint32_t* offset, uint32_t* length) const {
+    CHECK_LT(prefix_index, offsets_.size());
+    const uint32_t data = offsets_[prefix_index];
+    *length = data & kLengthMask;
+    *offset = data >> kLengthBits;
+  }
+
+  uint32_t AddOffset(uint32_t offset, uint32_t length) {
+    CHECK_LE(length, kLengthMask);
+    offsets_.push_back((offset << kLengthBits) | length);
+    return offsets_.size() - 1;
+  }
+
+ public:
+  std::vector<uint32_t> offsets_;
+  std::vector<uint8_t> prefix_data_;
+};
+
+class PrefixStrings {
+ public:
+  class Builder {
+   public:
+    explicit Builder(PrefixStrings* output) : output_(output) {}
+    void Build(const std::vector<std::string>& strings);
+
+   private:
+    PrefixStrings* const output_;
+  };
+
+  // Return the string index that was added.
+  size_t AddString(uint16_t prefix, const std::string& str) {
+    const size_t string_offset = chars_.size();
+    chars_.push_back(static_cast<uint8_t>(prefix >> 8));
+    chars_.push_back(static_cast<uint8_t>(prefix >> 0));
+    EncodeUnsignedLeb128(&chars_, str.length());
+    const uint8_t* ptr = reinterpret_cast<const uint8_t*>(&str[0]);
+    chars_.insert(chars_.end(), ptr, ptr + str.length());
+    string_offsets_.push_back(string_offset);
+    return string_offsets_.size() - 1;
+  }
+
+  std::string GetString(uint32_t string_idx) const {
+    const size_t offset = string_offsets_[string_idx];
+    const uint8_t* suffix_data = &chars_[offset];
+    uint16_t prefix_idx = (static_cast<uint16_t>(suffix_data[0]) << 8) +
+        suffix_data[1];
+    suffix_data += 2;
+    uint32_t prefix_offset;
+    uint32_t prefix_len;
+    dictionary_.GetOffset(prefix_idx, &prefix_offset, &prefix_len);
+    const uint8_t* prefix_data = &dictionary_.prefix_data_[prefix_offset];
+    std::string ret(prefix_data, prefix_data + prefix_len);
+    uint32_t suffix_len = DecodeUnsignedLeb128(&suffix_data);
+    ret.insert(ret.end(), suffix_data, suffix_data + suffix_len);
+    return ret;
+  }
+
+  bool Equal(uint32_t string_idx, const uint8_t* data, size_t len) const {
+    const size_t offset = string_offsets_[string_idx];
+    const uint8_t* suffix_data = &chars_[offset];
+    uint16_t prefix_idx = (static_cast<uint16_t>(suffix_data[0]) << 8) +
+        suffix_data[1];
+    suffix_data += 2;
+    uint32_t prefix_offset;
+    uint32_t prefix_len;
+    dictionary_.GetOffset(prefix_idx, &prefix_offset, &prefix_len);
+    uint32_t suffix_len = DecodeUnsignedLeb128(&suffix_data);
+    if (prefix_len + suffix_len != len) {
+      return false;
+    }
+    const uint8_t* prefix_data = &dictionary_.prefix_data_[prefix_offset];
+    return memcmp(prefix_data, data, prefix_len) == 0u &&
+        memcmp(suffix_data, data + prefix_len, len - prefix_len) == 0u;
+  }
+
+ public:
+  PrefixDictionary dictionary_;
+  std::vector<uint8_t> chars_;
+  std::vector<uint32_t> string_offsets_;
+};
+
+// Normal non prefix strings.
+class NormalStrings {
+ public:
+  // Return the string index that was added.
+  size_t AddString(const std::string& str) {
+    const size_t string_offset = chars_.size();
+    EncodeUnsignedLeb128(&chars_, str.length());
+    const uint8_t* ptr = reinterpret_cast<const uint8_t*>(&str[0]);
+    chars_.insert(chars_.end(), ptr, ptr + str.length());
+    string_offsets_.push_back(string_offset);
+    return string_offsets_.size() - 1;
+  }
+
+  std::string GetString(uint32_t string_idx) const {
+    const size_t offset = string_offsets_[string_idx];
+    const uint8_t* data = &chars_[offset];
+    uint32_t len = DecodeUnsignedLeb128(&data);
+    return std::string(data, data + len);
+  }
+
+  bool Equal(uint32_t string_idx, const uint8_t* data, size_t len) const {
+    const size_t offset = string_offsets_[string_idx];
+    const uint8_t* str_data = &chars_[offset];
+    uint32_t str_len = DecodeUnsignedLeb128(&str_data);
+    if (str_len != len) {
+      return false;
+    }
+    return memcmp(data, str_data, len) == 0u;
+  }
+
+ public:
+  std::vector<uint8_t> chars_;
+  std::vector<uint32_t> string_offsets_;
+};
+
+class StringTimings {
+ public:
+  void Dump(std::ostream& os) const;
+
+  uint64_t time_equal_comparisons_ = 0u;
+  uint64_t time_non_equal_comparisons_ = 0u;
+  uint64_t num_comparisons_ = 0u;
+};
+
 // Analyze string data and strings accessed from code.
 class AnalyzeStrings : public Experiment {
  public:
@@ -36,22 +175,26 @@ class AnalyzeStrings : public Experiment {
   void Dump(std::ostream& os, uint64_t total_size) const override;
 
  private:
-  void ProcessStrings(const std::vector<std::string>& strings, size_t iterations);
+  void ProcessStrings(const std::vector<std::string>& strings);
+  template <typename Strings> void Benchmark(const Strings& strings,
+                                             const std::vector<std::string>& reference,
+                                             StringTimings* timings);
 
+  StringTimings prefix_timings_;
+  StringTimings normal_timings_;
   int64_t wide_string_bytes_ = 0u;
   int64_t ascii_string_bytes_ = 0u;
   int64_t string_data_bytes_ = 0u;
+  int64_t total_unique_string_data_bytes_ = 0u;
   int64_t total_shared_prefix_bytes_ = 0u;
   int64_t total_prefix_savings_ = 0u;
   int64_t total_prefix_dict_ = 0u;
   int64_t total_prefix_table_ = 0u;
   int64_t total_prefix_index_cost_ = 0u;
   int64_t total_num_prefixes_ = 0u;
-  int64_t optimization_savings_ = 0u;
   int64_t strings_used_prefixed_ = 0u;
   int64_t short_strings_ = 0u;
   int64_t long_strings_ = 0u;
-  std::unordered_map<std::string, size_t> prefixes_;
 };
 
 }  // namespace dexanalyze
