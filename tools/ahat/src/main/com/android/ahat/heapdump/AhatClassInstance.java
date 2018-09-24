2bc2a5f2a0b7787801d7ac908282ab6c988171dc
Martijn Coenen <maco@google.com>
2018-09-24 10:44:29 +0200

    Annotate binder services and tokens.
    For all instances of android.os.Binder, try to annotate them by
    retrieving their descriptor. If the instance is a also (sub)class of
    'descriptor$Stub', mark it as a binder service. Otherwise, mark it as
    a binder token.
    
    Example:
    android.server.wm.Session@1aa85368 binder service (android.view.IWindowSession)
    
    Bug: 114365505
    Test: ahat-tests (including new tests) passes, visual inspection
    Change-Id: I7343467be5b745960c6e87c850c4125945de2f85

--- tools/ahat/src/main/com/android/ahat/heapdump/AhatClassInstance.java
+++ tools/ahat/src/main/com/android/ahat/heapdump/AhatClassInstance.java
@@ -160,6 +160,56 @@ public class AhatClassInstance extends AhatInstance {
     return null;
   }
 
+  /**
+   * Returns the descriptor of an android.os.Binder object.
+   * If no descriptor is set, returns an empty string.
+   * If the object is not an android.os.Binder object, returns null.
+   */
+  private String getBinderDescriptor() {
+    if (isInstanceOfClass("android.os.Binder")) {
+      Value value = getField("mDescriptor");;
+
+      if (value == null) {
+        return "";
+      } else {
+        return value.asAhatInstance().asString();
+      }
+    } else {
+      return null;
+    }
+  }
+
+  @Override public String getBinderTokenDescriptor() {
+    String descriptor = getBinderDescriptor();
+    if (descriptor == null) {
+      return null;
+    }
+
+    if (isInstanceOfClass(descriptor + "$Stub")) {
+      // This is an instance of an auto-generated interface class, and
+      // therefore not a binder token.
+      return null;
+    }
+
+    return descriptor;
+  }
+
+  @Override public String getBinderStubInterfaceName() {
+    String descriptor = getBinderDescriptor();
+    if (descriptor == null || descriptor.isEmpty()) {
+      // Binder interface stubs always have a non-empty descriptor
+      return null;
+    }
+
+    // We only consider something a binder service if it's an instance of the
+    // auto-generated descriptor$Stub class.
+    if (isInstanceOfClass(descriptor + "$Stub")) {
+      return descriptor;
+    }
+
+    return null;
+  }
+
   @Override public AhatInstance getAssociatedBitmapInstance() {
     return getBitmapInfo() == null ? null : this;
   }
