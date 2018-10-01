4d2a4d12f131a549a524f7c41408f85f4f32d16e
David Srbecky <dsrbecky@google.com>
2018-10-01 18:17:45 +0100

    Rewrite cpp-define-generator
    The new method works by generating temporary per-architecture
    human-readable object file with the constants embedded in it.
    Python script extracts those values and generates the header.
    
    This means the values can now implicitly depend on pointer size,
    compile time flags, or ABI specific object layout with no hacks.
    
    Test: test-art-host-gtest-arch_test
    Change-Id: Id6e8c77c01f9d6c49cd6d40e3487b56fa4777349

---
+++ tools/cpp-define-generator/make_header.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script looks through compiled object file (stored human readable text),
+# and looks for the compile-time constants (added through custom "asm" block).
+#   For example:  .ascii  ">>OBJECT_ALIGNMENT_MASK $7 $0<<"
+#
+# It will transform each such line to #define which is usabe in assembly code.
+#   For exmpale:  #define OBJECT_ALIGNMENT_MASK 0x7
+#
+# Usage: make_header.py out/soong/.intermediates/.../asm_defines.o
+#
+
+import sys, re, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('input', help="Object file as text")
+args = parser.parse_args()
+
+# Find all defines in the compiler generated assembly file.
+# Sort them to avoid potential recompilations on every build.
+asm_define_re = re.compile(r'">>(\w+) (?:\$|#)([-0-9]+) (?:\$|#)(0|1)<<"')
+asm_defines = sorted(asm_define_re.findall(open(args.input, "r").read()))
+
+if not asm_defines:
+  sys.exit(1)  # Failed to find any matches.
+for asm_define in asm_defines:
+  name = asm_define[0]
+  value = int(asm_define[1])
+  negative_value = (asm_define[2] == "1")
+  if value < 0 and not negative_value:
+    # Overflow - uint64_t constant was pretty printed as negiative int64_t.
+    value += 2 ** 64
+  print("#define " + name + " " + format(value, "#x"))  ## TODO Remove
+  print("#define " + name.upper() + " " + format(value, "#x"))
