2b07b3e0b0a146f450c53cd601a0d11f502894f4
Andreas Gampe <agampe@google.com>
2018-10-11 18:57:39 -0700

    ART: Add integrity check
    Boot classpath classes are expected to be fully verified. Check in
    the interpreter that we are not running access-checks for the boot
    classpath.
    
    Bug: 115834172
    Test: m test-art-host
    Change-Id: I39a9313e9a3b99133115e764c0155f2303b65f10

--- runtime/interpreter/interpreter.cc
+++ runtime/interpreter/interpreter.cc
@@ -348,6 +348,10 @@ static inline JValue Execute(
     }
   } else {
     // Enter the "with access check" interpreter.
+
+    // The boot classpath should really not have to run access checks.
+    DCHECK(method->GetDeclaringClass()->GetClassLoader() != nullptr);
+
     if (kInterpreterImplKind == kMterpImplKind) {
       // No access check variants for Mterp.  Just use the switch version.
       if (transaction_active) {
