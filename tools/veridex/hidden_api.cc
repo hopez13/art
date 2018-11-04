c4e6d3c0d6471ad1a00e9dfc7f7d330fcfc9a5a1
David Brazdil <dbrazdil@google.com>
2018-11-04 18:07:23 +0000

    Convert flags to CSV
    Test: m, phone boots
    Change-Id: Iffa64b36ffc5524779d5f57c3c6f0c0e84bfc606

--- tools/veridex/hidden_api.cc
+++ tools/veridex/hidden_api.cc
@@ -19,9 +19,50 @@
 #include <fstream>
 #include <sstream>
 
+#include "android-base/strings.h"
 #include "dex/dex_file-inl.h"
 
 namespace art {
+
+HiddenApi::HiddenApi(const char* filename, bool sdk_uses_only) {
+  CHECK(filename != nullptr);
+
+  std::ifstream in(filename);
+  for (std::string str; std::getline(api_file, str);) {
+    std::vector<std::string> values = android::base::Split(str, ",");
+    CHECK_EQ(values.size(), 2u) << "Currently only signature and one flag are supported";
+
+    const std::string& signature = values[0];
+    const std::string& flag_str = values[1];
+
+    hiddenapi::ApiList membership = hiddenapi::ApiList::kNoList;
+    for (size_t i = 0; i < hiddenapi::kApiListCount; ++i) {
+      if (flag_str == hiddenapi::kApiListNames[i]) {
+        membership = static_cast<hiddenapi::ApiList>(i);
+      }
+    }
+    CHECK_NE(membership, hiddenapi::ApiList::kNoList) << "Unknown ApiList name: " << flag_str;
+
+    CHECK(api_list_.find(signature) == api_list_.end()) << "Duplicate entry: " << signature;
+
+    api_list_.emplace(signature, membership);
+    size_t pos = signature.find("->");
+    if (pos != std::string::npos) {
+      // Add the class name.
+      api_list_.emplace(signature.substr(0, pos), membership);
+      pos = signature.find('(');
+      if (pos != std::string::npos) {
+        // Add the class->method name (so stripping the signature).
+        api_list_.emplace(signature.substr(0, pos), membership);
+      }
+      pos = signature.find(':');
+      if (pos != std::string::npos) {
+        // Add the class->field name (so stripping the type).
+        api_list_.emplace(signature.substr(0, pos), membership);
+      }
+    }
+  }
+}
 
 std::string HiddenApi::GetApiMethodName(const DexFile& dex_file, uint32_t method_index) {
   std::stringstream ss;
@@ -42,32 +83,6 @@ std::string HiddenApi::GetApiFieldName(const DexFile& dex_file, uint32_t field_i
      << ":"
      << dex_file.GetFieldTypeDescriptor(field_id);
   return ss.str();
-}
-
-void HiddenApi::FillList(const char* filename, std::set<std::string>& entries) {
-  if (filename == nullptr) {
-    return;
-  }
-  std::ifstream in(filename);
-  std::string str;
-  while (std::getline(in, str)) {
-    entries.insert(str);
-    size_t pos = str.find("->");
-    if (pos != std::string::npos) {
-      // Add the class name.
-      entries.insert(str.substr(0, pos));
-      pos = str.find('(');
-      if (pos != std::string::npos) {
-        // Add the class->method name (so stripping the signature).
-        entries.insert(str.substr(0, pos));
-      }
-      pos = str.find(':');
-      if (pos != std::string::npos) {
-        // Add the class->field name (so stripping the type).
-        entries.insert(str.substr(0, pos));
-      }
-    }
-  }
 }
 
 }  // namespace art
