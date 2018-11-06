877e1e5ec28f8026b9334595af7468554d1e37e3
Vladimir Marko <vmarko@google.com>
2018-11-06 14:55:54 +0000

    Refactor ClassLinker::FindPrimitiveClass().
    Introduce ClassLinker::LookupPrimitiveClass() that never
    throws an exception and use it to clean up callers that
    are not supposed to cause thread suspension.
    
    Test: m test-art-host-gtest
    Bug: 118909457
    Change-Id: Ibfee8b7991c4a0d1fa636c85f14437c9865d1658

--- runtime/native/java_lang_reflect_Field.cc
+++ runtime/native/java_lang_reflect_Field.cc
@@ -338,10 +338,11 @@ static void Field_set(JNIEnv* env, jobject javaField, jobject javaObj, jobject j
   Primitive::Type field_prim_type = Primitive::GetType(field_type_desciptor[0]);
   if (field_prim_type == Primitive::kPrimNot) {
     field_type = f->GetType();
-    DCHECK(field_type != nullptr);
   } else {
-    field_type = Runtime::Current()->GetClassLinker()->FindPrimitiveClass(field_type_desciptor[0]);
+    field_type =
+        Runtime::Current()->GetClassLinker()->LookupPrimitiveClass(field_type_desciptor[0]);
   }
+  DCHECK(field_type != nullptr) << field_type_desciptor;
   // We now don't expect suspension unless an exception is thrown.
   // Unbox the value, if necessary.
   ObjPtr<mirror::Object> boxed_value = soa.Decode<mirror::Object>(javaValue);
