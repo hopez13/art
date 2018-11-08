b6d70aea6c86a17b9a8db6014a3b7a05f764b4a1
Albert Mingkun Yang <albertnetymk@google.com>
2018-11-08 20:51:20 +0000

    ART: add GC time ratio metrics to GC performance
    Record CPU time spent in each GC cycle, and wall-clock for the Runtime.
    Print the ratio if DumpGCPerformanceOnShutdown is set.
    
    Test: Run art with -XX:DumpGCPerformanceOnShutdown on some benchmarks.
    Bug: 112187497
    Change-Id: I4a7c3cd457662e57faff21f506a5bc4ba143ee25

--- runtime/runtime.cc
+++ runtime/runtime.cc
@@ -279,6 +279,7 @@ Runtime::Runtime()
       // Initially assume we perceive jank in case the process state is never updated.
       process_state_(kProcessStateJankPerceptible),
       zygote_no_threads_(false),
+      runtime_start_time(NanoTime()),
       verifier_logging_threshold_ms_(100) {
   static_assert(Runtime::kCalleeSaveSize ==
                     static_cast<uint32_t>(CalleeSaveType::kLastCalleeSaveType), "Unexpected size");
@@ -322,11 +323,21 @@ Runtime::~Runtime() {
   }
 
   if (dump_gc_performance_on_shutdown_) {
+    runtime_end_time = NanoTime();
     ScopedLogSeverity sls(LogSeverity::INFO);
     // This can't be called from the Heap destructor below because it
     // could call RosAlloc::InspectAll() which needs the thread_list
     // to be still alive.
     heap_->DumpGcPerformanceInfo(LOG_STREAM(INFO));
+
+    uint64_t duration = runtime_end_time - runtime_start_time;
+    uint64_t gc_cpu_time = heap_->GetTotalGCCpuTime();
+    // NOTE: numerator is in CPU time, while denominator is in wall-clock time.
+    float ratio = static_cast<float>(gc_cpu_time) / duration;
+    LOG_STREAM(INFO) << "GC CPU time " << PrettyDuration(gc_cpu_time)
+        << " out of wall-clock time " << PrettyDuration(duration)
+        << " (" << ratio << ")"
+        << "\n";
   }
 
   if (jit_ != nullptr) {
