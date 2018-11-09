7bb928927b7bdef42ce0237c7d00b5e2f62f1b4f
Neil Fuller <nfuller@google.com>
2018-11-09 15:49:05 +0000

    Revert "Add core-simple to the standard boot classpath"
    This reverts commit 1391390a775ca54fa1dd22ceccf211eae84073ea.
    
    Test: build
    Bug: 119301609
    Change-Id: I28039a960ce5358744565f8943489677138a892c

--- build/Android.gtest.mk
+++ build/Android.gtest.mk
@@ -451,8 +451,7 @@ define define-art-gtest-rule-target
     $$($(3)TARGET_OUT_SHARED_LIBRARIES)/libjavacore.so \
     $$($(3)TARGET_OUT_SHARED_LIBRARIES)/libopenjdkd.so \
     $$(TARGET_OUT_JAVA_LIBRARIES)/core-libart-testdex.jar \
-    $$(TARGET_OUT_JAVA_LIBRARIES)/core-oj-testdex.jar \
-    $$(TARGET_OUT_JAVA_LIBRARIES)/core-simple-testdex.jar
+    $$(TARGET_OUT_JAVA_LIBRARIES)/core-oj-testdex.jar
 
   ART_TEST_TARGET_GTEST_DEPENDENCIES += $$(gtest_deps)
 
