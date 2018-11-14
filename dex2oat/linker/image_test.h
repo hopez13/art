762b547bfa29a9504ab5794540c6f79ece2463ac
Vladimir Marko <vmarko@google.com>
2018-11-14 15:09:02 +0000

    Rewrite image type in CompilerOptions.
    Prepare for introduction of boot image extension.
    
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing
    Test: Pixel 2 XL boots.
    Change-Id: Id007c4aeb4ada84fe65c1148fdf67e8a412e5b74

--- dex2oat/linker/image_test.h
+++ dex2oat/linker/image_test.h
@@ -216,7 +216,6 @@ inline void ImageTest::DoCompile(ImageHeader::StorageMode storage_mode,
   // TODO: compile_pic should be a test argument.
   std::unique_ptr<ImageWriter> writer(new ImageWriter(*compiler_options_,
                                                       kRequestedImageBase,
-                                                      /*compile_app_image*/false,
                                                       storage_mode,
                                                       oat_filename_vector,
                                                       dex_file_to_oat_index_map,
