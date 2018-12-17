771f92729dbc6168306b5eceb94313cdcb1fff1e
Mathieu Chartier <mathieuc@google.com>
2018-12-17 13:17:30 -0800

    Move startup thread pool back into runtime
    Added logic in heap trim to delete the thread pool, if there are no
    active users.
    
    Added a scoped accessor to prevent ref counting errors.
    
    Motivation, have workers already created when the app images are
    loaded.
    
    Bug: 116052292
    Test: test-art-host
    Change-Id: I8ea776d74e88601222a9989e0c6dac34cf77c683

--- runtime/runtime.h
+++ runtime/runtime.h
@@ -792,6 +792,22 @@ class Runtime {
     return verifier_logging_threshold_ms_;
   }
 
+  // Atomically delete the thread pool if the reference count is 0.
+  bool DeleteThreadPool() REQUIRES(!Locks::runtime_thread_pool_lock_);
+
+  class ScopedThreadPoolUsage {
+   public:
+    ScopedThreadPoolUsage();
+    ~ScopedThreadPoolUsage();
+
+    ThreadPool* GetThreadPool() const {
+      return thread_pool_;
+    }
+
+   private:
+    ThreadPool* thread_pool_;
+  };
+
  private:
   static void InitPlatformSignalHandlers();
 
@@ -827,6 +843,10 @@ class Runtime {
   //       only aborting code should retrieve this data (via GetFaultMessageForAbortLogging
   //       friend).
   std::string GetFaultMessage();
+
+  ThreadPool* AcquireThreadPool() REQUIRES(!Locks::runtime_thread_pool_lock_);
+  void ReleaseThreadPool() REQUIRES(!Locks::runtime_thread_pool_lock_);
+  void WaitForThreadPoolWorkersToStart() REQUIRES(!Locks::runtime_thread_pool_lock_);
 
   // A pointer to the active runtime or null.
   static Runtime* instance_;
@@ -910,6 +930,10 @@ class Runtime {
   std::unique_ptr<jit::Jit> jit_;
   std::unique_ptr<jit::JitCodeCache> jit_code_cache_;
   std::unique_ptr<jit::JitOptions> jit_options_;
+
+  // Runtime thread pool.
+  std::unique_ptr<ThreadPool> thread_pool_ GUARDED_BY(Locks::runtime_thread_pool_lock_);
+  size_t thread_pool_ref_count_ GUARDED_BY(Locks::runtime_thread_pool_lock_);
 
   // Fault message, printed when we get a SIGSEGV. Stored as a native-heap object and accessed
   // lock-free, so needs to be atomic.
@@ -1115,6 +1139,7 @@ class Runtime {
 
   // Note: See comments on GetFaultMessage.
   friend std::string GetFaultMessageForAbortLogging();
+  friend class ScopedThreadPoolUsage;
 
   DISALLOW_COPY_AND_ASSIGN(Runtime);
 };
