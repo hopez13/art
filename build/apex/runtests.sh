f43fae853ad6816a44a1c5ebc0c80861763eac15
Martin Stjernholm <mast@google.com>
2019-01-22 01:51:33 +0000

    Move libnativebridge to the runtime apex.
    Copy of http://r.android.com/867749.
    
    Test: Flash and boot
    Bug: 119840313
    Change-Id: I6ce073de9212b8a2c3dcf682e88f5657b814a29f

--- build/apex/runtests.sh
+++ build/apex/runtests.sh
@@ -183,6 +183,7 @@ function check_release_contents {
   check_library libziparchive.so
   # Check that the mounted image contains additional required libraries.
   check_library libadbconnection.so
+  check_library libnativebridge.so
 
   # TODO: Should we check for other libraries, such as:
   #
