458e6c0a09ac1a77bdd53604436a281ffdb345b6
David Brazdil <dbrazdil@google.com>
2019-02-07 20:06:19 +0000

    Introduce vdex-based verification caching for InMemoryDexClassLoader
    [WIP]
    
    Change-Id: I1d82dc441c35c7b5db04038eeaf74c3a5dcd64be

--- runtime/verifier/method_verifier.h
+++ runtime/verifier/method_verifier.h
@@ -41,6 +41,7 @@ class CompilerCallbacks;
 class DexFile;
 class Instruction;
 struct ReferenceMap2Visitor;
+class ScopedObjectAccess;
 class Thread;
 class VariableIndentationOutputStream;
 
@@ -99,6 +100,20 @@ class PcToRegisterLineTable {
 // The verifier
 class MethodVerifier {
  public:
+  static FailureKind VerifyClassForCompiler(Thread* self,
+                                            const DexFile&dex_file,
+                                            uint32_t class_def_index,
+                                            Handle<mirror::ClassLoader> class_loader,
+                                            Handle<mirror::Class> klass,
+                                            CompilerCallbacks* callbacks,
+                                            bool allow_soft_failures,
+                                            HardFailLogMode log_level,
+                                            uint32_t api_level,
+                                            ClassLinker* class_linker,
+                                            /* out */ bool *was_skipped,
+                                            /* out */ std::string* error)
+      REQUIRES_SHARED(Locks::mutator_lock_);
+
   // Verify a class. Returns "kNoFailure" on success.
   static FailureKind VerifyClass(Thread* self,
                                  ObjPtr<mirror::Class> klass,
