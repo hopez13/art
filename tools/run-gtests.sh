4760ee8258a80eae05520e60824d51312ff06562
Evgeny Astigeevich <evgeny.astigeevich@linaro.org>
2019-02-11 10:55:15 +0000

    ART: add running gtests in parallel to run-gtests.sh
    'run-gtests.sh' cannot run tests in parallel.
    
    This CL adds running tests in parallel using GNU parallel if it is
    available. A number of jobs to run can be specified with '-j <number>'.
    If it is not provided the default is 7.
    
    Test: run-gtests.sh
    Test: run-gtests.sh -j 8
    Change-Id: I3bfe70ccd0400675000e1cfdd39ece7cfccd2f29

--- tools/run-gtests.sh
+++ tools/run-gtests.sh
@@ -20,6 +20,8 @@ ADB="${ADB:-adb}"
 all_tests=()
 failing_tests=()
 
+# Arguments:
+#   ${1}: a path to tests
 function add_tests {
   all_tests+=$(${ADB} shell "test -d $ART_TEST_CHROOT/$1 && chroot $ART_TEST_CHROOT find $1 -name \*_test")
 }
@@ -28,14 +30,66 @@ function fail {
   failing_tests+=($1)
 }
 
+# Arguments:
+#   ${1}: a path to gtest
+function run_gtest {
+  ${ADB} shell "chroot $ART_TEST_CHROOT env LD_LIBRARY_PATH= ANDROID_ROOT='/system' ANDROID_RUNTIME_ROOT=/system $1"
+}
+
+function run_gtests {
+  for i in ${all_tests}; do
+    run_gtest "${i}" || fail "${i}"
+  done
+}
+
+# Arguments: -j <number of parallel jobs>
+function run_gtests_parallel {
+  if [[ ${#all_tests[@]} -eq 0 ]]; then
+    return
+  fi
+
+  export -f run_gtest
+
+  local jobs=7
+  if [[ "$1" == -j && $# -ge 2 ]]; then
+    jobs=$2
+  fi
+
+  local joblog_out_dir="${ANDROID_PRODUCT_OUT}"
+  if [[ -z "${joblog_out_dir}" ]]; then
+    joblog_out_dir=$(mktemp -d)
+  fi
+  local -r joblog="${joblog_out_dir}/log_jobs_gtest.txt"
+
+  echo "Running gtests with the job count: ${jobs}"
+
+  for i in ${all_tests}; do
+    echo "${i}"
+  done | parallel --jobs "${jobs}" --joblog "${joblog}" run_gtest {}
+
+  local return_code=$?
+  if [[ ${return_code} -ne 0 ]]; then
+    # Format of log_jobs_gtest.txt:
+    #   Seq Host Starttime JobRuntime Send Receive Exitval Signal Command
+    for i in $(awk '{if ($7 != 0) print $10}' "${joblog}"  | grep data); do
+      fail "${i}"
+    done
+  fi
+  rm -f "${joblog}"
+  return "${return_code}"
+}
+
 add_tests "/data/nativetest"
 add_tests "/data/nativetest64"
 
-for i in $all_tests; do
-  ${ADB} shell "chroot $ART_TEST_CHROOT env LD_LIBRARY_PATH= ANDROID_ROOT='/system' ANDROID_RUNTIME_ROOT=/system $i" || fail $i
-done
+if [[ -n "$(which parallel)" ]]; then
+  run_gtests_parallel "$@"
+else
+  run_gtests
+fi
 
-if [ -n "$failing_tests" ]; then
+if [[ -n "${failing_tests}" ]]; then
+  echo ""
   for i in "${failing_tests[@]}"; do
     echo "Failed test: $i"
   done
