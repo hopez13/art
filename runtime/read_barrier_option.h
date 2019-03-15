a530c6e128b58dca6da1895f464213991368400e
Vladimir Marko <vmarko@google.com>
2019-03-15 13:14:11 +0000

    Document when we can avoid read barriers.
    Test: Rely on TreeHugger.
    Bug: 119486698
    Change-Id: I6d5e18709ff7d624eea7a083f39a56ed8f8ffa49

--- runtime/read_barrier_option.h
+++ runtime/read_barrier_option.h
@@ -19,6 +19,34 @@
 namespace art {
 
 // Options for performing a read barrier or not.
+//
+// Besides disabled GC and GC's internal usage, there are a few cases where the read
+// barrier is unnecessary and can be avoided to reduce code size and improve performance.
+// In the following cases, the result of the operation or chain of operations shall be the
+// same whether we go through the from-space or to-space:
+//
+// 1. We're reading a reference known to point to an un-reclaimable non-moving space object.
+//    (For example the java.lang.Class.class or boot image classes and strings).
+// 2. We're reading the reference for comparison involving a non-moving space reference.
+//    (Whether the non-moving space reference is the one we're reading or the one we shall
+//    compare it with, the result is the same with and without read barrier.)
+// 3. We're reading the reference only for comparison with null.
+//    (Similar to 2 above, given that null is "non-moving".)
+// 4. We're reading a reference to a holder from which we shall read
+//     - constant primitive field, or
+//     - constant reference field known to point to an un-reclaimable non-moving space object, or
+//     - constant reference field for comparison involving a non-moving space reference, or
+//     - constant reference field for comparison with null, or
+//     - constant reference fields in a chain leading to one or more of the above purposes;
+//       the entire chain needs to be read without read barrier.
+//    The term "constant" refers to fields set to their final value between allocating
+//    the holder and the next opportunity for the holder to be moved by the GC, i.e.
+//    before the first suspend point after the allocation, or seen by another thread.
+//    This includes several fields in the Class object, such as the primitive type or
+//    component type but not the superclass.
+//
+// References read without a read barrier must not remain live at the next suspend point,
+// with the exception of references to un-reclaimable non-moving space objects.
 enum ReadBarrierOption {
   kWithReadBarrier,     // Perform a read barrier.
   kWithoutReadBarrier,  // Don't perform a read barrier.
