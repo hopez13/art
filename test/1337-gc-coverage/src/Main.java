d63c6e6bf5f9159958a688b9dbdef9459c0dd0e1
Mathieu Chartier <mathieuc@google.com>
2019-04-05 10:16:00 -0700

    Remove support for moving GC collector transitions
    Stop supporting CMS <-> SS transitions since they are no longer used
    with CC.
    
    Test: test-art-host
    Bug: 78286368
    Change-Id: I2f7b1538d2c73546ac2ab8bd1eb47f01f9f40503

--- test/1337-gc-coverage/src/Main.java
+++ test/1337-gc-coverage/src/Main.java
@@ -68,40 +68,10 @@ public class Main {
     }
   }
 
-  private static void testCollectorTransitions() {
-    if (supportCollectorTransition()) {
-      Object o = new Object();
-      // Transition to semi-space collector.
-      allocateStuff();
-      transitionToSS();
-      allocateStuff();
-      long addressBefore = objectAddress(o);
-      Runtime.getRuntime().gc();
-      long addressAfter = objectAddress(o);
-      if (addressBefore == addressAfter) {
-        System.out.println("error: Expected different adddress " + addressBefore + " vs " +
-            addressAfter);
-      }
-      // Transition back to CMS.
-      transitionToCMS();
-      allocateStuff();
-      addressBefore = objectAddress(o);
-      Runtime.getRuntime().gc();
-      addressAfter = objectAddress(o);
-      if (addressBefore != addressAfter) {
-        System.out.println("error: Expected same adddress " + addressBefore + " vs " +
-            addressAfter);
-      }
-    }
-  }
-
   // Methods to get access to ART internals.
   private static native boolean supportHomogeneousSpaceCompact();
   private static native boolean performHomogeneousSpaceCompact();
   private static native void incrementDisableMovingGC();
   private static native void decrementDisableMovingGC();
   private static native long objectAddress(Object object);
-  private static native boolean supportCollectorTransition();
-  private static native void transitionToSS();
-  private static native void transitionToCMS();
 }
