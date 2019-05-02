6b952757e624e05fb377e0405ab6cf99c73f3b03
Orion Hodson <oth@google.com>
2019-05-02 10:56:33 +0100

    Move stack logging entirely into palette library
    Make SignalCatcher::Output the same on host and target.
    
    Apply same logic of when to log stacks to system logger as prior to
    PaletteTombstonedMessage introduction.
    
    Add PaletteStatus::kFailedCheckLog
    
    Bug: 130025619
    Test: art/tools/buildbot-build.sh --host -j100 && art/test.py --host -j32
    Test: adb shell killall -QUIT system_server and check /data/anr
    Test: Fill /data using 'dd', again send SIGQUIT to system and \
      check logcat for stack traces.
    
    Change-Id: I1b3b6780622b9799d61fb12ad11a28085729c5e9

--- libartpalette/system/palette_fake.cc
+++ libartpalette/system/palette_fake.cc
@@ -19,6 +19,7 @@
 #include <map>
 #include <mutex>
 
+#include <android-base/logging.h>
 #include <android-base/macros.h>  // For ATTRIBUTE_UNUSED
 
 #include "palette_system.h"
@@ -52,8 +53,8 @@ enum PaletteStatus PaletteSchedGetPriority(int32_t tid,
   return PaletteStatus::kOkay;
 }
 
-enum PaletteStatus PaletteTombstonedMessage(/*in*/ const char* msg ATTRIBUTE_UNUSED,
-                                            size_t msg_len ATTRIBUTE_UNUSED) {
+enum PaletteStatus PaletteTombstonedMessage(/*in*/ const char* msg, size_t msg_len) {
+  LOG(INFO) << std::string_view(msg, msg_len);
   return PaletteStatus::kOkay;
 }
 
