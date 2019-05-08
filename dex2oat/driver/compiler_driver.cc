1a7b9ec7f49a75778bb8a35f3fd2743977f34c6c
David Sehr <sehr@google.com>
2019-05-08 14:53:36 -0700

    DO NOT SUBMIT: Add dump for redefinitions of classes
    Bug: none
    Test: none
    
    Change-Id: I55f52f87b6ea44c6d03838937e75ea07de3cf94f

--- dex2oat/driver/compiler_driver.cc
+++ dex2oat/driver/compiler_driver.cc
@@ -1648,6 +1648,16 @@ class ResolveClassFieldsAndMethodsVisitor : public CompilationVisitor {
       CheckAndClearResolveException(soa.Self());
       resolve_fields_and_methods = false;
     } else {
+      if (!manager_->GetCompiler()->GetCompilerOptions().IsBootImage()
+          && manager_->GetCompiler()->GetCompilerOptions().IsCrashOnRedefinition()) {
+        ObjPtr<mirror::ClassLoader> current_class_loader =
+            soa.Decode<mirror::ClassLoader>(jclass_loader);
+        ObjPtr<mirror::ClassLoader> resolving_class_loader = klass->GetClassLoader();
+        if (resolving_class_loader != current_class_loader) {
+          LOG(FATAL) << "OptStat#" << klass->PrettyClassAndClassLoader() << ": 1";
+        }
+      }
+      CHECK(klass->IsResolved()) << klass->PrettyClass();
       // We successfully resolved a class, should we skip it?
       if (SkipClass(jclass_loader, dex_file, klass)) {
         return;
