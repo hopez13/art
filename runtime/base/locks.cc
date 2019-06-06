7acb145e2b3f8d90612c51a639359944457fe019
Nicolas Geoffray <ngeoffray@google.com>
2019-06-06 09:04:07 +0100

    Move memory related regions of the JIT cache in its own class.
    - Moves the JIT lock into the global locks list
    - Jit cache has two regions: one inherited from zygote, and its own.
    
    Bug: 119800099
    Test: boot, test.py
    Change-Id: I833ba2b5a3d3c10fa01286cdf52603fa174a2cc7

--- runtime/base/locks.cc
+++ runtime/base/locks.cc
@@ -63,6 +63,7 @@ Mutex* Locks::reference_queue_weak_references_lock_ = nullptr;
 Mutex* Locks::runtime_shutdown_lock_ = nullptr;
 Mutex* Locks::runtime_thread_pool_lock_ = nullptr;
 Mutex* Locks::cha_lock_ = nullptr;
+Mutex* Locks::jit_lock_ = nullptr;
 Mutex* Locks::subtype_check_lock_ = nullptr;
 Mutex* Locks::thread_list_lock_ = nullptr;
 ConditionVariable* Locks::thread_exit_cond_ = nullptr;
@@ -147,6 +148,7 @@ void Locks::Init() {
     DCHECK(mutator_lock_ != nullptr);
     DCHECK(profiler_lock_ != nullptr);
     DCHECK(cha_lock_ != nullptr);
+    DCHECK(jit_lock_ != nullptr);
     DCHECK(subtype_check_lock_ != nullptr);
     DCHECK(thread_list_lock_ != nullptr);
     DCHECK(thread_suspend_count_lock_ != nullptr);
@@ -303,6 +305,10 @@ void Locks::Init() {
     DCHECK(custom_tls_lock_ == nullptr);
     custom_tls_lock_ = new Mutex("Thread::custom_tls_ lock", current_lock_level);
 
+    UPDATE_CURRENT_LOCK_LEVEL(kJitCodeCacheLock);
+    DCHECK(jit_lock_ == nullptr);
+    jit_lock_ = new Mutex("Jit code cache", current_lock_level);
+
     UPDATE_CURRENT_LOCK_LEVEL(kCHALock);
     DCHECK(cha_lock_ == nullptr);
     cha_lock_ = new Mutex("CHA lock", current_lock_level);
@@ -326,6 +332,7 @@ void Locks::Init() {
     UPDATE_CURRENT_LOCK_LEVEL(kLoggingLock);
     DCHECK(logging_lock_ == nullptr);
     logging_lock_ = new Mutex("logging lock", current_lock_level, true);
+
 
     #undef UPDATE_CURRENT_LOCK_LEVEL
 
