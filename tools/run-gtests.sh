eaeb540dc21d9c401684424b6602a4195c79071a
Victor Chang <vichang@google.com>
2019-07-05 16:32:41 +0100

    Remove icu .dat file from Runtime module
    Bug: 137009149
    Test: device boots
    Test: m out/target/common/obj/JAVA_LIBRARIES/ahat-test-dump_intermediates/test-dump.hprof
    Test: art/build/apex/runtests.sh fails due to bug 138103882
    Test: art/tools/buildbot-build.sh
    Change-Id: I0501fd6dd02dac3cf9780f5f1c0103e4b0475e1f

--- tools/run-gtests.sh
+++ tools/run-gtests.sh
@@ -40,7 +40,7 @@ add_tests "/data/nativetest64"
 
 for i in $all_tests; do
   echo "$i"
-  ${ADB} shell "chroot $ART_TEST_CHROOT env ANDROID_RUNTIME_ROOT='/apex/com.android.runtime' ANDROID_TZDATA_ROOT='/apex/com.android.tzdata' $i" || fail $i
+  ${ADB} shell "chroot $ART_TEST_CHROOT env ANDROID_I18N_ROOT='/apex/com.android.i18n' ANDROID_RUNTIME_ROOT='/apex/com.android.runtime' ANDROID_TZDATA_ROOT='/apex/com.android.tzdata' $i" || fail $i
 done
 
 if [ -n "$failing_tests" ]; then
