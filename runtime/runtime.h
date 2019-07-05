95a5fedd1c29687032440ffe076dfba146e6d71f
Priyanka Bose <priyanka.bose@intel.com>
2019-07-06 01:15:22 +0530

    Auto Fast JNI Detection for x86 & x86_64
    This patch implements Binary Analysis for x86 during native method registration at runtime.
    Marking methods as fast means the thread does not have to do costly transition from kRunnable to kNative.
    This will reduce the overhead of JNI transition from Java to Native
    The method is classified as fast if it satisfies certain constraints which we have mentioned as below .As of now we have checked as these
    minimal gating conditions to start of :
     - No indirect calls or jumps
     - No more than 3 levels of call nesting
     - No unsupported instructions,or interrupts
     - No repeats(or loops),
     - No instructions with LOCK prefix
     - Less than or equal to 3 levels of call depth nesting (20 Basic Blocks & 100 Instructions)
    Essentially, all simple Native methods that satisfy the above criteria are marked as fast.
    Disassembly of x86 Instructions is done by extending and utilizing the fundtionality of ART disassembler.
    The auto fast JNI is enabled by only in JIT mode and currently does the analysis for x86 only instruction set
    We have observed performance runtime benefits:
    => jArtBand JNI  benchmark - approx 30%
    Jave Apps ( meeting the gating condition )Launch time for Recursive Launches( second or third launch) :
    -> Twitter , UC browser : approx 5-6%
    Test: ./test.py --host --64
    
    Change-Id: Iacf45c4efa17a048a0b1da95ca28f30610f18d08
    Signed-off-by: Priyanka Bose <priyanka.bose@intel.com>

--- runtime/runtime.h
+++ runtime/runtime.h
@@ -859,6 +859,16 @@ class Runtime {
     return env_snapshot_.GetSnapshot();
   }
 
+  // Should Auto fast Detection be done.
+  bool IsAutoFastDetect() const {
+    return auto_fast_detect_;
+  }
+
+  // Set Auto Fast Detection.
+  void SetAutoFastDetect(bool value) {
+    auto_fast_detect_ = value;
+  }
+
   void AddSystemWeakHolder(gc::AbstractSystemWeakHolder* holder);
   void RemoveSystemWeakHolder(gc::AbstractSystemWeakHolder* holder);
 
@@ -1343,6 +1353,9 @@ class Runtime {
   friend class ScopedThreadPoolUsage;
   friend class OatFileAssistantTest;
   class NotifyStartupCompletedTask;
+
+  // Auto Fast JNI detection gate.
+  bool auto_fast_detect_;
 
   DISALLOW_COPY_AND_ASSIGN(Runtime);
 };
