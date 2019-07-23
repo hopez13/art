5410e08ef9e470c74a856e0b13b9025727fec59a
Alex Light <allight@google.com>
2019-07-23 13:10:20 -0700

    WIP: Add obsolete object event
    Test: TBD
    Bug: TBD
    
    Change-Id: If48b880814a751ba6c24c18d0ad116db4f8fdf64

--- openjdkjvmti/ti_heap.cc
+++ openjdkjvmti/ti_heap.cc
@@ -28,6 +28,7 @@
 #include "class_root.h"
 #include "deopt_manager.h"
 #include "dex/primitive.h"
+#include "events-inl.h"
 #include "gc/collector_type.h"
 #include "gc/gc_cause.h"
 #include "gc/heap-visit-objects-inl.h"
@@ -70,6 +71,8 @@
 #include <ios>
 
 namespace openjdkjvmti {
+
+EventHandler* HeapExtensions::gEventHandler = nullptr;
 
 namespace {
 
@@ -1817,6 +1820,36 @@ jvmtiError HeapExtensions::ChangeArraySize(jvmtiEnv* env, jobject arr, jsize new
           }
         });
   }
+  // Handle tags. We want to do this seprately from other weak-refs (handled below) because we need
+  // to send additional events and handle cases where the agent might have tagged the new
+  // replacement object during the VMObjectAlloc.
+  std::unordered_map<ArtJvmTiEnv*, jlong> obsolete_tags;
+  std::unordered_map<ArtJvmTiEnv*, jlong> non_obsolete_tags;
+  gEventHandler->ForEachEnv(self, [&](ArtJvmTiEnv* env) NO_THREAD_SAFETY_ANALYSIS {
+    // TODO
+    env->object_tag_table->Lock();
+    // Get the tags and clear them (so we don't need to special-case the normal weak-ref visitor)
+    jlong new_tag = 0;
+    jlong obsolete_tag = 0;
+    bool had_new_tag = env->object_tag_table->RemoveLocked(new_arr_ptr, &new_tag);
+    bool had_obsolete_tag = env->object_tag_table->RemoveLocked(old_arr_ptr, &obsolete_tag);
+    // Dispatch event.
+    gEventHandler->DispatchEventOnEnv<ArtJvmtiEvent::kObsoleteObjectCreated>(env,
+                                                                             self,
+                                                                             &obsolete_tag,
+                                                                             &new_tag);
+    if (had_obsolete_tag) {
+      obsolete_tags[env] = obsolete_tag;
+    }
+    if (had_new_tag) {
+      non_obsolete_tags[env] = new_tag;
+    }
+    // After weak-ref update go back and re-add obsoletes. We wait to avoid having to deal with
+    // the visit-weaks overwriting stuff and generally making things difficult. Since we have the
+    // mutator_lock_ and gc-paused throughout this whole process no threads should be able to see
+    // the interval where the objects are not tagged.
+    env->object_tag_table->Unlock();
+  });
   // Handle weak-refs.
   struct ReplaceWeaksVisitor : public art::IsMarkedVisitor {
    public:
@@ -1837,7 +1870,22 @@ jvmtiError HeapExtensions::ChangeArraySize(jvmtiEnv* env, jobject arr, jsize new
   };
   ReplaceWeaksVisitor rwv(old_arr_ptr, new_arr_ptr);
   art::Runtime::Current()->SweepSystemWeaks(&rwv);
+  // Re-add the object tags
+  gEventHandler->ForEachEnv(self, [&](ArtJvmTiEnv* env) NO_THREAD_SAFETY_ANALYSIS {
+    env->object_tag_table->Lock();
+    if (obsolete_tags.find(env) != obsolete_tags.end()) {
+      env->object_tag_table->SetLocked(old_arr_ptr, obsolete_tags[env]);
+    }
+    if (non_obsolete_tags.find(env) != obsolete_tags.end()) {
+      env->object_tag_table->SetLocked(new_arr_ptr, non_obsolete_tags[env]);
+    }
+    env->object_tag_table->Unlock();
+  });
   return OK;
+}
+
+void HeapExtensions::Register(EventHandler* eh) {
+  gEventHandler = eh;
 }
 
 }  // namespace openjdkjvmti
