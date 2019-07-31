f2e576a50e61258fa937720af558420ea6b1d9a2
Colin Cross <ccross@android.com>
2019-07-31 13:47:39 -0700

    Clean up art's Soong go files
    Replace deprecated BaseContext with BaseModuleContext.
    Use ctx.Config() instead of ctx.AConfig().
    Use existing ctx.Config() functions instead of wrappers.
    Sort imports.
    
    Test: m checkbuild
    Change-Id: Idf30996ef38f4056a93af511c607b2c24678a5fa

--- build/codegen.go
+++ build/codegen.go
@@ -19,22 +19,23 @@ package art
 // arches on the device.
 
 import (
-	"android/soong/android"
 	"sort"
 	"strings"
+
+	"android/soong/android"
 )
 
 func codegen(ctx android.LoadHookContext, c *codegenProperties, library bool) {
 	var hostArches, deviceArches []string
 
-	e := envDefault(ctx, "ART_HOST_CODEGEN_ARCHS", "")
+	e := ctx.Config().Getenv("ART_HOST_CODEGEN_ARCHS")
 	if e == "" {
 		hostArches = supportedArches
 	} else {
 		hostArches = strings.Split(e, " ")
 	}
 
-	e = envDefault(ctx, "ART_TARGET_CODEGEN_ARCHS", "")
+	e = ctx.Config().Getenv("ART_TARGET_CODEGEN_ARCHS")
 	if e == "" {
 		deviceArches = defaultDeviceCodegenArches(ctx)
 	} else {
@@ -131,11 +132,6 @@ type codegenProperties struct {
 	Codegen struct {
 		Arm, Arm64, Mips, Mips64, X86, X86_64 codegenArchProperties
 	}
-}
-
-type codegenCustomizer struct {
-	library           bool
-	codegenProperties codegenProperties
 }
 
 func defaultDeviceCodegenArches(ctx android.LoadHookContext) []string {
