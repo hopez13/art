7dcda9b0a292113967e8bdbede40c76d716d342c
Alex Light <allight@google.com>
2019-08-13 10:50:38 -0700

    WIP: Basic structural redef!
    No updating jmethod/jfieldIDs yet.
    
    Was able to add a static field.
    
    public static java.lang.Class art.Transform1975.CUR_CLASS = class art.Transform1975
    public static byte[] art.Transform1975.REDEFINED_DEX_BYTES = [B@8355c4b
    dalvikvm64 I 08-16 11:19:06 148884 148884 ti_redefine.cc:914] New field found 'new_string' (sig: Ljava/lang/String;)
    dalvikvm64 D 08-16 11:19:06 148884 148884 ti_heap.cc:1642] Updating class root!
    dalvikvm64 D 08-16 11:19:06 148884 148884 ti_heap.cc:1642] Updating class root!
    dalvikvm64 D 08-16 11:19:06 148884 148884 ti_heap.cc:1652] Updating field at offset 224 of type java.lang.Class<java.lang.Class>
    dalvikvm64 D 08-16 11:19:06 148884 148884 ti_heap.cc:1652] Updating field at offset 12 of type java.lang.Class<java.lang.reflect.Field>
    dalvikvm64 D 08-16 11:19:06 148884 148884 ti_heap.cc:1652] Updating field at offset 12 of type java.lang.Class<java.lang.reflect.Field>
    dalvikvm64 D 08-16 11:19:06 148884 148884 ti_heap.cc:1652] Updating field at offset 28 of type java.lang.Class<java.lang.Object[]>
    dalvikvm64 D 08-16 11:19:06 148884 148884 ti_heap.cc:1642] Updating class root!
    dalvikvm64 D 08-16 11:19:06 148884 148884 ti_heap.cc:1652] Updating field at offset 224 of type java.lang.Class<java.lang.Class>
    dalvikvm64 I 08-16 11:19:06 148884 148884 ti_redefine.cc:1945] Updating field dex-cache entry 8 of class java.lang.Class<art.Test1975>
    dalvikvm64 I 08-16 11:19:06 148884 148884 ti_redefine.cc:1945] Updating field dex-cache entry 9 of class java.lang.Class<art.Test1975>
    dalvikvm64 I 08-16 11:19:06 148884 148884 ti_redefine.cc:1945] Updating field dex-cache entry 0 of class java.lang.Class<art.Transform1975>
    dalvikvm64 I 08-16 11:19:06 148884 148884 ti_redefine.cc:1954] Updating meth dex-cache entry 0 of class java.lang.Class<art.Transform1975>
    dalvikvm64 I 08-16 11:19:06 148884 148884 ti_redefine.cc:1954] Updating meth dex-cache entry 1 of class java.lang.Class<art.Transform1975>
    public static java.lang.Class art.Transform1975.CUR_CLASS = class art.Transform1975
    public static byte[] art.Transform1975.REDEFINED_DEX_BYTES = [B@8355c4b
    public static java.lang.String art.Transform1975.new_string = null
    
    Test: TBD
    Bug: TBD
    
    Change-Id: I825d3a4bdb9594c0147223ae69f433ce9bbfc307

--- libdexfile/dex/modifiers.h
+++ libdexfile/dex/modifiers.h
@@ -55,6 +55,7 @@ static constexpr uint32_t kAccSkipAccessChecks =      0x00080000;  // method (ru
 // Used by a class to denote that the verifier has attempted to check it at least once.
 static constexpr uint32_t kAccVerificationAttempted = 0x00080000;  // class (runtime)
 static constexpr uint32_t kAccSkipHiddenapiChecks =   0x00100000;  // class (runtime)
+static constexpr uint32_t kAccObsoleteObject =        0x00200000;  // class (runtime)
 // This is set by the class linker during LinkInterfaceMethods. It is used by a method to represent
 // that it was copied from its declaring class into another class. All methods marked kAccMiranda
 // and kAccDefaultConflict will have this bit set. Any kAccDefault method contained in the methods_
---
+++ test/1975-hello-structural-transformation/src/art/Transform1975.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package art;
+
+import java.util.Base64;
+public class Transform1975 {
+
+  static {}
+  public static Class<?> CUR_CLASS = Transform1975.class;
+
+  /* Dex file for:
+   * package art;
+   * public class Transform1975 {
+   *  static {}
+   *  public static Class<?> CUR_CLASS;
+   *  public static byte[] REDEFINED_DEX_BYTES;
+   *  public static String new_string;
+   * }
+   */
+  public static byte[] REDEFINED_DEX_BYTES = Base64.getDecoder().decode(
+    "ZGV4CjAzNQBrmT/UE4TmXsBdCNfs5Hqwsycpt72EuT7EAwAAcAAAAHhWNBIAAAAAAAAAAAwDAAAR" +
+    "AAAAcAAAAAcAAAC0AAAAAQAAANAAAAADAAAA3AAAAAMAAAD0AAAAAQAAAAwBAACYAgAALAEAAGgB" +
+    "AABtAQAAdwEAAH8BAACKAQAAnwEAAL4BAADRAQAA5AEAAPgBAAAMAgAAIQIAADUCAAA4AgAAPAIA" +
+    "AEgCAABPAgAABAAAAAUAAAAGAAAACAAAAAkAAAAMAAAADQAAAAwAAAAFAAAAAAAAAAAAAgADAAAA" +
+    "AAAGAAoAAAAAAAQADgAAAAAAAAABAAAAAAAAAAIAAAADAAAAAgAAAAAAAAABAAAAAwAAAAAAAAAL" +
+    "AAAA9AIAANECAAAAAAAAAQAAAAAAAABgAQAABQAAABwAAABpAAAADgAAAAEAAQABAAAAZAEAAAQA" +
+    "AABwEAIAAAAOAAQADgACAA4AAyo+OwAIPGNsaW5pdD4ABjxpbml0PgAJQ1VSX0NMQVNTABNMYXJ0" +
+    "L1RyYW5zZm9ybTE5NzU7AB1MZGFsdmlrL2Fubm90YXRpb24vU2lnbmF0dXJlOwARTGphdmEvbGFu" +
+    "Zy9DbGFzczsAEUxqYXZhL2xhbmcvQ2xhc3M8ABJMamF2YS9sYW5nL09iamVjdDsAEkxqYXZhL2xh" +
+    "bmcvU3RyaW5nOwATUkVERUZJTkVEX0RFWF9CWVRFUwASVHJhbnNmb3JtMTk3NS5qYXZhAAFWAAJb" +
+    "QgAKbmV3X3N0cmluZwAFdmFsdWUAdn5+RDh7ImNvbXBpbGF0aW9uLW1vZGUiOiJkZWJ1ZyIsIm1p" +
+    "bi1hcGkiOjEsInNoYS0xIjoiYTgzNTJmMjU0ODg1MzYyY2NkOGQ5MDlkMzUyOWM2MDA5NGRkODk2" +
+    "ZSIsInZlcnNpb24iOiIxLjYuMjAtZGV2In0AAgEBDxwCFwcXAAMAAgAACQEJAQkAiIAErAIBgYAE" +
+    "yAIAAAAAAAEAAADHAgAA6AIAAAEAAAAAAAAAAAAAAAAAAADsAgAADwAAAAAAAAABAAAAAAAAAAEA" +
+    "AAARAAAAcAAAAAIAAAAHAAAAtAAAAAMAAAABAAAA0AAAAAQAAAADAAAA3AAAAAUAAAADAAAA9AAA" +
+    "AAYAAAABAAAADAEAAAEgAAACAAAALAEAAAMgAAACAAAAYAEAAAIgAAARAAAAaAEAAAQgAAABAAAA" +
+    "xwIAAAAgAAABAAAA0QIAAAMQAAACAAAA6AIAAAYgAAABAAAA9AIAAAAQAAABAAAADAMAAA==");
+}
