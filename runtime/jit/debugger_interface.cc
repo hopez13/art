acdd2cddb1754943a1250aba639f9936d6f617a0
David Srbecky <dsrbecky@google.com>
2019-08-27 16:22:33 +0100

    JIT mini-debug-info: Remove global seqlock synchronization.
    It was replaced by entry-level synchronization (CL/1108614).
    
    Test: test.py -b -r --host --jit --64
    Test: device boots
    Bug: 119800099
    Change-Id: Ica3c805b87ca35f6a0972f735d950363db7be166

--- runtime/jit/debugger_interface.cc
+++ runtime/jit/debugger_interface.cc
@@ -40,48 +40,13 @@
 //
 // See http://sourceware.org/gdb/onlinedocs/gdb/Declarations.html
 //
-// There are three ways for native tools to access the debug data safely:
+// There are two ways for native tools to access the debug data safely:
 //
 // 1) Synchronously, by setting a breakpoint in the __*_debug_register_code
 //    method, which is called after every modification of the linked list.
 //    GDB does this, but it is complex to set up and it stops the process.
 //
-// 2) Asynchronously, by monitoring the action_seqlock_.
-//   * The seqlock is a monotonically increasing counter which is incremented
-//     before and after every modification of the linked list. Odd value of
-//     the counter means the linked list is being modified (it is locked).
-//   * The tool should read the value of the seqlock both before and after
-//     copying the linked list.  If the seqlock values match and are even,
-//     the copy is consistent.  Otherwise, the reader should try again.
-//     * Note that using the data directly while is it being modified
-//       might crash the tool.  Therefore, the only safe way is to make
-//       a copy and use the copy only after the seqlock has been checked.
-//     * Note that the process might even free and munmap the data while
-//       it is being copied, therefore the reader should either handle
-//       SEGV or use OS calls to read the memory (e.g. process_vm_readv).
-//   * The seqlock can be used to determine the number of modifications of
-//     the linked list, which can be used to intelligently cache the data.
-//     Note the possible overflow of the seqlock.  It is intentionally
-//     32-bit, since 64-bit atomics can be tricky on some architectures.
-//   * The timestamps on the entry record the time when the entry was
-//     created which is relevant if the unwinding is not live and is
-//     postponed until much later.  All timestamps must be unique.
-//   * Memory barriers are used to make it possible to reason about
-//     the data even when it is being modified (e.g. the process crashed
-//     while that data was locked, and thus it will be never unlocked).
-//     * In particular, it should be possible to:
-//       1) read the seqlock and then the linked list head pointer.
-//       2) copy the entry and check that seqlock has not changed.
-//       3) copy the symfile and check that seqlock has not changed.
-//       4) go back to step 2 using the next pointer (if non-null).
-//       This safely creates copy of all symfiles, although other data
-//       might be inconsistent/unusable (e.g. prev_, action_timestamp_).
-//   * For full conformance with the C++ memory model, all seqlock
-//     protected accesses should be atomic. We currently do this in the
-//     more critical cases. The rest will have to be fixed before
-//     attempting to run TSAN on this code.
-//
-// 3) Asynchronously, using the entry seqlocks.
+// 2) Asynchronously, using the entry seqlocks.
 //   * The seqlock is a monotonically increasing counter, which
 //     is even if the entry is valid and odd if it is invalid.
 //     It is set to even value after all other fields are set,
@@ -149,14 +114,6 @@ extern "C" {
     uint32_t action_flag_ = JIT_NOACTION;             // One of the JITAction enum values.
     const JITCodeEntry* relevant_entry_ = nullptr;    // The entry affected by the action.
     std::atomic<const JITCodeEntry*> head_{nullptr};  // Head of link list of all entries.
-
-    // Android-specific fields:
-    uint8_t magic_[8] = {'A', 'n', 'd', 'r', 'o', 'i', 'd', '1'};
-    uint32_t flags_ = 0;  // Reserved for future use. Must be 0.
-    uint32_t sizeof_descriptor = sizeof(JITDescriptorPublic);
-    uint32_t sizeof_entry = sizeof(JITCodeEntryPublic);
-    std::atomic_uint32_t action_seqlock_{0};  // Incremented before and after any modification.
-    uint64_t action_timestamp_ = 1;           // CLOCK_MONOTONIC time of last action.
   };
 
   // Implementation-specific fields (which can be used only in this file).
@@ -236,29 +193,6 @@ ArrayRef<const uint8_t> GetJITCodeEntrySymFile(const JITCodeEntry* entry) {
   return ArrayRef<const uint8_t>(entry->symfile_addr_, entry->symfile_size_);
 }
 
-// Ensure the timestamp is monotonically increasing even in presence of low
-// granularity system timer.  This ensures each entry has unique timestamp.
-template<class NativeInfo>
-static uint64_t GetTimestamp() {
-  return std::max(NativeInfo::Descriptor().action_timestamp_ + 1, NanoTime());
-}
-
-// Mark the descriptor as "locked", so native tools know the data is being modified.
-static void ActionSeqlock(JITDescriptor& descriptor) {
-  DCHECK_EQ(descriptor.action_seqlock_.load() & 1, 0u) << "Already locked";
-  descriptor.action_seqlock_.fetch_add(1, std::memory_order_relaxed);
-  // Ensure that any writes within the locked section cannot be reordered before the increment.
-  std::atomic_thread_fence(std::memory_order_release);
-}
-
-// Mark the descriptor as "unlocked", so native tools know the data is safe to read.
-static void ActionSequnlock(JITDescriptor& descriptor) {
-  DCHECK_EQ(descriptor.action_seqlock_.load() & 1, 1u) << "Already unlocked";
-  // Ensure that any writes within the locked section cannot be reordered after the increment.
-  std::atomic_thread_fence(std::memory_order_release);
-  descriptor.action_seqlock_.fetch_add(1, std::memory_order_relaxed);
-}
-
 // This must be called with the appropriate lock taken (g_{jit,dex}_debug_lock).
 template<class NativeInfo>
 static const JITCodeEntry* CreateJITCodeEntryInternal(
@@ -315,16 +249,12 @@ static const JITCodeEntry* CreateJITCodeEntryInternal(
   writable_entry->seqlock_.fetch_add(1, std::memory_order_release);  // Mark as valid.
 
   // Add the entry to the main link-list.
-  ActionSeqlock(descriptor);
   if (head->load() != nullptr) {
     NativeInfo::Writable(head->load())->prev_ = entry;
   }
   head->store(entry, std::memory_order_release);
   descriptor.relevant_entry_ = entry;
   descriptor.action_flag_ = JIT_REGISTER_FN;
-  descriptor.action_timestamp_ = GetTimestamp<NativeInfo>();
-  ActionSequnlock(descriptor);
-
   NativeInfo::NotifyNativeDebugger();
 
   return entry;
@@ -337,7 +267,6 @@ static void DeleteJITCodeEntryInternal(const JITCodeEntry* entry) {
   JITDescriptor& descriptor = NativeInfo::Descriptor();
 
   // Remove the entry from the main linked-list.
-  ActionSeqlock(descriptor);
   const JITCodeEntry* next = entry->next_.load();
   if (entry->prev_ != nullptr) {
     NativeInfo::Writable(entry->prev_)->next_.store(next);
@@ -349,9 +278,6 @@ static void DeleteJITCodeEntryInternal(const JITCodeEntry* entry) {
   }
   descriptor.relevant_entry_ = entry;
   descriptor.action_flag_ = JIT_UNREGISTER_FN;
-  descriptor.action_timestamp_ = GetTimestamp<NativeInfo>();
-  ActionSequnlock(descriptor);
-
   NativeInfo::NotifyNativeDebugger();
 
   // Delete the entry.
