5bc64afc71b3f7a38d181337d57a19d7b1b3aaa5
Hans Boehm <hboehm@google.com>
2019-08-30 16:14:32 -0700

    Add spin loop to mutex, overhaul monitor.
    DO NOT SUBMIT. The new test will fail since it currently generates
    unexpected timing output.
    
    Since Linux context switch overhead is typically larger than a
    microsecond, this may greatly reduce the overhead of waiting for a
    mutex that is only briefly held by another thread. Rather than going
    to sleep and having to be woken back up again, at a cost of several
    microseconds, we just spin, hopefully for much less than microsecond,
    until the mutex becomes available. It does waste some CPU cycles
    when spinning fails, either because the lock is held too long, or
    we are being scheduled against the thread holding the lock. But we
    expect those to be unlikely.
    
    We test the lock only a few times, with pauses in between. It's
    unclear that's beneficial; we should perhaps just loop reading
    the variable.
    
    Add a test that mutual exclusion works, which can also be run as
    lock microbenchmark.
    
    The old monitor implementation did not benefit much from this, because
    it used mutex only as a low-level lock to protect the monitor data
    structure. Instead use monitor_lock_ as the actual lock protecting the
    data structure, i.e. hold onto it while the monitor is fatlocked.
    Among other things, this requires that the monitor_lock_ always
    be acquired by, or explicitly on behalf of, the thread holding the
    monitor.
    
    This in turn makes it really hard to deflate a monitor held by another
    thread. Just stop doing that, since it was unclear whether that's
    actually beneficial.
    
    Very tentatively, this appears to get us about a decimal order of
    magnitude in inflated contended locking performance.
    
    Bug: 111835365
    Test: Successfully built and ran 2029-contended-monitors. More coming.
    Change-Id: Iff7457fff59efcb24e25d35a4ef71b67b8a9082a

--- runtime/monitor-inl.h
+++ runtime/monitor-inl.h
@@ -29,6 +29,52 @@ inline ObjPtr<mirror::Object> Monitor::GetObject() REQUIRES_SHARED(Locks::mutato
   return obj_.Read<kReadBarrierOption>();
 }
 
+// Lock monitor lock n more times.
+void Monitor::LockMonitorLock(Thread* thread, int n) NO_THREAD_SAFETY_ANALYSIS {
+  // Since this only adjusts the number of times a lock is held, we pretend it
+  // doesn't acquire any locks.
+  // The expected value of n is zero; the obvious inefficiency doesn't matter.
+  for (int i = 0; i < n; ++i) {
+    monitor_lock_.Lock(thread);
+  }
+}
+
+// Unlock monitor n times, but not completely.
+void Monitor::UnlockMonitorLock(Thread* thread, int n) NO_THREAD_SAFETY_ANALYSIS {
+  // We lie about locking behavior as in UnlockMonitorLock().
+  for (int i = 0; i < n; ++i) {
+    monitor_lock_.Unlock(thread);
+  }
+}
+
+uintptr_t Monitor::LockOwnerInfoChecksum(ArtMethod* m, uint32_t dex_pc) {
+  uintptr_t dpc = static_cast<uintptr_t>(dex_pc);
+  return reinterpret_cast<uintptr_t>(m) ^ dpc ^ (dpc << (/* ptr_size / 2 */ (sizeof m) << 2));
+}
+
+void Monitor::SetLockOwnerInfo(ArtMethod* method, uint32_t dex_pc) REQUIRES(monitor_lock_) {
+  lock_owner_method_.store(method, std::memory_order_relaxed);
+  lock_owner_dex_pc_.store(dex_pc, std::memory_order_relaxed);
+  lock_owner_sum_.store(LockOwnerInfoChecksum(method, dex_pc), std::memory_order_relaxed);
+}
+
+void Monitor::GetLockOwnerInfo(/*out*/ArtMethod** method, /*out*/uint32_t* dex_pc) {
+  ArtMethod* owners_method;
+  uint32_t owners_dex_pc;
+  uintptr_t owners_sum;
+  do {
+    owners_method = lock_owner_method_.load(std::memory_order_relaxed);
+    if (owners_method == nullptr) {
+      break;
+    }
+    owners_dex_pc = lock_owner_dex_pc_.load(std::memory_order_relaxed);
+    owners_sum = lock_owner_sum_.load(std::memory_order_relaxed);
+  } while (owners_sum != LockOwnerInfoChecksum(owners_method, owners_dex_pc));
+  *method = owners_method;
+  *dex_pc = (owners_method == nullptr ? 0 : owners_dex_pc);
+}
+
+
 }  // namespace art
 
 #endif  // ART_RUNTIME_MONITOR_INL_H_
