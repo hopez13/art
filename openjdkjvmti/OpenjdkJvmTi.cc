effbb0b9d2d28a652a14face0bc3bc72bbad6112
Alex Light <allight@google.com>
2019-09-24 10:46:24 -0700

    WIP: Just the consolidate stuff
    Test: TBD
    Bug: TBD
    Change-Id: Ic5fc1db7db0a30f947a1a67259dc024e149ebd57

--- openjdkjvmti/OpenjdkJvmTi.cc
+++ openjdkjvmti/OpenjdkJvmTi.cc
@@ -1535,6 +1535,8 @@ extern "C" bool ArtPlugin_Initialize() {
   HeapExtensions::Register(gEventHandler);
   SearchUtil::Register();
   HeapUtil::Register();
+  FieldUtil::Register(gEventHandler);
+  BreakpointUtil::Register(gEventHandler);
   Transformer::Setup();
 
   {
@@ -1558,6 +1560,8 @@ extern "C" bool ArtPlugin_Deinitialize() {
   MethodUtil::Unregister();
   SearchUtil::Unregister();
   HeapUtil::Unregister();
+  FieldUtil::Unregister();
+  BreakpointUtil::Unregister();
 
   // TODO It would be good to delete the gEventHandler and gDeoptManager here but we cannot since
   // daemon threads might be suspended and we want to make sure that even if they wake up briefly
