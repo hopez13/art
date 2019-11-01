534cb7af3598963242d9a9f4b256d05860c57bce
Vladimir Marko <vmarko@google.com>
2019-11-01 14:59:36 +0000

    Fix boot image extension relocation.
    Store boot image begin and size to the extension header as
    it is needed for relocation; this requires rewriting the
    ImageHeader::IsAppImage(). Also mark the class roots as
    visited to satisfy a DCHECK() for extension relocation.
    
    And re-enable loading boot image extensions for tests.
    
    Test: Add relocation tests to dex2oat_image_test.
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing --relocate --no-relocate
    Test: testrunner.py --target --64 --optimizing --relocate
    Bug: 119800099
    Change-Id: Ie7e883beaa927ca4dc91f0ae660c8de74f7d4ddb

--- runtime/image.cc
+++ runtime/image.cc
@@ -93,6 +93,12 @@ void ImageHeader::RelocateBootImageReferences(int64_t delta) {
   }
 }
 
+bool ImageHeader::IsAppImage() const {
+  // Unlike boot image and boot image extensions, app images are loaded separately from
+  // oat files and their reservation size is the image size rounded up to full page.
+  return image_reservation_size_ == RoundUp(image_size_, kPageSize);
+}
+
 bool ImageHeader::IsValid() const {
   if (memcmp(magic_, kImageMagic, sizeof(kImageMagic)) != 0) {
     return false;
