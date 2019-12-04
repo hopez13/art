571c1bc82fa363ec9fd2de5c42cc906231889922
Nicolas Geoffray <ngeoffray@google.com>
2019-12-04 16:18:15 +0000

    Introduce the notion of an nterp frame.
    See comments in nterp_helpers.cc. An nterp frame follows the
    calling conventions and exception handling of the compiler. There are
    no ManagedStack transitions, and the compiler and interpreter can
    just call each other directly.
    
    For the sack walker, an nterp frame looks like a compiled frame.
    
    This CL introduces an nterp frame, another CL will contain an
    implementation for x64.
    
    Bug: 119800099
    Test: test.py
    Change-Id: Ie9b691f58908b7f283b4cd63b84b651526155d27

--- runtime/check_reference_map_visitor.h
+++ runtime/check_reference_map_visitor.h
@@ -40,7 +40,11 @@ class CheckReferenceMapVisitor : public StackVisitor {
       CHECK_EQ(GetDexPc(), dex::kDexNoIndex);
     }
 
-    if (m == nullptr || m->IsNative() || m->IsRuntimeMethod() || IsShadowFrame()) {
+    if (m == nullptr ||
+        m->IsNative() ||
+        m->IsRuntimeMethod() ||
+        IsShadowFrame() ||
+        !GetCurrentOatQuickMethodHeader()->IsOptimized()) {
       return true;
     }
 
