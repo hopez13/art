f4707eb987654b0bec122eec035947a9976ec192
Mathieu Chartier <mathieuc@google.com>
2019-12-21 21:07:56 -0800

    WIP: Inline TLAB experiment
    Bug: 139805154
    Test: manual
    
    Change-Id: I232633e7e177983f3c4d8c04ef1acfbe2e886893

--- tools/compile-jar.sh
+++ tools/compile-jar.sh
@@ -31,10 +31,11 @@ shift
 ISA=$1
 shift
 
+IMG_DIR="${OUT}/apex/com.android.art/javalib"
 dex2oat \
     --runtime-arg -Xms64m --runtime-arg -Xmx512m \
-    --boot-image=${OUT}/system/framework/boot.art \
-    `$ANDROID_BUILD_TOP/art/tools/host_bcp.sh $OUT/system/framework/$ISA/boot.oat --use-first-dir` \
+    --boot-image=${IMG_DIR}/boot.art \
+    `$ANDROID_BUILD_TOP/art/tools/host_bcp.sh ${IMG_DIR}/$ISA/boot.oat --use-first-dir` \
     --dex-file=${FILE} --dex-location=/system/framework/${FILE} \
     --oat-file=${OUTPUT} \
     --android-root=${OUT}/system --instruction-set=$ISA \
