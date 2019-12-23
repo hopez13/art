2662dc76813501d91db87ea135ba31e6e292d0f0
Dan Zimmerman <danzimm@fb.com>
2019-12-23 06:59:06 -0800

    [art] Introduce BaseDexClassLoader.computeClassLoaderContextsNative
    This will be used to compute the contexts that should be sent over to
    the dex load reporter. See associated changes in libcore &
    frameworks/base.
    
    Motivation: At the moment of committing there are two classloader
    context encoders- one in ART and one in the package manager. The
    duplicate logic is susceptible to divergences. For example at the moment
    if a package uses shared libraries and has secondary dex files then the
    context encoded for secondary dex files will be incorrect[1]. In order to
    eliminate this bug and future possible bugs lets centralize where all
    classloader context computation is done.
    
    [1]: The context will be incorrect because it doesn't take into account
    the shared libraries that are loaded at runtime.
    
    Test: m test-art-host-gtest-class_loader_context_test
    Test: m test-art-host-gtest
    Test: ./test/testrunner/testrunner.py --host -b
    Test: Introduced a set of tests for the new API(s)
    Test: See tests in associated libcore & framework/base commits
    
    Change-Id: Id39293a2e1d3d05194f2864f4febb3e652bce075

---
+++ runtime/native/dalvik_system_BaseDexClassLoader.cc
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "dalvik_system_DexFile.h"
+
+#include <memory>
+
+#include "class_loader_context.h"
+#include "native_util.h"
+#include "nativehelper/jni_macros.h"
+#include "well_known_classes.h"
+
+namespace art {
+
+static jobjectArray BaseDexClassLoader_computeClassLoaderContextsNative(JNIEnv* env,
+                                                                        jobject class_loader) {
+  CHECK(class_loader != nullptr);
+  constexpr const char* kBaseDir = "";
+  std::unique_ptr<ClassLoaderContext> clc =
+      ClassLoaderContext::CreateContextForClassLoader(class_loader, nullptr);
+  if (clc == nullptr) {
+    return nullptr;
+  }
+
+  std::vector<std::string> contexts = clc->EncodeClassPathContexts(kBaseDir);
+  jobjectArray result = env->NewObjectArray(contexts.size(),
+                                            WellKnownClasses::java_lang_String,
+                                            nullptr);
+  if (result == nullptr) {
+    DCHECK(env->ExceptionCheck());
+    return nullptr;
+  }
+  uint32_t i = 0;
+  for (const std::string& context : contexts) {
+    ScopedLocalRef<jstring> jcontext(env, env->NewStringUTF(context.c_str()));
+    if (jcontext.get() == nullptr) {
+      return nullptr;
+    }
+    env->SetObjectArrayElement(result, i, jcontext.get());
+    i += 1;
+  }
+  return result;
+}
+
+static JNINativeMethod gMethods[] = {
+  NATIVE_METHOD(BaseDexClassLoader, computeClassLoaderContextsNative,
+                "()[Ljava/lang/String;"),
+};
+
+void register_dalvik_system_BaseDexClassLoader(JNIEnv* env) {
+  REGISTER_NATIVE_METHODS("dalvik/system/BaseDexClassLoader");
+}
+
+}  // namespace art
