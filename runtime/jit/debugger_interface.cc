f921ace74683d5533d94de787596927d5262f782
David Srbecky <dsrbecky@google.com>
2020-02-20 20:27:58 +0000

    Refactor RemoveNativeDebugInfoForJit.
    This is partial revert of CL/1099280 (Remove global maps).
    
    It somewhat resurrects the lazy method removal.
    
    The original goal was to only remove methods from the GC,
    and do all of them in bulk for simplicity and efficiency.
    
    However, this is proving infeasible since we have several
    corner cases which remove methods outside the GC code path.
    
    The behaviour for the GC code path is preserved by this CL.
    Instead of passing method array, the methods are individually
    marked for removal and then repacking is immediately forced.
    The only difference is that coroner cases are done lazily.
    
    Test: ./art/test.py -b -r --host --jit --64
    Change-Id: I42729545d6b51df788d92f9cf149a6e065b90c68

--- runtime/jit/debugger_interface.cc
+++ runtime/jit/debugger_interface.cc
@@ -549,11 +549,29 @@ static void RepackEntries(bool compress_entries, ArrayRef<const void*> removed)
   g_jit_num_unpacked_entries = 0;
 }
 
+// Used only in debug builds to check that we are not adding duplicate entries.
+static std::unordered_set<const void*> g_dcheck_all_jit_functions GUARDED_BY(g_jit_debug_lock);
+
+// Methods that have been marked for deletion on the next repack pass.
+static std::vector<const void*> g_removed_jit_functions GUARDED_BY(g_jit_debug_lock);
+
+void RepackNativeDebugInfoForJitLocked() REQUIRES(g_jit_debug_lock);
+
 void AddNativeDebugInfoForJit(const void* code_ptr,
                               const std::vector<uint8_t>& symfile,
                               bool allow_packing) {
   MutexLock mu(Thread::Current(), g_jit_debug_lock);
   DCHECK_NE(symfile.size(), 0u);
+  if (kIsDebugBuild && code_ptr != nullptr) {
+    DCHECK(g_dcheck_all_jit_functions.insert(code_ptr).second) << code_ptr << " already added";
+  }
+
+  // Remove all methods which have been marked for removal.  The JIT GC should
+  // force repack, so this should happen only rarely for various corner cases.
+  // Must be done before addition in case the added code_ptr is in the removed set.
+  if (!g_removed_jit_functions.empty()) {
+    RepackNativeDebugInfoForJitLocked();
+  }
 
   CreateJITCodeEntryInternal<JitNativeInfo>(ArrayRef<const uint8_t>(symfile),
                                             /*addr=*/ code_ptr,
@@ -575,9 +593,20 @@ void AddNativeDebugInfoForJit(const void* code_ptr,
   }
 }
 
-void RemoveNativeDebugInfoForJit(ArrayRef<const void*> removed) {
+void RemoveNativeDebugInfoForJit(const void* code_ptr) {
   MutexLock mu(Thread::Current(), g_jit_debug_lock);
-  RepackEntries(/*compress_entries=*/ true, removed);
+  g_dcheck_all_jit_functions.erase(code_ptr);
+
+  // Method removal is very expensive since we need to decompress and read ELF files.
+  // Collet methods to be removed and do the removal in bulk later.
+  g_removed_jit_functions.push_back(code_ptr);
+}
+
+void RepackNativeDebugInfoForJitLocked() {
+  // Remove entries which are inside packed and compressed ELF files.
+  std::vector<const void*>& removed = g_removed_jit_functions;
+  std::sort(removed.begin(), removed.end());
+  RepackEntries(/*compress_entries=*/ true, ArrayRef<const void*>(removed));
 
   // Remove entries which are not allowed to be packed (containing single method each).
   for (const JITCodeEntry* it = __jit_debug_descriptor.head_; it != nullptr;) {
@@ -587,6 +616,14 @@ void RemoveNativeDebugInfoForJit(ArrayRef<const void*> removed) {
     }
     it = next;
   }
+
+  removed.clear();
+  removed.shrink_to_fit();
+}
+
+void RepackNativeDebugInfoForJit() {
+  MutexLock mu(Thread::Current(), g_jit_debug_lock);
+  RepackNativeDebugInfoForJitLocked();
 }
 
 size_t GetJitMiniDebugInfoMemUsage() {
