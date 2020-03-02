3a779c3daa41365a2e75e7635586320c8da1d530
Jooyung Han <jooyung@google.com>
2020-03-03 00:46:50 +0900

    Loading JNI libraries in an APEX
    To load JNI libraries in an APEX, libnativeloader relies on
    jni.config.txt file which contains lines of available JNI libraries
    like:
    
      com_android_foo libfoo_jni.so:...
      com_android_bar libbar_jni.so:...
    
    This file is generated by linkerconfig when it generates ld.config.txt.
    
    Bug: 143733063
    Test: cuttlestone boots
          (For now, no behavioral changes because jni.config.txt is empty)
    Change-Id: I066de90a73875118be53972e50d076061922d762

--- libnativeloader/public_libraries.h
+++ libnativeloader/public_libraries.h
@@ -18,6 +18,7 @@
 #define ART_LIBNATIVELOADER_PUBLIC_LIBRARIES_H_
 
 #include <algorithm>
+#include <optional>
 #include <string>
 
 #include <android-base/result.h>
@@ -41,6 +42,7 @@ const std::string& llndk_libraries_product();
 const std::string& llndk_libraries_vendor();
 const std::string& vndksp_libraries_product();
 const std::string& vndksp_libraries_vendor();
+std::optional<std::string> apex_jni_libraries(const std::string& apex_name);
 
 // Returns true if libnativeloader is running on devices and the device has
 // ro.product.vndk.version property. It returns false for host.
