f05e20d8d86a626f84f14d0b2c1b0ebabd3faeab
Shin Wang <shinwang@google.com>
2020-05-08 02:49:38 +0000

    Revert ""Move ICU from ART APEX to i18n APEX" Attempt 2"
    Revert ""Move core-icu4j into I18n APEX" Attempt 2"
    
    Revert ""Move v8 and libpac into i18n APEX" Attempt 2"
    
    Revert ""Move ICU from ART APEX into i18n APEX" Attempt 2"
    
    Revert ""Move core-icu4j into i18n APEX" Attempt 2"
    
    Revert ""Move libpac into i18n APEX" Attempt 2"
    
    Revert ""Make com_android_i18n namespace visible" Attempt 2"
    
    Revert submission 1302774-move_icu
    
    Reason for revert: Droidcop-triggered revert due to breakage https://android-build.googleplex.com/builds/quarterdeck?branch=git_master-without-vendor&target=aosp_arm64-eng&lkgb=6470873&lkbb=6471768&fkbb=6471768, bug https://buganizer.corp.google.com/issues/156043360.
    Reverted Changes:
    I446bbfdb0:"Move ICU from ART APEX into i18n APEX" Attempt 2
    Idb3e2450f:"Move ICU from ART APEX to i18n APEX" Attempt 2
    Iba05e6992:"Make com_android_i18n namespace visible" Attempt ...
    If77331626:"Move core-icu4j into i18n APEX" Attempt 2
    I9e03b5c4d:"Move v8 and libpac into i18n APEX" Attempt 2
    I076c41260:"Move ICU from ART APEX into i18n APEX" Attempt 2
    Ia71f57fc1:"Move core-icu4j into I18n APEX" Attempt 2
    Ib94b4ca25:"Move libpac into i18n APEX" Attempt 2
    
    Change-Id: Ic60e7211be8acc6fde85311eb9f40e30daf39ed0

--- libartbase/base/file_utils.h
+++ libartbase/base/file_utils.h
@@ -29,7 +29,6 @@ namespace art {
 
 static constexpr const char* kAndroidArtApexDefaultPath = "/apex/com.android.art";
 static constexpr const char* kAndroidConscryptApexDefaultPath = "/apex/com.android.conscrypt";
-static constexpr const char* kAndroidI18nApexDefaultPath = "/apex/com.android.i18n";
 
 // These methods return the Android Root, which is the historical location of
 // the Android "system" directory, containing the built Android artifacts. On
@@ -103,9 +102,6 @@ bool LocationIsOnArtModule(const char* location);
 
 // Return whether the location is on /apex/com.android.conscrypt
 bool LocationIsOnConscryptModule(const char* location);
-
-// Return whether the location is on /apex/com.android.i18n
-bool LocationIsOnI18nModule(const char* location);
 
 // Return whether the location is on system (i.e. android root).
 bool LocationIsOnSystem(const char* location);
