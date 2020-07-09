ed8049984ce2477ea5805f8f62137297afa54c7f
Alex Light <allight@google.com>
2020-07-09 13:24:56 -0700

    Partial LSE analysis & store removal
    This is the first piece of partial LSE for art. This CL adds analysis
    tools needed to implement partial LSE. More immediately, it improves
    LSE so that it will remove stores that are provably non-observable
    based on the location they occur. For example:
    
    ```
    Foo o = new Foo();
    if (xyz) {
      check(foo);
      foo.x++;
    } else {
      foo.x = 12;
    }
    return foo.x;
    ```
    
    The store of 12 can be removed because the only escape in this method
    is unreachable and was not executed by the point we reach the store.
    
    The main purpose of this CL is to add the analysis tools needed to
    implement partial Load-Store elimination. Namely it includes tracking
    of which blocks are escaping and the groups of blocks that we cannot
    remove allocations from.
    
    The actual impact of this change is incredibly minor, being triggered
    only once in a AOSP code. go/lem shows only minor effects to
    compile-time and no effect on the compiled code. There is a
    non-trivial increase in peak memory use.
    
    This CL adds a new 'reachability' concept to the HGraph. If this has
    been calculated it allows one to quickly query whether there is any
    execution path containing two blocks in a given order. This is used to
    define a notion of sections of graph from which the escape of some
    allocation is inevitable.
    
    Test: art_compiler_tests
    Test: treehugger
    Bug: 67037140
    Change-Id: I0edc8d6b73f7dd329cb1ea7923080a0abe913ea6

--- compiler/optimizing/load_store_analysis.cc
+++ compiler/optimizing/load_store_analysis.cc
@@ -15,12 +15,217 @@
  */
 
 #include "load_store_analysis.h"
+#include "exec_utils.h"
 
 namespace art {
 
 // A cap for the number of heap locations to prevent pathological time/space consumption.
 // The number of heap locations for most of the methods stays below this threshold.
 constexpr size_t kMaxNumberOfHeapLocations = 32;
+
+// Removes sink nodes.
+void ExecutionSubgraph::Prune() {
+  if (!valid_) {
+    return;
+  }
+  needs_prune_ = false;
+  // Grab blocks further up the tree.
+  ScopedArenaVector<std::optional<std::bitset<kMaxFilterableSuccessors>>> results(
+      graph_->GetBlocks().size(), allocator_->Adapter(kArenaAllocLSA));
+  ArenaBitVector visiting(allocator_, graph_->GetBlocks().size(), false, kArenaAllocLSE);
+  unreachable_blocks_vec_.ClearAllBits();
+  // TODO We should support infinite loops as well.
+  if (graph_->GetExitBlock() == nullptr) {
+    // Infinite loop
+    valid_ = false;
+    return;
+  }
+  results[graph_->GetExitBlock()->GetBlockId()] = ~(std::bitset<kMaxFilterableSuccessors>());
+  // Fills up the 'results' map with what we need to add to update
+  // allowed_successors to in order to prune sink nodes.
+  // NB C++ Doesn't like recursive calls of lambdas like this so just pass
+  // down the 'reaches_end' function explicitly.
+  auto reaches_end = [&](const HBasicBlock* blk, auto& reaches_end_recur) {
+    std::optional<std::bitset<kMaxFilterableSuccessors>>& maybe_result = results[blk->GetBlockId()];
+    if (visiting.IsBitSet(blk->GetBlockId())) {
+      // We are in a loop so the block is live.
+      return true;
+    } else if (maybe_result) {
+      CHECK(maybe_result->any() || unreachable_blocks_vec_.IsBitSet(blk->GetBlockId()));
+      return maybe_result->any();
+    }
+    visiting.SetBit(blk->GetBlockId());
+    // what we currently allow.
+    std::bitset<kMaxFilterableSuccessors> succ_bitmap = GetAllowedSuccessors(blk);
+    // The new allowed successors. We use visiting to break loops so we don't
+    // need to figure out how many bits to turn on.
+    maybe_result = std::bitset<kMaxFilterableSuccessors>();
+    std::bitset<kMaxFilterableSuccessors>& result = maybe_result.value();
+    for (auto [succ, i] :
+         ZipCount(MakeIterationRange(blk->GetSuccessors().begin(), blk->GetSuccessors().end()))) {
+      if (succ_bitmap.test(i) && reaches_end_recur(succ, reaches_end_recur)) {
+        result.set(i);
+      }
+    }
+    visiting.ClearBit(blk->GetBlockId());
+    bool res = result.any();
+    if (!res) {
+      // If this is a sink block it will be removed from the successors of all
+      // its predecessors and made unreachable.
+      CHECK(blk != nullptr);
+      unreachable_blocks_vec_.SetBit(blk->GetBlockId());
+    }
+    return res;
+  };
+  bool start_reaches_end = reaches_end(graph_->GetEntryBlock(), reaches_end);
+  if (!start_reaches_end) {
+    valid_ = false;
+    return;
+  }
+  for (const HBasicBlock* blk : graph_->GetBlocks()) {
+    if (blk != nullptr && !results[blk->GetBlockId()] && blk != graph_->GetEntryBlock()) {
+      // We never visited this block, must be unreachable.
+      unreachable_blocks_vec_.SetBit(blk->GetBlockId());
+    }
+  }
+  results[graph_->GetExitBlock()->GetBlockId()].reset();
+  allowed_successors_.clear();
+  for (auto [v, id] : ZipCount(MakeIterationRange(results))) {
+    if (!v) {
+      continue;
+    }
+    HBasicBlock* block = graph_->GetBlocks()[id];
+    if (v->count() != block->GetSuccessors().size()) {
+      allowed_successors_.Overwrite(block, *v);
+    }
+  }
+  RecalculateExcludedCohort();
+}
+
+void ExecutionSubgraph::RemoveConcavity() {
+  if (!valid_) {
+    return;
+  }
+  DCHECK(!needs_prune_);
+  ArenaBitVector initial(allocator_, graph_->GetBlocks().size(), false, kArenaAllocLSA);
+  initial.Copy(&unreachable_blocks_vec_);
+  for (const HBasicBlock* blk : graph_->GetBlocks()) {
+    if (blk == nullptr || initial.IsBitSet(blk->GetBlockId())) {
+      continue;
+    }
+    for (uint32_t skipped1 : initial.Indexes()) {
+      if (LIKELY(!graph_->PathBetween(skipped1, blk->GetBlockId()))) {
+        continue;
+      }
+      for (uint32_t skipped2 : initial.Indexes()) {
+        if (graph_->PathBetween(blk->GetBlockId(), skipped2)) {
+          RemoveBlock(blk);
+        }
+      }
+    }
+  }
+  Prune();
+}
+
+void ExecutionSubgraph::RecalculateExcludedCohort() {
+  DCHECK(!needs_prune_);
+  excluded_list_.emplace(allocator_->Adapter(kArenaAllocLSA));
+  ScopedArenaVector<ExcludedCohort>& res = excluded_list_.value();
+  // Make a copy of unreachable_blocks_;
+  ArenaBitVector unreachable(allocator_, graph_->GetBlocks().size(), false, kArenaAllocLSA);
+  unreachable.Copy(&unreachable_blocks_vec_);
+  // Split cohorts with union-find
+  while (unreachable.IsAnyBitSet()) {
+    res.emplace_back(allocator_, graph_);
+    ExcludedCohort& cohort = res.back();
+    // We don't allocate except for the queue beyond here so create another arena to save memory.
+    ScopedArenaAllocator alloc(graph_->GetArenaStack());
+    ScopedArenaQueue<const HBasicBlock*> worklist(alloc.Adapter(kArenaAllocLSA));
+    // Select an arbitrary node
+    const HBasicBlock* first = graph_->GetBlocks()[unreachable.GetHighestBitSet()];
+    worklist.push(first);
+    do {
+      // Flood-fill both forwards and backwards.
+      const HBasicBlock* cur = worklist.front();
+      worklist.pop();
+      if (!unreachable.IsBitSet(cur->GetBlockId())) {
+        // Already visited or reachable somewhere else.
+        continue;
+      }
+      unreachable.ClearBit(cur->GetBlockId());
+      if (cur == nullptr) {
+        continue;
+      }
+      cohort.blocks_.SetBit(cur->GetBlockId());
+      // don't bother filtering here, it's done next go-around
+      for (const HBasicBlock* pred : cur->GetPredecessors()) {
+        worklist.push(pred);
+      }
+      for (const HBasicBlock* succ : cur->GetSuccessors()) {
+        worklist.push(succ);
+      }
+    } while (!worklist.empty());
+  }
+  // Figure out entry & exit nodes.
+  for (ExcludedCohort& cohort : res) {
+    DCHECK(cohort.blocks_.IsAnyBitSet());
+    auto is_external = [&](const HBasicBlock* ext) -> bool {
+      return !cohort.blocks_.IsBitSet(ext->GetBlockId());
+    };
+    for (const HBasicBlock* blk : cohort.Blocks()) {
+      const auto& preds = blk->GetPredecessors();
+      const auto& succs = blk->GetSuccessors();
+      if (std::any_of(preds.cbegin(), preds.cend(), is_external)) {
+        cohort.entry_blocks_.SetBit(blk->GetBlockId());
+      }
+      if (std::any_of(succs.cbegin(), succs.cend(), is_external)) {
+        cohort.exit_blocks_.SetBit(blk->GetBlockId());
+      }
+    }
+  }
+}
+
+std::ostream& operator<<(std::ostream& os, const ExecutionSubgraph::ExcludedCohort& ex) {
+  ex.Dump(os);
+  return os;
+}
+
+void ExecutionSubgraph::ExcludedCohort::Dump(std::ostream &os) const {
+  auto dump = [&](BitVecBlockRange arr) {
+    os << "[";
+    bool first = true;
+    for (const HBasicBlock* b : arr) {
+      if (!first) {
+        os << ", ";
+      }
+      first = false;
+      os << b->GetBlockId();
+    }
+    os << "]";
+  };
+  auto dump_blocks = [&]() {
+    os << "[";
+    bool first = true;
+    for (const HBasicBlock* b : Blocks()) {
+      if (!entry_blocks_.IsBitSet(b->GetBlockId()) && !exit_blocks_.IsBitSet(b->GetBlockId())) {
+        if (!first) {
+          os << ", ";
+        }
+        first = false;
+        os << b->GetBlockId();
+      }
+    }
+    os << "]";
+  };
+
+  os << "{ entry: ";
+  dump(EntryBlocks());
+  os << ", interior: ";
+  dump_blocks();
+  os << ", exit: ";
+  dump(ExitBlocks());
+  os << "}";
+}
 
 // Test if two integer ranges [l1,h1] and [l2,h2] overlap.
 // Note that the ranges are inclusive on both ends.
@@ -166,12 +371,12 @@ bool LoadStoreAnalysis::Run() {
   if (heap_location_collector_.HasVolatile() || heap_location_collector_.HasMonitorOps()) {
     // Don't do load/store elimination if the method has volatile field accesses or
     // monitor operations, for now.
-    // TODO: do it right.
     heap_location_collector_.CleanUp();
     return false;
   }
 
   heap_location_collector_.BuildAliasingMatrix();
+  heap_location_collector_.DumpReferenceStats(stats_);
   return true;
 }
 
