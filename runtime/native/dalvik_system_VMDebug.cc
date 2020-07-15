e9cdbb2d4bb281c56fca2da675e42acaa1eb611d
zhang chuang <zhangchuang3@xiaomi.com>
2020-07-15 11:06:42 +0800

    Increase region space statistics about allocSize of Heap
    Art currently uses a concurrent copy collector, and the corresponding allocation space uses region space.
    So when obtaining the heap state, we need to fetch the statistics of region space.
    
    Bug: 161244406
    Test: Call VMDebug_getHeapSpaceStats interface
    
    Signed-off-by: zhang chuang <zhangchuang3@xiaomi.com>
    Change-Id: I07c00a23eeadde6cd1c3e3fb3388d43be74c99d4

--- runtime/native/dalvik_system_VMDebug.cc
+++ runtime/native/dalvik_system_VMDebug.cc
@@ -415,6 +415,15 @@ static void VMDebug_getHeapSpaceStats(JNIEnv* env, jclass, jlongArray data) {
         gc::space::BumpPointerSpace* bump_pointer_space = space->AsBumpPointerSpace();
         allocSize += bump_pointer_space->Size();
         allocUsed += bump_pointer_space->GetBytesAllocated();
+      } else if (space->IsRegionSpace()) {
+        gc::space::RegionSpace* region_space = space->AsRegionSpace();
+        //when using concurrent copy collector, the corresponding allocation 
+        //space uses region space. 
+        //The space we actually apply for includes from space and to space, 
+        //and their sum is twice the actual available space.
+        //so here we need to divide by 2 to get the actual space size that can be used.
+        allocSize += region_space->Size() / 2;
+        allocUsed += region_space->GetBytesAllocated();
       }
     }
     for (gc::space::DiscontinuousSpace* space : heap->GetDiscontinuousSpaces()) {
