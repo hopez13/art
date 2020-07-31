77726fe05c8cb5872f478e811a00c5f61544eb26
Yo Chiang <yochiang@google.com>
2020-07-31 20:55:17 +0800

    art: Remove dependencies on platform unavailable modules
    The device variants of these tests depend on tools such as
    `dex2oatd` which are not available to the platform. The tools
    are provided by the ART APEX (com.android.art.debug / .testing).
    
    This also breaks darwin host build coincidentally, because on
    linux host build the build system confuses the host and device
    variants of `dex2oatd`, so the build system mistakenly thought
    device variant of "dex2oatd" exists (while in reality only the
    host variant exists.)
    On darwin host however, both device and host variants of
    `dex2oatd` doesn't exist, thus triggering a build break.
    
    Bug: 162102724
    Bug: 7456955
    Test: TH build mac host target
    Change-Id: I8f3435ff11f19f7191e87b0728e6d524bbd0f559

--- Android.mk
+++ Android.mk
@@ -454,15 +454,6 @@ LOCAL_IS_HOST_MODULE := true
 include $(BUILD_PHONY_PACKAGE)
 endif
 
-# ART on the target.
-ifeq ($(ART_BUILD_TARGET_DEBUG),true)
-include $(CLEAR_VARS)
-LOCAL_MODULE := art-libartd-libopenjdkd-target-dependency
-LOCAL_MULTILIB := both
-LOCAL_REQUIRED_MODULES := libopenjdkd
-include $(BUILD_PHONY_PACKAGE)
-endif
-
 ########################################################################
 # "m build-art" for quick minimal build
 .PHONY: build-art
