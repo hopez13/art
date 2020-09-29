d451267c67dc4d4277b7264994218de34498e1d5
Eric Holk <eholk@google.com>
2020-09-29 11:13:55 -0700

    [metrics] Define Counter and related data structures
    Add a definition for Counter, which is a type of metric ART can
    collect. Note that this does not yet define any particular counters,
    just the infrastructure needed to add them.
    
    Bug: 160734914
    Test: m test-art-host-gtest-art_libartbase_tests
    Change-Id: Ideabadbf3cca222911da357af56c7ebc2ab637fd

---
+++ libartbase/base/metrics.cc
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "metrics.h"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic error "-Wconversion"
+
+namespace art {
+namespace metrics {
+
+ArtMetrics::ArtMetrics()
+    :
+#define ART_COUNTER(name) name{},
+      ART_COUNTERS(ART_COUNTER)
+#undef ART_COUNTER
+              unused_{} {}
+
+void ArtMetrics::ReportAllMetrics([[maybe_unused]] MetricsBackend* backend) const {
+// TODO: remove maybe_unused once a counter is defined.
+
+// Dump counters
+#define ART_COUNTER(name) backend->ReportCounter(DatumId::name, name.value_);
+  ART_COUNTERS(ART_COUNTER)
+#undef ART_COUNTERS
+}
+
+}  // namespace metrics
+}  // namespace art
+
+#pragma clang diagnostic pop  // -Wconversion
