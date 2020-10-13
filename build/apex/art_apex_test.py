55ab0b9579a176c5ee7633b0685b0f4de883c373
Orion Hodson <oth@google.com>
2020-10-13 14:31:43 +0100

    odrefresh: regenerate artifacts after ART APEX update
    Replaces compile_bcp.sh to check and compile boot class path
    extensions and system server jars.
    
    Enable ART to load those artifacts when present in the ART APEX data
    directory.
    
    Bug: 160683548
    Test: art_libartbase_tests
    Test: adb root && adb odrefresh {--force-check,--force-compile}
    Test: adb install com.android.art && adb reboot && adb root && \
          adb shell odrefresh {--check,--compile} && adb reboot && \
          adb shell cat /proc/<zygote>/maps | grep apexdata
    Change-Id: I81bf520d38f9dc0109c91f192bc6e728099049fd

--- build/apex/art_apex_test.py
+++ build/apex/art_apex_test.py
@@ -619,9 +619,9 @@ class ReleaseTargetChecker:
     # removed in Android R.
 
     # Check binaries for ART.
-    self._checker.check_executable("compile_bcp.sh")
-    self._checker.check_executable('oatdump')
     self._checker.check_multilib_executable('dex2oat')
+    self._checker.check_executable('oatdump')
+    self._checker.check_executable("odrefresh")
 
     # Check internal libraries for ART.
     self._checker.check_native_library('libperfetto_hprof')
@@ -646,6 +646,7 @@ class ReleaseHostChecker:
     self._checker.check_executable('hprof-conv')
     self._checker.check_symlinked_first_executable('dex2oatd')
     self._checker.check_symlinked_first_executable('dex2oat')
+    self._checker.check_executable("odrefresh")
 
     # Check exported native libraries for Managed Core Library.
     self._checker.check_native_library('libicu')
