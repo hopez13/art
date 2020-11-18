2ec190434510ddc30883a9bad7f7f89a320210f3
Alex Light <allight@google.com>
2020-11-18 12:23:48 -0800

    Make RTP::Visit robust against input order
    ReferenceTypePropogation::Visit(ArrayRef) relied on the input having a
    particular order with known values at the front then topological. This
    could cause issues if the list was not properly sorted, causing the
    RTP to silently fail. This makes RTP robust against the ordering of
    inputs for this function.
    
    Test: ./test.py --host
    Bug: 67037140
    Change-Id: I03c522ea745f271ce438c82f7c6f3ab476c8249a

--- compiler/optimizing/reference_type_propagation_test.cc
+++ compiler/optimizing/reference_type_propagation_test.cc
@@ -16,7 +16,12 @@
 
 #include "reference_type_propagation.h"
 
+
+#include <random>
+
 #include "base/arena_allocator.h"
+#include "base/transform_array_ref.h"
+#include "base/transform_iterator.h"
 #include "builder.h"
 #include "nodes.h"
 #include "object_lock.h"
@@ -42,6 +47,11 @@ class ReferenceTypePropagationTest : public CommonCompilerTest, public Optimizin
                                                                  true,
                                                                  "test_prop");
   }
+
+  template <typename Func>
+  void RunVisitListTest(Func mutator);
+
+  void SortTopological(std::vector<HInstruction*>& inst);
 
   // Relay method to merge type in reference type propagation.
   ReferenceTypeInfo MergeTypes(const ReferenceTypeInfo& a,
@@ -155,6 +165,123 @@ TEST_F(ReferenceTypePropagationTest, MergeValidTypes) {
   EXPECT_TRUE(t7.IsValid());
   EXPECT_FALSE(t7.IsExact());
   EXPECT_TRUE(t7.IsEqual(ObjectType(false)));
+}
+
+// This generates a large graph with a ton of phis. It then calls the 'mutator'
+// function with the list of all the phis and then tries to propagate the types.
+// mutator should reorder the list in some way. We verify everything worked by
+// making sure every phi has valid type information.
+template <typename Func>
+void ReferenceTypePropagationTest::RunVisitListTest(Func mutator) {
+  ScopedObjectAccess soa(Thread::Current());
+  VariableSizedHandleScope handles(soa.Self());
+  SetupPropagation(&handles);
+  // Make a well-connected graph with a lot of edges.
+  constexpr size_t kNumBlocks = 5000;
+  constexpr size_t kTestMaxSuccessors = 3;
+  std::vector<std::string> mid_blocks;
+  for (auto i : Range(kNumBlocks)) {
+    std::ostringstream oss;
+    oss << "blk" << i;
+    mid_blocks.push_back(oss.str());
+  }
+  // Create the edge list.
+  std::vector<AdjacencyListGraph::Edge> edges;
+  for (auto cur : Range(kNumBlocks)) {
+    for (auto nxt : Range(cur + 1, std::min(cur + 1 + kTestMaxSuccessors, kNumBlocks))) {
+      edges.emplace_back(mid_blocks[cur], mid_blocks[nxt]);
+    }
+  }
+  AdjacencyListGraph alg(graph_, GetAllocator(), mid_blocks.front(), mid_blocks.back(), edges);
+  std::unordered_map<HBasicBlock*, HInstruction*> single_value;
+  // Setup the entry-block with the type to be propagated.
+  HInstruction* cls =
+      new (GetAllocator()) HLoadClass(graph_->GetCurrentMethod(),
+                                      dex::TypeIndex(10),
+                                      graph_->GetDexFile(),
+                                      graph_->GetHandleCache()->GetObjectClassHandle(),
+                                      false,
+                                      0,
+                                      false);
+  HInstruction* new_inst =
+      new (GetAllocator()) HNewInstance(cls,
+                                        0,
+                                        dex::TypeIndex(10),
+                                        graph_->GetDexFile(),
+                                        false,
+                                        QuickEntrypointEnum::kQuickAllocObjectInitialized);
+  single_value[alg.Get(mid_blocks.front())] = new_inst;
+  HBasicBlock* start = alg.Get(mid_blocks.front());
+  start->AddInstruction(cls);
+  start->AddInstruction(new_inst);
+  new_inst->SetReferenceTypeInfo(ObjectType(true));
+
+  // Setup all the other blocks with a single PHI
+  auto succ_blk_names = MakeIterationRange(mid_blocks.begin() + 1, mid_blocks.end());
+  auto succ_blocks =
+      MakeTransformRange(succ_blk_names, [&](const auto& sv) { return alg.Get(sv); });
+  for (HBasicBlock* blk : succ_blocks) {
+    HPhi* phi_inst = new (GetAllocator()) HPhi(
+        GetAllocator(), kNoRegNumber, blk->GetPredecessors().size(), DataType::Type::kReference);
+    single_value[blk] = phi_inst;
+  }
+  for (HBasicBlock* blk : succ_blocks) {
+    HInstruction* my_val = single_value[blk];
+    for (const auto& [pred, index] : ZipCount(MakeIterationRange(blk->GetPredecessors()))) {
+      my_val->SetRawInputAt(index, single_value[pred]);
+    }
+    blk->AddPhi(my_val->AsPhi());
+  }
+  auto vals = MakeTransformRange(succ_blocks, [&](HBasicBlock* blk) { return single_value[blk]; });
+  std::vector<HInstruction*> ins(vals.begin(), vals.end());
+  graph_->ClearReachabilityInformation();
+  graph_->ComputeReachabilityInformation();
+  mutator(ins);
+  propagation_->Visit(ArrayRef<HInstruction* const>(ins));
+  for (auto [blk, i] : single_value) {
+    if (blk == start) {
+      continue;
+    }
+    EXPECT_TRUE(i->GetReferenceTypeInfo().IsValid())
+        << i->GetId() << " blk: " << alg.GetName(i->GetBlock());
+  }
+}
+
+void ReferenceTypePropagationTest::SortTopological(std::vector<HInstruction *>& lst) {
+  std::sort(lst.begin(), lst.end(), [&](HInstruction* a, HInstruction* b) {
+    return a->GetReferenceTypeInfo().IsValid() ||
+            std::any_of(a->GetInputs().begin(),
+                        a->GetInputs().end(),
+                        [&](HInputsRef::reference ref) {
+                          return ref->GetReferenceTypeInfo().IsValid();
+                        }) ||
+            graph_->PathBetween(a->GetBlock(), b->GetBlock());
+  });
+}
+
+TEST_F(ReferenceTypePropagationTest, VisitReverseTopological) {
+  RunVisitListTest([&](std::vector<HInstruction*>& lst) {
+    SortTopological(lst);
+    std::reverse(lst.begin(), lst.end());
+  });
+}
+TEST_F(ReferenceTypePropagationTest, VisitTopological) {
+  RunVisitListTest([&](std::vector<HInstruction*>& lst) {
+    SortTopological(lst);
+  });
+}
+
+TEST_F(ReferenceTypePropagationTest, VisitAlmostTopological) {
+  RunVisitListTest([&](std::vector<HInstruction*>& lst) {
+    SortTopological(lst);
+    std::swap(lst.front(), lst.back());
+  });
+}
+
+TEST_F(ReferenceTypePropagationTest, VisitRandom) {
+  std::default_random_engine g(std::rand());
+  RunVisitListTest(
+      [&](std::vector<HInstruction*>& lst) { std::shuffle(lst.begin(), lst.end(), g); });
 }
 
 }  // namespace art
