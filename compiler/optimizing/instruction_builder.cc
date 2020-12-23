656a10b35ba742dc4cf79dbde536ee7445d3742f
Nicolas Geoffray <ngeoffray@google.com>
2020-12-23 17:29:32 +0000

    Handle VERIFY_ERROR_INSTANTIATION in compiler and nterp.
    Nterp always supported it, but the compiler was missing a check.
    
    Test: test.py
    Test: 600-verifier-fails
    Change-Id: I8675ca6f61c68c4152212c81843da8248781f18a

--- compiler/optimizing/instruction_builder.cc
+++ compiler/optimizing/instruction_builder.cc
@@ -1264,6 +1264,7 @@ HNewInstance* HInstructionBuilder::BuildNewInstance(dex::TypeIndex type_index, u
   if (load_class->NeedsAccessCheck() ||
       klass == nullptr ||  // Finalizable/instantiable is unknown.
       klass->IsFinalizable() ||
+      klass.Get() == klass->GetClass() ||  // Classes cannot be allocated in code
       !klass->IsInstantiable()) {
     entrypoint = kQuickAllocObjectWithChecks;
   }
