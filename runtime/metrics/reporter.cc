3cf1b0f2461a5d68eec69dcbeee7cea97c1b76e5
Eric Holk <eholk@google.com>
2021-02-17 12:48:53 -0800

    Revert^2 "[metrics] Add StatsdBackend"
    Adds a new backend that allows reporting ART's metrics to statsd, where
    they can then be uploaded to Westworld.
    
    Manual Testing Instructions:
    
    Run the following commands.
    
        m statsd_testdrive
        adb shell setprop dalvik.vm.extra-opts              \
                          -Xwrite-metrics-to-statsd\\\      \
                          -Xwrite-metrics-to-log\\\         \
                          -Xmetrics-reporting-period=10
        adb shell stop && adb shell start
        statsd_testdrive 332
    
    After about a minute, you should see several atoms logged from ART.
    
    This reverts commit dbad1ef673140c66cdbcbbf40424674ae8e2b4c8.
    Reason for revert: Added statsd apex to prebuilts
    
    Bug: 178236337
    Test: Manual, see above
    Test: see instructions on https://r.android.com/1591932
    Change-Id: I2071983c04c51efe88df6a56c59fc418fe6e9424

--- runtime/metrics/reporter.cc
+++ runtime/metrics/reporter.cc
@@ -14,11 +14,12 @@
  * limitations under the License.
  */
 
-#include "metrics_reporter.h"
+#include "reporter.h"
 
 #include "base/flags.h"
 #include "runtime.h"
 #include "runtime_options.h"
+#include "statsd.h"
 #include "thread-current-inl.h"
 
 #pragma clang diagnostic push
@@ -75,6 +76,9 @@ void MetricsReporter::BackgroundThreadRun() {
   }
   if (config_.dump_to_file.has_value()) {
     backends_.emplace_back(new FileBackend(config_.dump_to_file.value()));
+  }
+  if (config_.dump_to_statsd) {
+    backends_.emplace_back(CreateStatsdBackend());
   }
 
   MaybeResetTimeout();
