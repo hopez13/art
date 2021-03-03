e8fd825865201976fe5619152b7dae5763e4bea4
Eric Holk <eholk@google.com>
2021-03-03 15:14:58 -0800

    [metrics] Enable periodic reporting for system server
    This enables reporting from system server once an hour.
    
    The reason is that system server is long-running, so startup metrics,
    which are all that are enabled by default otherwise, are not as
    meaningful. Note that the reporting period can still be overridden by
    a command line argument.
    
    Testing instructions:
    
        adb shell setprop dalvik.vm.extra-opts -Xwrite-metrics-to-log
        adb logcat -e "ART internal metrics"
    
        Observe periodic system server reports.
    
    Bug: 170149255
    Test: manual (see above)
    Change-Id: I71775e454c6fd07c4312e3df9ae8082fa497ea93

--- runtime/metrics/reporter.cc
+++ runtime/metrics/reporter.cc
@@ -37,6 +37,17 @@ MetricsReporter::MetricsReporter(ReportingConfig config, Runtime* runtime)
 
 MetricsReporter::~MetricsReporter() { MaybeStopBackgroundThread(); }
 
+bool MetricsReporter::IsPeriodicReportingEnabled() const {
+  return config_.periodic_report_seconds.has_value();
+}
+
+void MetricsReporter::SetReportingPeriod(unsigned int period_seconds) {
+  DCHECK(!thread_.has_value()) << "The reporting period should not be changed after the background "
+                                  "reporting thread is started.";
+
+  config_.periodic_report_seconds = period_seconds;
+}
+
 void MetricsReporter::MaybeStartBackgroundThread(SessionData session_data) {
   CHECK(!thread_.has_value());
 
