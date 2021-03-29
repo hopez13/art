b39a29c514cc83635833026b8bae2fa050d3891f
Chris Wailes <chriswailes@google.com>
2021-03-29 11:16:56 -0700

    Add the ArtD binder interface
    Test: None
    Bug: 177273468
    Change-Id: I7dab21636e41744dd29b43fb61aac390a94f5977

--- artd/artd.cc
+++ artd/artd.cc
@@ -18,15 +18,30 @@
 
 #include <unistd.h>
 
+#include <binder/BinderService.h>
+
+#include "android/os/BnArtd.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "tools/tools.h"
 
-namespace {
-
-class Artd {
+namespace android {
+namespace artd {
+class Artd : public BinderService<Artd>, public os::BnArtd {
  public:
   Artd(ATTRIBUTE_UNUSED const int argc, ATTRIBUTE_UNUSED char* argv[]) {}
+
+  /*
+   * Binder API
+   */
+
+  binder::Status buildTest() {
+    return binder::Status::ok();
+  }
+
+  /*
+   * Server API
+   */
 
   NO_RETURN
   void Run() {
@@ -41,13 +56,14 @@ class Artd {
   }
 };
 
-}  // namespace
+}  // namespace artd
+}  // namespace android
 
 int main(const int argc, char* argv[]) {
   setenv("ANDROID_LOG_TAGS", "*:v", 1);
   android::base::InitLogging(argv);
 
-  Artd artd(argc, argv);
+  android::artd::Artd artd(argc, argv);
 
   artd.Run();
 }
