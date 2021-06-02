f6ce0ce70f3b514f682db700aef761641e69d7f8
Ulya Trafimovich <skvadrik@google.com>
2021-06-02 17:35:16 +0100

    Factor out arch-independent ScaleFactor definition.
    Bug: 65872996
    Test: art/test.py --host -r --optimizing
    Change-Id: I27763286847b45a5a3a493c3dba48418575b3eb6

--- compiler/optimizing/code_generator_x86.cc
+++ compiler/optimizing/code_generator_x86.cc
@@ -6159,30 +6159,6 @@ void LocationsBuilderX86::VisitArrayGet(HArrayGet* instruction) {
   }
 }
 
-static ScaleFactor ScaleFactorForType(DataType::Type type) {
-  switch (type) {
-    case DataType::Type::kBool:
-    case DataType::Type::kUint8:
-    case DataType::Type::kInt8:
-      return TIMES_1;
-    case DataType::Type::kUint16:
-    case DataType::Type::kInt16:
-      return TIMES_2;
-    case DataType::Type::kInt32:
-    case DataType::Type::kUint32:
-    case DataType::Type::kFloat32:
-    case DataType::Type::kReference:
-      return TIMES_4;
-    case DataType::Type::kInt64:
-    case DataType::Type::kUint64:
-    case DataType::Type::kFloat64:
-      return TIMES_8;
-    case DataType::Type::kVoid:
-      LOG(FATAL) << "Unreachable type " << type;
-      UNREACHABLE();
-  }
-}
-
 void InstructionCodeGeneratorX86::VisitArrayGet(HArrayGet* instruction) {
   LocationSummary* locations = instruction->GetLocations();
   Location obj_loc = locations->InAt(0);
