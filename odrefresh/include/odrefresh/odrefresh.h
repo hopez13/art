d62832663ddb61b31d5baca14ff179d481dbb8e6
Jiakai Zhang <jiakaiz@google.com>
2021-08-11 08:25:40 +0000

    Add unit tests for odrefresh - step 1
    - Define `class OnDeviceRefresh` in the header file and expose some
    public methods.
    - Changed the ordering of functions. (Function bodies are
    not changed.)
    - Move static functions to the anonymous namespace, outside of the
    class.
    - Cleanup "static" keywords inside the anonymous namespace.
    - Move the `main` function and related functions about argument parsing
    and usage help to a separate file `odrefresh_main.cc`.
    - Move `Concatenate` and `QuotePath` to separate files
    `odr_common.h`/`odr_common.cc`.
    
    Bug: 196188549
    Test: m odrefresh
    Test: m odrefreshd
    Test: atest odsign_e2e_tests
    Change-Id: I0ae1cf34181ae2edf7fd2728ec90b842470d729e

--- odrefresh/include/odrefresh/odrefresh.h
+++ odrefresh/include/odrefresh/odrefresh.h
@@ -19,6 +19,17 @@
 
 #include <sysexits.h>
 
+#include <ctime>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include "com_android_apex.h"
+#include "com_android_art.h"
+#include "odr_artifacts.h"
+#include "odr_config.h"
+#include "odr_metrics.h"
+
 namespace art {
 namespace odrefresh {
 
@@ -60,6 +71,148 @@ enum ExitCode : int {
 static_assert(EX_OK == 0);
 static_assert(ExitCode::kOkay < EX__BASE);
 static_assert(ExitCode::kLastExitCode < 0xff);  // The `exit()` man page discusses the mask value.
+
+class OnDeviceRefresh final {
+ public:
+  explicit OnDeviceRefresh(const OdrConfig& config);
+
+  // Returns the exit code, a list of ISAs that boot extensions should be compiled for, and a
+  // boolean indicating whether the system server should be compiled.
+  WARN_UNUSED ExitCode
+  CheckArtifactsAreUpToDate(OdrMetrics& metrics,
+                            /*out*/ std::vector<InstructionSet>* compile_boot_extensions,
+                            /*out*/ bool* compile_system_server) const;
+
+  WARN_UNUSED ExitCode Compile(OdrMetrics& metrics,
+                               const std::vector<InstructionSet>& compile_boot_extensions,
+                               bool compile_system_server) const;
+
+  // Verify all artifacts are up-to-date.
+  //
+  // This method checks artifacts can be loaded by the runtime.
+  //
+  // Returns ExitCode::kOkay if artifacts are up-to-date, ExitCode::kCompilationRequired
+  // otherwise.
+  //
+  // NB This is the main function used by the --verify command-line option.
+  WARN_UNUSED ExitCode VerifyArtifactsAreUpToDate() const;
+
+  WARN_UNUSED bool RemoveArtifactsDirectory() const;
+
+ private:
+  time_t GetExecutionTimeUsed() const;
+
+  time_t GetExecutionTimeRemaining() const;
+
+  time_t GetSubprocessTimeout() const;
+
+  // Gets the `ApexInfo` for active APEXes.
+  std::optional<std::vector<com::android::apex::ApexInfo>> GetApexInfoList() const;
+
+  // Reads the ART APEX cache information (if any) found in `kOdrefreshArtifactDirectory`.
+  std::optional<com::android::art::CacheInfo> ReadCacheInfo() const;
+
+  // Write ART APEX cache information to `kOnDeviceRefreshOdrefreshArtifactDirectory`.
+  void WriteCacheInfo() const;
+
+  void ReportNextBootAnimationProgress(uint32_t current_compilation) const;
+
+  std::vector<com::android::art::Component> GenerateBootClasspathComponents() const;
+
+  std::vector<com::android::art::Component> GenerateBootExtensionCompilableComponents() const;
+
+  std::vector<com::android::art::Component> GenerateSystemServerComponents() const;
+
+  static std::string GetBootImage();
+
+  std::string GetBootImageExtensionImage(bool on_system) const;
+
+  std::string GetBootImageExtensionImagePath(bool on_system, const InstructionSet isa) const;
+
+  std::string GetSystemServerImagePath(bool on_system, const std::string& jar_path) const;
+
+  WARN_UNUSED bool RemoveSystemServerArtifactsFromData() const;
+
+  // Remove boot extension artifacts from /data.
+  WARN_UNUSED bool RemoveBootExtensionArtifactsFromData(InstructionSet isa) const;
+
+  WARN_UNUSED bool RemoveArtifacts(const OdrArtifacts& artifacts) const;
+
+  // Checks whether all boot extension artifacts are present. Returns true if all are present, false
+  // otherwise.
+  WARN_UNUSED bool BootExtensionArtifactsExist(bool on_system,
+                                               const InstructionSet isa,
+                                               /*out*/ std::string* error_msg) const;
+
+  // Checks whether all system_server artifacts are present. The artifacts are checked in their
+  // order of compilation. Returns true if all are present, false otherwise.
+  WARN_UNUSED bool SystemServerArtifactsExist(bool on_system,
+                                              /*out*/ std::string* error_msg) const;
+
+  WARN_UNUSED bool CheckBootExtensionArtifactsAreUpToDate(
+      OdrMetrics& metrics,
+      const InstructionSet isa,
+      const com::android::apex::ApexInfo& art_apex_info,
+      const std::optional<com::android::art::CacheInfo>& cache_info,
+      /*out*/ bool* cleanup_required) const;
+
+  WARN_UNUSED bool CheckSystemServerArtifactsAreUpToDate(
+      OdrMetrics& metrics,
+      const std::vector<com::android::apex::ApexInfo>& apex_info_list,
+      const std::optional<com::android::art::CacheInfo>& cache_info,
+      /*out*/ bool* cleanup_required) const;
+
+  // Check the validity of boot class path extension artifacts.
+  //
+  // Returns true if artifacts exist and are valid according to dexoptanalyzer.
+  WARN_UNUSED bool VerifyBootExtensionArtifactsAreUpToDate(const InstructionSet isa,
+                                                           bool on_system) const;
+
+  // Verify whether boot extension artifacts for `isa` are valid on system partition or in
+  // apexdata. This method has the side-effect of removing boot classpath extension artifacts on
+  // /data, if there are valid artifacts on /system, or if the artifacts on /data are not valid.
+  // Returns true if valid boot externsion artifacts are valid.
+  WARN_UNUSED bool VerifyBootExtensionArtifactsAreUpToDate(InstructionSet isa) const;
+
+  WARN_UNUSED bool VerifySystemServerArtifactsAreUpToDate(bool on_system) const;
+
+  // Verify the validity of system server artifacts on both /system and /data.
+  // This method has the side-effect of removing system server artifacts on /data, if there are
+  // valid artifacts on /system, or if the artifacts on /data are not valid.
+  // Returns true if valid artifacts are found.
+  WARN_UNUSED bool VerifySystemServerArtifactsAreUpToDate() const;
+
+  WARN_UNUSED bool CompileBootExtensionArtifacts(const InstructionSet isa,
+                                                 const std::string& staging_dir,
+                                                 OdrMetrics& metrics,
+                                                 uint32_t* dex2oat_invocation_count,
+                                                 std::string* error_msg) const;
+
+  WARN_UNUSED bool CompileSystemServerArtifacts(const std::string& staging_dir,
+                                                OdrMetrics& metrics,
+                                                uint32_t* dex2oat_invocation_count,
+                                                std::string* error_msg) const;
+
+  // Configuration to use.
+  const OdrConfig& config_;
+
+  // Path to cache information file that is used to speed up artifact checking.
+  const std::string cache_info_filename_;
+
+  // List of boot extension components that should be compiled.
+  std::vector<std::string> boot_extension_compilable_jars_;
+
+  // List of system_server components that should be compiled.
+  std::vector<std::string> systemserver_compilable_jars_;
+
+  // List of all boot classpath components. Used as the dependencies for compiling the
+  // system_server.
+  std::vector<std::string> boot_classpath_jars_;
+
+  const time_t start_time_;
+
+  DISALLOW_COPY_AND_ASSIGN(OnDeviceRefresh);
+};
 
 }  // namespace odrefresh
 }  // namespace art
