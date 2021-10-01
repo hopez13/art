985dd296ee823256ae72089a7fa4b9e293ecf029
Victor Hsieh <victorhsieh@google.com>
2021-10-01 12:06:42 -0700

    Provide API for CompOS to reconstruct dexopt context
    The public API exposed by the ART APEX is meant to be used by CompOS
    APEX as a client. In order to let ART maintain the flexibility to evolve
    the compiler arguments (e.g. more/less flags/files), the API argument is
    an opaque object that ART itself marshalled. On the API call, ART can
    unmarshal the request since they are built in the same package, even
    though the marshalled request may be routed to outside (in this case, to
    run in the CompOS VM).
    
    The API constructs the dex2oat command line arguments instead of
    executing the command directly. This makes it the easiest to the client
    to coordinate the fork(2). For example, when the client is a
    multithreaded program, it is dangerous to fork(2) to execute the program
    without involving the client. The alternative to make the API exec is
    still possible though, if we expect the clients to get itself ready for
    the exec.
    
    Bug: 193668901
    Test: atest ComposHostTestCases
    
    Change-Id: I2199b4dcbd1d7409673d77660fc5bff5906abe5b

---
+++ artd/external/include/libdexopt_context.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ART_ARTD_EXTERNAL_INCLUDE_LIBDEXOPT_CONTEXT_H_
+#define ART_ARTD_EXTERNAL_INCLUDE_LIBDEXOPT_CONTEXT_H_
+
+#include <stdint.h>
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+/**
+ * Opaque type that contains the dexopt execution context.
+ *
+ * Introduced in API 33.
+ */
+typedef struct ADexoptContext ADexoptContext;
+
+/**
+ * Create a dexopt execution context given a marshaled byte array from ART. Return null if a valid
+ * context cannot be created.
+ *
+ * Available since API level 33.
+ */
+const ADexoptContext* ADexopt_CreateAndValidateDexoptContext(const uint8_t* marshaled, size_t size)
+        __INTRODUCED_IN(33);
+
+/**
+ * Delete the dexopt execution context.
+ *
+ * Available since API level 33.
+ */
+void ADexopt_DeleteDexoptContext(const ADexoptContext* context) __INTRODUCED_IN(33);
+
+/**
+ * Returns the command line arguments (excluding the executable path) in the execution context.
+ *
+ * Available since API level 33.
+ */
+const char* const* ADexopt_GetCmdlineArguments(const ADexoptContext* context) __INTRODUCED_IN(33);
+
+__END_DECLS
+
+#endif  // ART_ARTD_EXTERNAL_INCLUDE_LIBDEXOPT_CONTEXT_H_
