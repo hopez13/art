2e17c032e04f3e62ed55e469602c0438aa372cbc
Kevin Jeon <kevinjeon@google.com>
2021-11-19 20:39:30 +0000

    Set @NeverCompile methods as not compilable
    This change sets methods as not compilable in the class verifier if they
    are annotated with @NeverCompile. This is done by adding a new method in
    dex_file_annotations.h that checks if a method is annotated (identical
    to how @FastNative/@CriticalNative are checked in
    GetNativeMethodAnnotationAccessFlags).
    
    Bug: 197753440
    Test: WIP
    Change-Id: I22a09054dc9c2d20f5bb7aaf7bb4b504d8b59b00

--- runtime/dex/dex_file_annotations.cc
+++ runtime/dex/dex_file_annotations.cc
@@ -1291,9 +1291,32 @@ uint32_t GetNativeMethodAnnotationAccessFlags(const DexFile& dex_file,
           WellKnownClasses::dalvik_annotation_optimization_CriticalNative)) {
     access_flags |= kAccCriticalNative;
   }
-  CHECK_NE(access_flags, kAccFastNative | kAccCriticalNative);
+  if (IsMethodBuildAnnotationPresent(
+      dex_file,
+      *annotation_set,
+      "Ldalvik/annotation/optimization/NeverCompile;",
+      WellKnownClasses::dalvik_annotation_optimization_NeverCompile)) {
+    access_flags |= kAccCompileDontBother;
+  }
+  CHECK_NE(access_flags, kAccFastNative | kAccCriticalNative | kAccCompileDontBother);
   return access_flags;
 }
+
+bool MethodIsNeverCompile(const DexFile& dex_file,
+                          const dex::ClassDef& class_def,
+                          uint32_t method_index) {
+  const dex::AnnotationSetItem* annotation_set =
+      FindAnnotationSetForMethod(dex_file, class_def, method_index);
+  if (annotation_set == nullptr) {
+    return false;
+  }
+  return (IsMethodBuildAnnotationPresent(
+      dex_file,
+      *annotation_set,
+      "Ldalvik/annotation/optimization/NeverCompile;",
+      WellKnownClasses::dalvik_annotation_optimization_NeverCompile));
+}
+
 
 bool FieldIsReachabilitySensitive(const DexFile& dex_file,
                                   const dex::ClassDef& class_def,
