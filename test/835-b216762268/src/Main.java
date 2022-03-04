85abb90351e129ec00c51914e9ef617c811534cc
Vladimir Marko <vmarko@google.com>
2022-03-04 09:36:14 +0000

    Add simlified test cases to run-test 835-b216762268.
    Test: run-test --host --interpreter 835-b216762268
    Bug: 216762268
    Change-Id: I5e8f0258eda2a6318eb84426beb35815db30fa9a

--- test/835-b216762268/src/Main.java
+++ test/835-b216762268/src/Main.java
@@ -17,6 +17,34 @@
 public class Main {
     static long count;
 
+    // Simplified version: Removed both outer loops from `$noinline$mainTest()`.
+    static int $noinline$verySimpleTest() {
+        int i1 = 10, i17, i20, i21 = -2;
+        for (i17 = 2; i17 > 1; i17--) {
+            i1 += i17;
+            count = i1;
+        }
+        for (i20 = 1; i20 < 2; ++i20) {
+            i21 += i17;
+        }
+        return i21;
+    }
+
+    // Simplified version: Removed outer loop from `$noinline$mainTest()`.
+    static int $noinline$simpleTest() {
+        int i1 = 10, i15, i17, i20, i21 = -2;
+        for (i15 = 11; i15 < 188; ++i15) {
+            for (i17 = 2; i17 > 1; i17--) {
+                i1 += i17;
+                count = i1;
+            }
+            for (i20 = 1; i20 < 2; ++i20) {
+                i21 += i17;
+            }
+        }
+        return i21;
+    }
+
     static int $noinline$mainTest() {
         int i, i1 = 10, i15 = 49099, i17, i20, i21 = -2;
         for (i = 5; i < 138; i++)
@@ -32,13 +60,9 @@ public class Main {
         return i21;
     }
 
-    public static void assertEquals(int expected, int actual) {
-      if (expected != actual) {
-        throw new Error("Expected " + expected + ", got " +  actual);
-      }
-    }
-
     public static void main(String[] strArr) {
-      assertEquals(23539, $noinline$mainTest());
+      System.out.println("verySimpleTest: " + $noinline$verySimpleTest());
+      System.out.println("simpleTest: " + $noinline$simpleTest());
+      System.out.println("mainTest: " + $noinline$mainTest());
     }
 }
