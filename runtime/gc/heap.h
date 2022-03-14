3efbcffa7ede9408aaa60c3792d45d649eab9773
Lokesh Gidra <lokeshgidra@google.com>
2022-03-14 14:20:27 -0700

    Use userfaultfd syscall and related ioctls
    Bug: 160737021
    Test: Install ART module update and reboot
    Change-Id: I8d23d3d1ec4017cc000e4b097f9f905449418c01

--- runtime/gc/heap.h
+++ runtime/gc/heap.h
@@ -1677,6 +1677,9 @@ class Heap {
   // Stack trace hashes that we already saw,
   std::unordered_set<uint64_t> seen_backtraces_ GUARDED_BY(backtrace_lock_);
 
+  // Userfaultfd file descriptor.
+  // TODO (lokeshgidra): remove this when the userfaultfd-based GC is in use.
+  int uffd_;
   // We disable GC when we are shutting down the runtime in case there are daemon threads still
   // allocating.
   bool gc_disabled_for_shutdown_ GUARDED_BY(gc_complete_lock_);
