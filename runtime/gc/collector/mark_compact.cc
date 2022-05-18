42188950979af397de9a2c08bc7cbce5a44003e7
Lokesh Gidra <lokeshgidra@google.com>
2022-05-18 04:52:24 +0000

    Enable using nterp interpreter with userfaultfd GC
    Test: build module and install on device
    Bug: 160737021
    Change-Id: Iee69e8d5b486a0b6d1ce49b8c5e09ab1727f8a6f

--- runtime/gc/collector/mark_compact.cc
+++ runtime/gc/collector/mark_compact.cc
@@ -102,11 +102,11 @@ bool ShouldUseUserfaultfd() {
 #if defined(ART_FORCE_USE_READ_BARRIER)
 const bool gUseReadBarrier = kUseBakerReadBarrier || kUseTableLookupReadBarrier;
 #elif defined(ART_TARGET)
-const bool gUseReadBarrier = (kUseBakerReadBarrier || kUseTableLookupReadBarrier)
-                             && !(android::base::GetBoolProperty(
-                                    "persist.device_config.runtime_native_boot.enable_uffd_gc",
-                                    false)
-                                  && ShouldUseUserfaultfd());
+const bool gUseReadBarrier =
+    (kUseBakerReadBarrier || kUseTableLookupReadBarrier)
+    && !(android::base::GetBoolProperty("persist.device_config.runtime_native_boot.enable_uffd_gc",
+                                        false)
+         && ShouldUseUserfaultfd());
 #else
 const bool gUseReadBarrier = (kUseBakerReadBarrier || kUseTableLookupReadBarrier)
                              && !ShouldUseUserfaultfd();
