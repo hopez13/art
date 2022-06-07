b7a7816130e376f8bb8b52ae6f8562707ebe4063
Sorin Basca <sorinbasca@google.com>
2022-06-07 18:17:57 +0100

    Add access control for nest group
    Add the nest group access control checks as specified in JVMS 5.4.4
    (https://docs.oracle.com/javase/specs/jvms/se17/html/jvms-5.html#jvms-5.4.4).
    
    For a reference to the NestHost and NestMembers attributes, see JVMS 4.7.28 and 4.7.29
    (https://docs.oracle.com/javase/specs/jvms/se17/html/jvms-4.html#jvms-4.7.28).
    
    Bug: 210843415
    Test: testrunner.py --host -t 185-nest-group-access
    Change-Id: I453dfb4a2c628c1fec2083ffd692b31ad9cc0231

--- runtime/verifier/method_verifier.cc
+++ runtime/verifier/method_verifier.cc
@@ -3846,8 +3846,11 @@ ArtMethod* MethodVerifier<kVerifierDebug>::ResolveMethodAndCheckAccess(
                                      << " from " << referrer << ")";
     return res_method;
   }
-  // Check that invoke-virtual and invoke-super are not used on private methods of the same class.
-  if (res_method->IsPrivate() && (method_type == METHOD_VIRTUAL || method_type == METHOD_SUPER)) {
+  // Check that invoke-virtual is not used on private methods of the same class, nor is invoke-super
+  // used on a private method of a class outside the nest group.
+  if (res_method->IsPrivate() &&
+      (method_type == METHOD_VIRTUAL ||
+       (method_type == METHOD_SUPER && !referrer.HasSameNestHost(klass_type)) )) {
     Fail(VERIFY_ERROR_BAD_CLASS_HARD) << "invoke-super/virtual can't be used on private method "
                                       << res_method->PrettyMethod();
     return nullptr;
