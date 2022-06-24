3113e91a4172cf5b4f95ade27f05af106c2ec5d4
Stefano Cianciulli <scianciulli@google.com>
2022-06-24 17:03:52 +0000

    Include dlmalloc the right way
    Test: m com.android.art (with no errors)
    Bug: 236243696
    Change-Id: I23d44261e8eebc16a6774dca3e99561286bee204

--- runtime/gc/allocator/art-dlmalloc.cc
+++ runtime/gc/allocator/art-dlmalloc.cc
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#include "dlmalloc.h"
+#include "art-dlmalloc.h"
 
 #include <android-base/logging.h>
 
@@ -39,7 +39,7 @@ static void art_heap_usage_error(const char* function, void* p);
 #pragma GCC diagnostic ignored "-Wstrict-aliasing"
 #pragma GCC diagnostic ignored "-Wnull-pointer-arithmetic"
 #pragma GCC diagnostic ignored "-Wexpansion-to-defined"
-#include "../../../external/dlmalloc/malloc.c"
+#include "dlmalloc.c"  // from external/dlmalloc
 // Note: malloc.c uses a DEBUG define to drive debug code. This interferes with the DEBUG severity
 //       of libbase, so undefine it now.
 #undef DEBUG
