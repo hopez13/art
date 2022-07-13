15293361fbdd5f45e806a0cd7b8bd1a221b744b2
David Srbecky <dsrbecky@google.com>
2022-07-13 15:58:56 +0100

    Use hermetic python to run tests on build bots.
    Ensure that we use the python from the Android tree,
    to get consistent python version and feature set.
    
    Test: ./art/test/testrunner/run_build_test_target.py -j80 art-test
    Change-Id: Ic1926a767ff438877645a79d0e6064baa9e2983b

--- test/testrunner/run_build_test_target.py
+++ test/testrunner/run_build_test_target.py
@@ -35,6 +35,13 @@ import sys
 from target_config import target_config
 import env
 
+# Ensure the script is run using the python from Android tree
+print("Using", sys.executable, sys.version)
+PYTHON3 = "prebuilts/build-tools/path/linux-x86/python3"
+if not os.path.samefile(sys.executable, PYTHON3):
+  p = subprocess.run([PYTHON3] + sys.argv)
+  sys.exit(p.returncode)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-j', default='1', dest='n_threads')
 # either -l/--list OR build-target is required (but not both).
