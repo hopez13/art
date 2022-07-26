cc64208cee9ff1f31aa41034f08df64c53317414
Martin Stjernholm <mast@google.com>
2022-07-26 16:39:25 +0100

    Plumb through SetTaskProfiles in libprocessgroup.
    To avoid adding that library into the ART module. The function is
    required by artd to set up dex2oat execution environments.
    
    Test: adb root
          atest -a art_standalone_libartpalette_tests
    Test: adb unroot
          atest -a art_standalone_libartpalette_tests
    Test: atest ArtGtestsTargetChroot
    Test: art/tools/run-gtests.sh \
            /apex/com.android.art/bin/art/arm64/art_libartpalette_tests
      in ART test chroot
    Test: m test-art-host-gtest-art_libartpalette_tests
    Bug: 238182454
    Change-Id: I5dde4ea38140c742ebdad4a683a5ee9aec745b47

--- libartpalette/apex/palette_test.cc
+++ libartpalette/apex/palette_test.cc
@@ -21,6 +21,8 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
+#include <filesystem>
+
 #include "gtest/gtest.h"
 
 namespace {
@@ -96,5 +98,47 @@ TEST_F(PaletteClientTest, JniInvocation) {
   PaletteNotifyEndJniInvocation(env);
 
   EXPECT_EQ(JNI_OK, jvm->DestroyJavaVM());
+#endif
+}
+
+TEST_F(PaletteClientTest, SetTaskProfiles) {
+#ifndef ART_TARGET_ANDROID
+  GTEST_SKIP() << "SetTaskProfiles is only supported on Android";
+#else
+  if (!std::filesystem::exists("/sys/fs/cgroup/cgroup.controllers")) {
+    // This is intended to detect ART chroot setups, where SetTaskProfiles won't work.
+    GTEST_SKIP() << "Kernel cgroup support missing";
+  }
+
+  const char* profiles[] = {"NormalIoPriority", "TimerSlackNormal"};
+  palette_status_t res = PaletteSetTaskProfiles(GetTid(), &profiles[0], 2, false);
+  // SetTaskProfiles will only work fully if we run as root. Otherwise it'll
+  // return false which is mapped to PALETTE_STATUS_FAILED_CHECK_LOG.
+  if (getuid() == 0) {
+    EXPECT_EQ(PALETTE_STATUS_OK, res);
+  } else {
+    EXPECT_EQ(PALETTE_STATUS_FAILED_CHECK_LOG, res);
+  }
+#endif
+}
+
+TEST_F(PaletteClientTest, SetTaskProfilesCpp) {
+#ifndef ART_TARGET_ANDROID
+  GTEST_SKIP() << "SetTaskProfiles is only supported on Android";
+#else
+  if (!std::filesystem::exists("/sys/fs/cgroup/cgroup.controllers")) {
+    // This is intended to detect ART chroot setups, where SetTaskProfiles won't work.
+    GTEST_SKIP() << "Kernel cgroup support missing";
+  }
+
+  std::vector<std::string> profiles = {"NormalIoPriority", "TimerSlackNormal"};
+  palette_status_t res = PaletteSetTaskProfiles(GetTid(), profiles);
+  // SetTaskProfiles will only work fully if we run as root. Otherwise it'll
+  // return false which is mapped to PALETTE_STATUS_FAILED_CHECK_LOG.
+  if (getuid() == 0) {
+    EXPECT_EQ(PALETTE_STATUS_OK, res);
+  } else {
+    EXPECT_EQ(PALETTE_STATUS_FAILED_CHECK_LOG, res);
+  }
 #endif
 }
