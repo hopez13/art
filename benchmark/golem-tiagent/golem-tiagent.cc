d4df6a6c857cc546de1a654304bc9af2525fb943
Mythri Alle <mythria@google.com>
2022-09-26 15:31:34 +0000

    Add code and rules to build golemtiagent.so to use in golem benchmarks
    We are updating debuggable-cc configuration in golem benchmarks to
    measure the performance of debugger. As a first step, we are adding
    a breakpoint to measure the overhead of instrumentation related code.
    To achieve this we need a tiagent to be able to set a breakpoint.
    This CL adds a minimal agent that adds OnLoad callback and necessary
    methods to set a breakpoint.
    
    Bug: 232212577
    Test: m build-art-host-golem
    Change-Id: I2da3770ff85edb46da64d09ec0047ec6e7564fd0

---
+++ benchmark/golem-tiagent/golem-tiagent.cc
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "android-base/macros.h"
+#include "android-base/logging.h"
+
+#include "jni.h"
+#include "jvmti.h"
+
+namespace art {
+
+static const jvmtiCapabilities standard_caps = {
+  .can_tag_objects                                 = 1,
+  .can_generate_field_modification_events          = 1,
+  .can_generate_field_access_events                = 1,
+  .can_get_bytecodes                               = 1,
+  .can_get_synthetic_attribute                     = 1,
+  .can_get_owned_monitor_info                      = 0,
+  .can_get_current_contended_monitor               = 1,
+  .can_get_monitor_info                            = 1,
+  .can_pop_frame                                   = 0,
+  .can_redefine_classes                            = 1,
+  .can_signal_thread                               = 1,
+  .can_get_source_file_name                        = 1,
+  .can_get_line_numbers                            = 1,
+  .can_get_source_debug_extension                  = 1,
+  .can_access_local_variables                      = 0,
+  .can_maintain_original_method_order              = 1,
+  .can_generate_single_step_events                 = 1,
+  .can_generate_exception_events                   = 0,
+  .can_generate_frame_pop_events                   = 0,
+  .can_generate_breakpoint_events                  = 1,
+  .can_suspend                                     = 1,
+  .can_redefine_any_class                          = 0,
+  .can_get_current_thread_cpu_time                 = 0,
+  .can_get_thread_cpu_time                         = 0,
+  .can_generate_method_entry_events                = 1,
+  .can_generate_method_exit_events                 = 1,
+  .can_generate_all_class_hook_events              = 0,
+  .can_generate_compiled_method_load_events        = 0,
+  .can_generate_monitor_events                     = 0,
+  .can_generate_vm_object_alloc_events             = 1,
+  .can_generate_native_method_bind_events          = 1,
+  .can_generate_garbage_collection_events          = 1,
+  .can_generate_object_free_events                 = 1,
+  .can_force_early_return                          = 0,
+  .can_get_owned_monitor_stack_depth_info          = 0,
+  .can_get_constant_pool                           = 0,
+  .can_set_native_method_prefix                    = 0,
+  .can_retransform_classes                         = 1,
+  .can_retransform_any_class                       = 0,
+  .can_generate_resource_exhaustion_heap_events    = 0,
+  .can_generate_resource_exhaustion_threads_events = 0,
+};
+
+jvmtiEnv* jvmti_env = nullptr;
+
+void CheckJvmtiError(jvmtiEnv* env, jvmtiError error) {
+  if (error != JVMTI_ERROR_NONE) {
+    char* error_name;
+    jvmtiError name_error = env->GetErrorName(error, &error_name);
+    if (name_error != JVMTI_ERROR_NONE) {
+      LOG(FATAL) << "Unable to get error name for " << error;
+    }
+    LOG(FATAL) << "Unexpected error: " << error_name;
+  }
+}
+
+static void JNICALL VMInitCallback(jvmtiEnv *jenv ATTRIBUTE_UNUSED, JNIEnv* jni_env, jthread thread ATTRIBUTE_UNUSED) {
+  // Set a breakpoint on a rare method that we won't expect to be hit.
+  // java.lang.Thread.stop is deprecated and not expected to be used.
+  jclass cl = jni_env->FindClass("java/lang/Thread");
+  if (cl == nullptr) {
+    LOG(FATAL) << "Cannot find class java/lang/Thread to set a breakpoint";
+  }
+
+  jmethodID method = jni_env->GetMethodID(cl, "stop", "()V");
+  if (method == nullptr) {
+    LOG(FATAL) << "Cannot find method to set a breapoint";
+  }
+
+  jlong start = 0;
+  jlong end;
+  CheckJvmtiError(jvmti_env, jvmti_env->GetMethodLocation(method, &start, &end));
+  CheckJvmtiError(jvmti_env, jvmti_env->SetBreakpoint(method, start));
+}
+
+extern "C" JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM* vm,
+                                               char* options ATTRIBUTE_UNUSED,
+                                               void* reserved ATTRIBUTE_UNUSED) {
+  // Setup jvmti_env and set the capabilities
+  if (vm->GetEnv(reinterpret_cast<void**>(&jvmti_env), JVMTI_VERSION_1_0) != 0) {
+    LOG(ERROR) << "Unable to get jvmti env!";
+    return 1;
+  }
+  CheckJvmtiError(jvmti_env, jvmti_env->AddCapabilities(&standard_caps));
+
+  // Set a callback for VM_INIT phase so we can set a breakpoint. We cannot just
+  // set a breakpoint here since vm isn't fully initialized here.
+  jvmtiEventCallbacks callbacks;
+  memset(&callbacks, 0, sizeof(jvmtiEventCallbacks));
+  callbacks.VMInit = VMInitCallback;
+  CheckJvmtiError(jvmti_env, jvmti_env->SetEventCallbacks(&callbacks, sizeof(callbacks)));
+  CheckJvmtiError(jvmti_env,
+                  jvmti_env->SetEventNotificationMode(JVMTI_ENABLE, JVMTI_EVENT_VM_INIT, nullptr));
+
+  return 0;
+}
+
+}  // namespace art
