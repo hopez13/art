44cb564c76ce7c854899207c0555fed71be67e1e
David Srbecky <dsrbecky@google.com>
2022-10-06 11:26:27 +0100

    Convert per-test run scripts to python.
    Test: test.py -r --all-target
    Change-Id: I05003b5cde0e39ffc7d037ef875a45f13dd83755

--- test/run-test-build.py
+++ test/run-test-build.py
@@ -43,7 +43,7 @@ def copy_sources(args, tmp, mode, srcdir):
   shutil.copytree(srcdir, dstdir)
 
   # Copy the default scripts if the test does not have a custom ones.
-  for name in ["build.py", "run", "check"]:
+  for name in ["build.py", "check"]:
     src, dst = f"art/test/etc/default-{name}", join(dstdir, name)
     if os.path.exists(dst):
       shutil.copy2(src, dstdir)  # Copy default script next to the custom script.
