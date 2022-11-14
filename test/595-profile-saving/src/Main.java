c449dd37fc2aa7286a18f5b8a54ae830d632a220
Vladimir Marko <vmarko@google.com>
2022-11-14 14:03:10 +0000

    ART: Change indentation to 4 spaces in run-test shards 95-99.
    Created with
        for testName in \
            `git grep -E '^  public static void main\(String\[\]' \
                -- test/*<i>-*/src/Main.java | \
            sed '-es/\/src\/Main.java:.*//'`; \
        do \
            find $testName/ -type f -name *.java | \
            xargs sed -E '-es/^((  )+)/\1\1/' --in-place ; \
        done
    with <i> manually set to 99 down to 95 (stopping when the
    change is getting large).
    
    Manually address many long lines.
    
    Also fix indentation in two files where
        https://android-review.googlesource.com/2300941
    just updated previously wrong indentation.
    
    Test: buildbot-build.sh --target
    Change-Id: I02fe676f3983610c0cdda1134aee2476f71bc7ce

--- test/595-profile-saving/src/Main.java
+++ test/595-profile-saving/src/Main.java
@@ -20,121 +20,120 @@ import java.lang.reflect.Method;
 
 public class Main {
 
-  public static void main(String[] args) throws Exception {
-    System.loadLibrary(args[0]);
+    public static void main(String[] args) throws Exception {
+        System.loadLibrary(args[0]);
 
-    File file = null;
-    try {
-      file = createTempFile();
-      // String codePath = getDexBaseLocation();
-      String codePath = System.getenv("DEX_LOCATION") + "/595-profile-saving.jar";
-      VMRuntime.registerAppInfo("test.app",
-                                file.getPath(),
-                                file.getPath(),
-                                new String[] {codePath},
-                                VMRuntime.CODE_PATH_TYPE_PRIMARY_APK);
+        File file = null;
+        try {
+            file = createTempFile();
+            // String codePath = getDexBaseLocation();
+            String codePath = System.getenv("DEX_LOCATION") + "/595-profile-saving.jar";
+            VMRuntime.registerAppInfo("test.app",
+                                      file.getPath(),
+                                      file.getPath(),
+                                      new String[] {codePath},
+                                      VMRuntime.CODE_PATH_TYPE_PRIMARY_APK);
 
-      // Test that the profile saves an app method with a profiling info.
-      Method appMethod = Main.class.getDeclaredMethod("testAddMethodToProfile",
-          File.class, Method.class);
-      testAddMethodToProfile(file, appMethod);
+            // Test that the profile saves an app method with a profiling info.
+            Method appMethod = Main.class.getDeclaredMethod("testAddMethodToProfile",
+                    File.class, Method.class);
+            testAddMethodToProfile(file, appMethod);
 
-      // Test that the profile saves a boot class path method with a profiling info.
-      Method bootMethod = File.class.getDeclaredMethod("delete");
-      if (bootMethod.getDeclaringClass().getClassLoader() != Object.class.getClassLoader()) {
-        System.out.println("Class loader does not match boot class");
-      }
-      testAddMethodToProfile(file, bootMethod);
+            // Test that the profile saves a boot class path method with a profiling info.
+            Method bootMethod = File.class.getDeclaredMethod("delete");
+            if (bootMethod.getDeclaringClass().getClassLoader() != Object.class.getClassLoader()) {
+                System.out.println("Class loader does not match boot class");
+            }
+            testAddMethodToProfile(file, bootMethod);
 
-      // We never expect System.console to be executed before Main.main gets invoked, and therefore
-      // it should never be in a profile.
-      Method bootNotInProfileMethod = System.class.getDeclaredMethod("console");
-      testMethodNotInProfile(file, bootNotInProfileMethod);
+            // We never expect System.console to be executed before Main.main gets invoked,
+            // and therefore it should never be in a profile.
+            Method bootNotInProfileMethod = System.class.getDeclaredMethod("console");
+            testMethodNotInProfile(file, bootNotInProfileMethod);
 
-      System.out.println("IsForBootImage: " + isForBootImage(file.getPath()));
-    } finally {
-      if (file != null) {
-        file.delete();
-      }
-    }
-  }
-
-  static void testAddMethodToProfile(File file, Method m) {
-    // Make sure we have a profile info for this method without the need to loop.
-    ensureProfilingInfo(m);
-    // Make sure the profile gets saved.
-    ensureProfileProcessing();
-    // Verify that the profile was saved and contains the method.
-    if (!presentInProfile(file.getPath(), m)) {
-      throw new RuntimeException("Expected method " + m + " to be in the profile");
-    }
-  }
-
-  static void testMethodNotInProfile(File file, Method m) {
-    // Make sure the profile gets saved.
-    ensureProfileProcessing();
-    // Verify that the profile was saved and contains the method.
-    if (presentInProfile(file.getPath(), m)) {
-      throw new RuntimeException("Did not expect method " + m + " to be in the profile");
-    }
-  }
-
-  // Ensure a method has a profiling info.
-  public static native void ensureProfilingInfo(Method method);
-  // Ensures the profile saver does its usual processing.
-  public static native void ensureProfileProcessing();
-  // Checks if the profiles saver knows about the method.
-  public static native boolean presentInProfile(String profile, Method method);
-  // Returns true if the profile is for the boot image.
-  public static native boolean isForBootImage(String profile);
-
-  private static final String TEMP_FILE_NAME_PREFIX = "temp";
-  private static final String TEMP_FILE_NAME_SUFFIX = "-file";
-
-  static native String getProfileInfoDump(
-      String filename);
-
-  private static File createTempFile() throws Exception {
-    try {
-      return File.createTempFile(TEMP_FILE_NAME_PREFIX, TEMP_FILE_NAME_SUFFIX);
-    } catch (IOException e) {
-      System.setProperty("java.io.tmpdir", "/data/local/tmp");
-      try {
-        return File.createTempFile(TEMP_FILE_NAME_PREFIX, TEMP_FILE_NAME_SUFFIX);
-      } catch (IOException e2) {
-        System.setProperty("java.io.tmpdir", "/sdcard");
-        return File.createTempFile(TEMP_FILE_NAME_PREFIX, TEMP_FILE_NAME_SUFFIX);
-      }
-    }
-  }
-
-  private static class VMRuntime {
-    public static final int CODE_PATH_TYPE_PRIMARY_APK = 1;
-    private static final Method registerAppInfoMethod;
-
-    static {
-      try {
-        Class<? extends Object> c = Class.forName("dalvik.system.VMRuntime");
-        registerAppInfoMethod = c.getDeclaredMethod("registerAppInfo",
-            String.class, String.class, String.class, String[].class, int.class);
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
+            System.out.println("IsForBootImage: " + isForBootImage(file.getPath()));
+        } finally {
+            if (file != null) {
+                file.delete();
+            }
+        }
     }
 
-    public static void registerAppInfo(
-        String packageName,
-        String curProfile,
-        String refProfile,
-        String[] codePaths,
-        int codePathsType) throws Exception {
-      registerAppInfoMethod.invoke(
-          null,
-          packageName,
-          curProfile,
-          refProfile,
-          codePaths,
-          codePathsType);
+    static void testAddMethodToProfile(File file, Method m) {
+        // Make sure we have a profile info for this method without the need to loop.
+        ensureProfilingInfo(m);
+        // Make sure the profile gets saved.
+        ensureProfileProcessing();
+        // Verify that the profile was saved and contains the method.
+        if (!presentInProfile(file.getPath(), m)) {
+            throw new RuntimeException("Expected method " + m + " to be in the profile");
+        }
     }
-  }
+
+    static void testMethodNotInProfile(File file, Method m) {
+        // Make sure the profile gets saved.
+        ensureProfileProcessing();
+        // Verify that the profile was saved and contains the method.
+        if (presentInProfile(file.getPath(), m)) {
+            throw new RuntimeException("Did not expect method " + m + " to be in the profile");
+        }
+    }
+
+    // Ensure a method has a profiling info.
+    public static native void ensureProfilingInfo(Method method);
+    // Ensures the profile saver does its usual processing.
+    public static native void ensureProfileProcessing();
+    // Checks if the profiles saver knows about the method.
+    public static native boolean presentInProfile(String profile, Method method);
+    // Returns true if the profile is for the boot image.
+    public static native boolean isForBootImage(String profile);
+
+    private static final String TEMP_FILE_NAME_PREFIX = "temp";
+    private static final String TEMP_FILE_NAME_SUFFIX = "-file";
+
+    static native String getProfileInfoDump(String filename);
+
+    private static File createTempFile() throws Exception {
+        try {
+            return File.createTempFile(TEMP_FILE_NAME_PREFIX, TEMP_FILE_NAME_SUFFIX);
+        } catch (IOException e) {
+            System.setProperty("java.io.tmpdir", "/data/local/tmp");
+            try {
+                return File.createTempFile(TEMP_FILE_NAME_PREFIX, TEMP_FILE_NAME_SUFFIX);
+            } catch (IOException e2) {
+                System.setProperty("java.io.tmpdir", "/sdcard");
+                return File.createTempFile(TEMP_FILE_NAME_PREFIX, TEMP_FILE_NAME_SUFFIX);
+            }
+        }
+    }
+
+    private static class VMRuntime {
+        public static final int CODE_PATH_TYPE_PRIMARY_APK = 1;
+        private static final Method registerAppInfoMethod;
+
+        static {
+            try {
+                Class<? extends Object> c = Class.forName("dalvik.system.VMRuntime");
+                registerAppInfoMethod = c.getDeclaredMethod("registerAppInfo",
+                        String.class, String.class, String.class, String[].class, int.class);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static void registerAppInfo(
+                String packageName,
+                String curProfile,
+                String refProfile,
+                String[] codePaths,
+                int codePathsType) throws Exception {
+            registerAppInfoMethod.invoke(
+                    null,
+                    packageName,
+                    curProfile,
+                    refProfile,
+                    codePaths,
+                    codePathsType);
+        }
+    }
 }
