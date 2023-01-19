37fa46dad79521a2f428f1ce14ff4aa6c787ee98
Vladimír Marko <vmarko@google.com>
2023-01-19 13:41:28 +0000

    Reland "Do not use unnecessary `membarrier()` on x86."
    This reverts commit 6c2275d43d6e432a610a4a90deac6896c25e3c14.
    
    Reason for revert: Apply the workaround only on x86/x86-64
    and fix the code to reject kPrivateExpedited without
    kRegisterPrivateExpedited.
    
    Change-Id: If2d382002c278c1770ba6b1847adfa7b63fe8f1d
    Test: m test-art-host-gtest
    Bug: 38383823

--- libartbase/base/membarrier.cc
+++ libartbase/base/membarrier.cc
@@ -24,6 +24,7 @@
 #include <sys/utsname.h>
 #include <unistd.h>
 #endif
+#include "arch/instruction_set.h"
 #include "macros.h"
 
 #if defined(__BIONIC__)
@@ -45,9 +46,8 @@ CHECK_MEMBARRIER_CMD(art::MembarrierCommand::kPrivateExpedited, MEMBARRIER_CMD_P
 
 namespace art {
 
-#if defined(__NR_membarrier)
-
 int membarrier(MembarrierCommand command) {
+#if defined(__NR_membarrier)
   // Check kernel version supports membarrier(2).
   static constexpr int kRequiredMajor = 4;
   static constexpr int kRequiredMinor = 16;
@@ -71,17 +71,33 @@ int membarrier(MembarrierCommand command) {
   }
 #endif  // __BIONIC__
   return syscall(__NR_membarrier, static_cast<int>(command), 0);
-}
-
 #else  // __NR_membarrier
-
-int membarrier(MembarrierCommand command ATTRIBUTE_UNUSED) {
   // In principle this could be supported on linux, but Android's prebuilt glibc does not include
   // the system call number defintions (b/111199492).
+  // The following "implementation" is used on x86 to eliminate log spam for host tests.
+
+  // On x86, the `kPrivateExpedited` command is unnecessary thanks to the memory model,
+  // but we want to prevent instruction reordering in case of whole program optimization.
+  // We also want to return an error if the process did not register for the command.
+  if (kRuntimeISA == InstructionSet::kX86 || kRuntimeISA == InstructionSet::kX86_64) {
+    std::atomic<bool> registered(false);
+    if (command == MembarrierCommand::kPrivateExpedited) {
+      if (registered.load(std::memory_order_seq_cst)) {
+        return 0;
+      } else {
+        errno = EPERM;
+        return -1;
+      }
+    }
+    if (command == MembarrierCommand::kRegisterPrivateExpedited) {
+      registered.store(std::memory_order_seq_cst);
+      return 0;
+    }
+  }
+
   errno = ENOSYS;
   return -1;
-}
-
 #endif  // __NR_membarrier
+}
 
 }  // namespace art
