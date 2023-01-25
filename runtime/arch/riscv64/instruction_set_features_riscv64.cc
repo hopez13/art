1b5a805a8ed1bb0194bce10b5d0cb57fd7e5e640
Ulya Trafimovich <skvadrik@google.com>
2023-01-25 15:55:55 +0000

    riscv64: define instruction set features.
    At the moment we assume the basic extension set G (IMAFD) plus the C
    extension (compression). V extension (vector) is also defined, but not
    assumed by default.
    
    Test: lunch aosp_riscv64-userdebug && m dist
    
    Change-Id: I11f32e75f546e62d6a0f66e02d24865bff96c67c

---
+++ runtime/arch/riscv64/instruction_set_features_riscv64.cc
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "instruction_set_features_riscv64.h"
+
+#include <fstream>
+#include <sstream>
+
+#include "android-base/stringprintf.h"
+#include "android-base/strings.h"
+#include "base/logging.h"
+
+namespace art {
+
+using android::base::StringPrintf;
+
+Riscv64FeaturesUniquePtr Riscv64InstructionSetFeatures::FromVariant(
+    const std::string& variant, std::string* error_msg ATTRIBUTE_UNUSED) {
+  if (variant != "generic") {
+    LOG(WARNING) << "Unexpected CPU variant for Riscv64 using defaults: " << variant;
+  }
+  uint32_t bits = kExtGeneric | kExtCompressed;  // rv64gc, aka rv64imafdc
+  return Riscv64FeaturesUniquePtr(new Riscv64InstructionSetFeatures(bits));
+}
+
+Riscv64FeaturesUniquePtr Riscv64InstructionSetFeatures::FromBitmap(uint32_t bitmap) {
+  return Riscv64FeaturesUniquePtr(new Riscv64InstructionSetFeatures(bitmap));
+}
+
+Riscv64FeaturesUniquePtr Riscv64InstructionSetFeatures::FromCppDefines() {
+  uint32_t bits = kExtGeneric | kExtCompressed;  // rv64gc, aka rv64imafdc
+  return Riscv64FeaturesUniquePtr(new Riscv64InstructionSetFeatures(bits));
+}
+
+Riscv64FeaturesUniquePtr Riscv64InstructionSetFeatures::FromCpuInfo() {
+  UNIMPLEMENTED(WARNING);
+  return FromCppDefines();
+}
+
+Riscv64FeaturesUniquePtr Riscv64InstructionSetFeatures::FromHwcap() {
+  UNIMPLEMENTED(WARNING);
+  return FromCppDefines();
+}
+
+Riscv64FeaturesUniquePtr Riscv64InstructionSetFeatures::FromAssembly() {
+  UNIMPLEMENTED(WARNING);
+  return FromCppDefines();
+}
+
+Riscv64FeaturesUniquePtr Riscv64InstructionSetFeatures::FromCpuFeatures() {
+  UNIMPLEMENTED(WARNING);
+  return FromCppDefines();
+}
+
+bool Riscv64InstructionSetFeatures::Equals(const InstructionSetFeatures* other) const {
+  if (InstructionSet::kRiscv64 != other->GetInstructionSet()) {
+    return false;
+  }
+  return bits_ == other->AsRiscv64InstructionSetFeatures()->bits_;
+}
+
+uint32_t Riscv64InstructionSetFeatures::AsBitmap() const {
+  return kExtGeneric | kExtCompressed;  // rv64gc, aka rv64imafdc
+}
+
+std::string Riscv64InstructionSetFeatures::GetFeatureString() const {
+  std::string result = "rv64";
+  if (bits_ & kExtGeneric) {
+    result += "imafd";
+  }
+  if (bits_ & kExtCompressed) {
+    result += "c";
+  }
+  if (bits_ & kExtVector) {
+    result += "v";
+  }
+  return result;
+}
+
+std::unique_ptr<const InstructionSetFeatures>
+Riscv64InstructionSetFeatures::AddFeaturesFromSplitString(
+    const std::vector<std::string>& features ATTRIBUTE_UNUSED,
+    std::string* error_msg ATTRIBUTE_UNUSED) const {
+  uint32_t bits = kExtGeneric | kExtCompressed;  // rv64gc, aka rv64imafdc
+  return std::unique_ptr<const InstructionSetFeatures>(new Riscv64InstructionSetFeatures(bits));
+}
+
+}  // namespace art
