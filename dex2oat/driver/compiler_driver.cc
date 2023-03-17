88e03089a27156a2581e24d90220b41371161d95
Nicolas Geoffray <ngeoffray@google.com>
2023-03-17 08:20:33 +0000

    Only special case boot image extension for class initialization.
    Special case for the mainline extension, which didn't use to get its
    classes initialized.
    
    Test: test.py
    Change-Id: I1bb92d262c8bedb0ffefd5fa322fa8155d10ceb7

--- dex2oat/driver/compiler_driver.cc
+++ dex2oat/driver/compiler_driver.cc
@@ -2179,13 +2179,14 @@ class InitializeClassVisitor : public CompilationVisitor {
         bool too_many_encoded_fields = (!is_boot_image && !is_boot_image_extension) &&
             klass->NumStaticFields() > kMaxEncodedFields;
 
+        bool have_profile = (compiler_options.GetProfileCompilationInfo() != nullptr);
         // If the class was not initialized, we can proceed to see if we can initialize static
         // fields. Limit the max number of encoded fields.
         if (!klass->IsInitialized() &&
             (is_app_image || is_boot_image || is_boot_image_extension) &&
             try_initialize_with_superclasses && !too_many_encoded_fields &&
             compiler_options.IsImageClass(descriptor) &&
-            compiler_options.IsAotCompilationEnabled()) {
+            (have_profile || !is_boot_image_extension)) {
           bool can_init_static_fields = false;
           if (is_boot_image || is_boot_image_extension) {
             // We need to initialize static fields, we only do this for image classes that aren't
