0f67073b31826ce7d13574e54af74935bc2dbf4a
Victor Chang <vichang@google.com>
2023-03-21 17:00:46 +0000

    Set Record flag during class linking
    It's needed for adding new record-specific behavior in ART.
    For example, fields are still immutable and un-modifiable
    via java.lang.reflect.Field#set*() API. See http://b/272698028
    
    Bug: 272698028
    Test: atest CtsLibcoreTestCases:RecordTest
    Change-Id: I6805fc4a8b8966b913eaba9f7c4dfdd1ab5886c4

--- runtime/class_linker.h
+++ runtime/class_linker.h
@@ -1186,6 +1186,8 @@ class ClassLinker {
       REQUIRES_SHARED(Locks::mutator_lock_);
   bool LinkInstanceFields(Thread* self, Handle<mirror::Class> klass)
       REQUIRES_SHARED(Locks::mutator_lock_);
+  void SetRecordClassFlagIfNeeded(Handle<mirror::Class> klass)
+      REQUIRES_SHARED(Locks::mutator_lock_);
   void CreateReferenceInstanceOffsets(Handle<mirror::Class> klass)
       REQUIRES_SHARED(Locks::mutator_lock_);
 
