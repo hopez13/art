01231d6acd899c6fc8058e393ce8befdd1a2e02c
Ulya Trafimovich <skvadrik@google.com>
2023-04-13 13:13:52 +0100

    riscv64: Move shadow stack register from X18 to X3(GP).
    This is following a corresponding change in LLVM and bionic. Note that
    we don't use X18 yet to make the migration path easier. It can be used
    in the future after all compiler prebuilts are updated.
    
    This commit also drops art thread register TR from `kSaveEverything`
    frame (which allowas us to keep frame size unchanged). Dropping TR
    should have been done in https://r.android.com/2430733.
    
    Bug: 277909695
    Bug: b/271573990
    Test: run ART run-tests on a Linux RISC-V VM, observe that they pass all
          except one expected failure 051-thread.
    
      lunch aosp_riscv64-userdebug
    
      export ART_TEST_SSH_USER=ubuntu
      export ART_TEST_SSH_HOST=localhost
      export ART_TEST_SSH_PORT=10001
      export ART_TEST_ON_VM=true
    
      . art/tools/buildbot-utils.sh
      art/tools/buildbot-build.sh --target
    
      # Create, boot and configure the VM.
      art/tools/buildbot-vm.sh create
      art/tools/buildbot-vm.sh boot
      art/tools/buildbot-vm.sh setup-ssh  # password: 'ubuntu'
    
      art/tools/buildbot-cleanup-device.sh
      art/tools/buildbot-setup-device.sh
      art/tools/buildbot-sync.sh
    
      art/test.py --target -r --no-prebuild --ndebug --no-image \
        --64 --interpreter 984-obsolete-invoke
    
    Change-Id: I52ee3c8e82ac734638ba9f1546620d26b1c20c73

--- runtime/arch/riscv64/registers_riscv64.h
+++ runtime/arch/riscv64/registers_riscv64.h
@@ -28,7 +28,7 @@ enum XRegister {
   Zero = 0,  // X0, hard-wired zero
   RA = 1,    // X1, return address
   SP = 2,    // X2, stack pointer
-  GP = 3,    // X3, global pointer
+  GP = 3,    // X3, global pointer (unavailable, used for shadow stack by the compiler / libc)
   TP = 4,    // X4, thread pointer (points to TLS area, not ART-internal thread)
 
   T0 = 5,  // X5, temporary 0
@@ -47,7 +47,7 @@ enum XRegister {
   A6 = 16,  // X16, argument 6
   A7 = 17,  // X17, argument 7
 
-  S2 = 18,   // X18, callee-saved 2 (unavailable, used for shadow stack by the compiler / libc)
+  S2 = 18,   // X18, callee-saved 2
   S3 = 19,   // X19, callee-saved 3
   S4 = 20,   // X20, callee-saved 4
   S5 = 21,   // X21, callee-saved 5
