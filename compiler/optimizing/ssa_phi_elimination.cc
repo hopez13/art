a9f65a4f656ae22bba2867d2d21d41106a01c85c
Vladimir Marko <vmarko@google.com>
2023-04-25 16:40:06 +0000

    Optimizing: Add `HInstruction::As##type()`.
    After the old implementation was renamed in
        https://android-review.googlesource.com/2526708 ,
    we introduce a new function with the old name but new
    behavior, just `DCHECK()`-ing theinstruction kind before
    casting down the pointer.
    
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing
    Test: run-gtests.sh
    Test: testrunner.py --target --optimizing
    Change-Id: I025681612a77ca2157fed4886ca47f2053975d4e

--- compiler/optimizing/ssa_phi_elimination.cc
+++ compiler/optimizing/ssa_phi_elimination.cc
@@ -45,8 +45,7 @@ void SsaDeadPhiElimination::MarkDeadPhis() {
   // Add to the worklist phis referenced by non-phi instructions.
   for (HBasicBlock* block : graph_->GetReversePostOrder()) {
     for (HInstructionIterator inst_it(block->GetPhis()); !inst_it.Done(); inst_it.Advance()) {
-      // TODO: Remove "OrNull".
-      HPhi* phi = inst_it.Current()->AsPhiOrNull();
+      HPhi* phi = inst_it.Current()->AsPhi();
       if (phi->IsDead()) {
         continue;
       }
@@ -77,8 +76,7 @@ void SsaDeadPhiElimination::MarkDeadPhis() {
     HPhi* phi = worklist.back();
     worklist.pop_back();
     for (HInstruction* raw_input : phi->GetInputs()) {
-      // TODO: Remove "OrNull".
-      HPhi* input = raw_input->AsPhiOrNull();
+      HPhi* input = raw_input->AsPhi();
       if (input != nullptr && input->IsDead()) {
         // Input is a dead phi. Revive it and add to the worklist. We make sure
         // that the phi was not dead initially (see definition of `initially_live`).
@@ -99,8 +97,7 @@ void SsaDeadPhiElimination::EliminateDeadPhis() {
     HInstruction* next = nullptr;
     HPhi* phi;
     while (current != nullptr) {
-      // TODO: Remove "OrNull".
-      phi = current->AsPhiOrNull();
+      phi = current->AsPhi();
       next = current->GetNext();
       if (phi->IsDead()) {
         // Make sure the phi is only used by other dead phis.
@@ -108,8 +105,7 @@ void SsaDeadPhiElimination::EliminateDeadPhis() {
           for (const HUseListNode<HInstruction*>& use : phi->GetUses()) {
             HInstruction* user = use.GetUser();
             DCHECK(user->IsLoopHeaderPhi());
-            // TODO: Remove "OrNull".
-            DCHECK(user->AsPhiOrNull()->IsDead());
+            DCHECK(user->AsPhi()->IsDead());
           }
         }
         // Remove the phi from use lists of its inputs.
@@ -139,8 +135,7 @@ bool SsaRedundantPhiElimination::Run() {
   // neither will necessarily converge faster.
   for (HBasicBlock* block : graph_->GetReversePostOrder()) {
     for (HInstructionIterator inst_it(block->GetPhis()); !inst_it.Done(); inst_it.Advance()) {
-      // TODO: Remove "OrNull".
-      worklist.push_back(inst_it.Current()->AsPhiOrNull());
+      worklist.push_back(inst_it.Current()->AsPhi());
     }
   }
 
@@ -202,11 +197,9 @@ bool SsaRedundantPhiElimination::Run() {
             continue;
           } else if (input->IsPhi()) {
             if (!visited_phis_in_cycle.IsBitSet(input->GetId())) {
-              // TODO: Remove "OrNull".
-              cycle_worklist.push_back(input->AsPhiOrNull());
+              cycle_worklist.push_back(input->AsPhi());
               visited_phis_in_cycle.SetBit(input->GetId());
-              // TODO: Remove "OrNull".
-              catch_phi_in_cycle |= input->AsPhiOrNull()->IsCatchPhi();
+              catch_phi_in_cycle |= input->AsPhi()->IsCatchPhi();
               irreducible_loop_phi_in_cycle |= input->IsIrreducibleLoopHeaderPhi();
             } else {
               // Already visited, nothing to do.
@@ -255,8 +248,7 @@ bool SsaRedundantPhiElimination::Run() {
       for (const HUseListNode<HInstruction*>& use : current->GetUses()) {
         HInstruction* user = use.GetUser();
         if (user->IsPhi() && !visited_phis_in_cycle.IsBitSet(user->GetId())) {
-          // TODO: Remove "OrNull".
-          worklist.push_back(user->AsPhiOrNull());
+          worklist.push_back(user->AsPhi());
         }
       }
       DCHECK(candidate->StrictlyDominates(current));
