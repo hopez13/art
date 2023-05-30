5fdb84cc9e8d9727b58847b39b7775f49982d010
Vladimir Marko <vmarko@google.com>
2023-05-30 17:19:21 +0200

    riscv64: Enable nterp with a method filter.
    Filter out methods with try-blocks or unimplemented opcodes.
    
    Test: make sure zygote does not SIGILL at boot:
          $ lunch aosp_cf_riscv64_phone-userdebug && m
          $ launch_cvd --gpu_mode=drm_virgl
          # grep logcat for SIGILL (in zygote64 process), observe none
    Bug: 283082047
    Change-Id: I8cee2b955f6f556fbf46f7add320f02d339fcdf8

--- runtime/interpreter/mterp/nterp.cc
+++ runtime/interpreter/mterp/nterp.cc
@@ -42,8 +42,7 @@ bool IsNterpSupported() {
     case InstructionSet::kArm64:
       return !kPoisonHeapReferences && kReserveMarkingRegister;
     case InstructionSet::kRiscv64:
-      // TODO(riscv64): Support nterp.
-      return false;
+      return true;
     case InstructionSet::kX86:
     case InstructionSet::kX86_64:
       return true;
