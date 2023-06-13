9c3428364b12e376eb04a90957aa17d7d9592dc9
Lifang Xia <lifang_xia@linux.alibaba.com>
2023-06-13 09:44:37 +0800

    RISCV: [Codegen] Add MoveLocation
    Test: m test-art-host-gtest
    Bug: 283082089
    Change-Id: I0fc7aba6b2dfaff4e8f5c697b3dac4ad20112033

--- compiler/optimizing/code_generator_riscv64.h
+++ compiler/optimizing/code_generator_riscv64.h
@@ -292,28 +292,32 @@ class CodeGeneratorRISCV64 : public CodeGenerator {
     return false;
   }
 
+  // Get FP register width in bytes for spilling/restoring in the slow paths.
+  //
+  // Note: In SIMD graphs this should return SIMD register width as all FP and SIMD registers
+  // alias and live SIMD registers are forced to be spilled in full size in the slow paths.
   size_t GetSlowPathFPWidth() const override {
-    LOG(FATAL) << "CodeGeneratorRISCV64::GetSlowPathFPWidth is unimplemented";
-    UNREACHABLE();
+    // Default implementation.
+    return GetCalleePreservedFPWidth();
   }
 
   size_t GetCalleePreservedFPWidth() const override {
-    LOG(FATAL) << "CodeGeneratorRISCV64::GetCalleePreservedFPWidth is unimplemented";
+    return kRiscv64FloatRegSizeInBytes;
+  };
+
+  size_t GetSIMDRegisterWidth() const override {
+    LOG(FATAL) << "Vector is not unimplemented";
     UNREACHABLE();
   };
 
-  size_t GetSIMDRegisterWidth() const override;
-
   uintptr_t GetAddressOf(HBasicBlock* block) override {
-    UNUSED(block);
-    LOG(FATAL) << "CodeGeneratorRISCV64::GetAddressOf is unimplemented";
-    UNREACHABLE();
+    return assembler_.GetLabelLocation(GetLabelOf(block));
   };
 
   void Initialize() override { block_labels_ = CommonInitializeLabels<Riscv64Label>(); }
 
   void MoveConstant(Location destination, int32_t value) override;
-  void MoveLocation(Location dst, Location src, DataType::Type dst_type) override;
+  void MoveLocation(Location destination, Location source, DataType::Type dst_type) override;
   void AddLocationAsTemp(Location location, LocationSummary* locations) override;
 
   HGraphVisitor* GetInstructionVisitor() override {
