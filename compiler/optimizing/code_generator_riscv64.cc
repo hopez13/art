60f4680b324af4f731d4743c6d390eebd48753d1
Lifang Xia <lifang_xia@linux.alibaba.com>
2023-07-10 17:37:24 +0800

    RISCV: [Codegen] Add VisitReturnVoid
    Test: m test-art-host-gtest
    Bug: 283082089
    Change-Id: I18a79cdce6844183a980dab1fde938bb29d3938b

--- compiler/optimizing/code_generator_riscv64.cc
+++ compiler/optimizing/code_generator_riscv64.cc
@@ -1484,13 +1484,11 @@ void InstructionCodeGeneratorRISCV64::VisitReturn([[maybe_unused]] HReturn* inst
 }
 
 void LocationsBuilderRISCV64::VisitReturnVoid(HReturnVoid* instruction) {
-  UNUSED(instruction);
-  LOG(FATAL) << "Unimplemented";
+  instruction->SetLocations(nullptr);
 }
 
-void InstructionCodeGeneratorRISCV64::VisitReturnVoid(HReturnVoid* instruction) {
-  UNUSED(instruction);
-  LOG(FATAL) << "Unimplemented";
+void InstructionCodeGeneratorRISCV64::VisitReturnVoid([[maybe_unused]] HReturnVoid* instruction) {
+  codegen_->GenerateFrameExit();
 }
 
 void LocationsBuilderRISCV64::VisitRor(HRor* instruction) {
