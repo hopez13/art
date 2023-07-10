addd21a7918109a66d0a8d6f3f4d5aa17caa6086
Myriam Anis <myriamanis@google.com>
2023-07-10 20:11:02 +0000

    Testing rebase.
    Change-Id: I8fc6d3cffcf80ee99853541f583e2300154adff5

--- compiler/optimizing/block_builder.cc
+++ compiler/optimizing/block_builder.cc
@@ -1,4 +1,8 @@
+<<<<<<< PATCH SET (068a1e Testing rebase.)
+/* Test.
+=======
 /* Changing file.
+>>>>>>> BASE      (226773 Create a commit.)
  * Copyright (C) 2016 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
