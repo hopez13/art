1b4ccbb8f70e332b766ede0be07221c60156f267
Jiakai Zhang <jiakaiz@google.com>
2023-07-17 15:48:11 +0000

    Use host `conscrypt` and `core-icu4j` for target Golem test.
    Change-Id: I6cc83cfd64f6a2c7d262d83a91b27c58a030b444

--- test/generate-boot-image/generate-boot-image.cc
+++ test/generate-boot-image/generate-boot-image.cc
@@ -24,11 +24,13 @@
 #include <string>
 #include <vector>
 
+#include "android-base/parsebool.h"
 #include "android-base/stringprintf.h"
 #include "android-base/strings.h"
 #include "arch/instruction_set.h"
 #include "base/file_utils.h"
 #include "base/globals.h"
+#include "base/macros.h"
 #include "base/os.h"
 #include "base/testing.h"
 
@@ -36,9 +38,31 @@ namespace art {
 
 namespace {
 
+using ::android::base::ConsumePrefix;
+using ::android::base::ConsumeSuffix;
 using ::android::base::Join;
+using ::android::base::ParseBool;
+using ::android::base::ParseBoolResult;
 using ::android::base::StringPrintf;
 using ::art::testing::GetLibCoreDexFileNames;
+using ::art::testing::GetLibCoreDexLocations;
+
+struct Options {
+  std::string output_dir = "";
+  // Set the compiler filter to `verify` by default to make test preparation
+  // faster.
+  std::string compiler_filter = "verify";
+  bool use_profile = true;
+  std::string dex2oat_bin = "";
+  std::string android_root = "";
+  std::string profile_file = "";
+  std::string instruction_set = "";
+};
+
+[[noreturn]] void Usage(const std::string& message) {
+  LOG(ERROR) << message;
+  exit(EX_USAGE);
+}
 
 std::string GetCompilerExecutable() {
   std::string compiler_executable = GetArtBinDir() + "/dex2oat";
@@ -64,23 +88,30 @@ std::string BuildCommand(const std::vector<std::string>& args) {
   return command;
 }
 
-int GenerateBootImage(const std::string& dir, const std::string& compiler_filter) {
-  std::string isa = GetInstructionSetString(kRuntimeISA);
-
+int GenerateBootImage(const Options& options) {
   std::vector<std::string> args;
-  args.push_back(GetCompilerExecutable());
+  args.push_back(options.dex2oat_bin);
 
-  std::vector<std::string> dex_files = GetLibCoreDexFileNames(/*core_only=*/true);
+  std::vector<std::string> dex_files =
+      GetLibCoreDexFileNames(options.android_root, /*core_only=*/false);
+  std::vector<std::string> dex_locations = GetLibCoreDexLocations(/*core_only=*/false);
   args.push_back("--runtime-arg");
   args.push_back("-Xbootclasspath:" + Join(dex_files, ":"));
+  args.push_back("--runtime-arg");
+  args.push_back("-Xbootclasspath-locations:" + Join(dex_locations, ":"));
   for (const std::string& file : dex_files) {
     args.push_back("--dex-file=" + file);
   }
+  for (const std::string& location : dex_locations) {
+    args.push_back("--dex-location=" + location);
+  }
 
-  args.push_back("--instruction-set=" + isa);
+  args.push_back("--instruction-set=" + options.instruction_set);
   args.push_back(StringPrintf("--base=0x%08x", ART_BASE_ADDRESS));
-  args.push_back("--compiler-filter=" + compiler_filter);
-  args.push_back(StringPrintf("--profile-file=%s/etc/boot-image.prof", GetArtRoot().c_str()));
+  args.push_back("--compiler-filter=" + options.compiler_filter);
+  if (options.use_profile) {
+    args.push_back("--profile-file=" + options.profile_file);
+  }
   args.push_back("--avoid-storing-invocation");
   args.push_back("--generate-debug-info");
   args.push_back("--generate-build-id");
@@ -88,7 +119,7 @@ int GenerateBootImage(const std::string& dir, const std::string& compiler_filter
   args.push_back("--strip");
   args.push_back("--android-root=out/empty");
 
-  std::string path = StringPrintf("%s/%s", dir.c_str(), isa.c_str());
+  std::string path = ART_FORMAT("{}/{}", options.output_dir, options.instruction_set);
   if (!OS::DirectoryExists(path.c_str())) {
     CHECK_EQ(mkdir(path.c_str(), S_IRWXU), 0);
   }
@@ -102,32 +133,74 @@ int GenerateBootImage(const std::string& dir, const std::string& compiler_filter
   return exit_code;
 }
 
-}  // namespace
-}  // namespace art
-
-int main(int argc, char** argv) {
+int Main(int argc, char** argv) {
   android::base::InitLogging(argv, android::base::LogdLogger(android::base::SYSTEM));
 
-  std::string dir = "";
-  // Set the compiler filter to `verify` by default to make test preparation
-  // faster.
-  std::string compiler_filter = "verify";
+  Options options;
   for (int i = 1; i < argc; i++) {
     std::string_view arg{argv[i]};
-    if (android::base::ConsumePrefix(&arg, "--output-dir=")) {
-      dir = arg;
-    } else if (android::base::ConsumePrefix(&arg, "--compiler-filter=")) {
-      compiler_filter = arg;
+    if (ConsumePrefix(&arg, "--output-dir=")) {
+      options.output_dir = arg;
+    } else if (ConsumePrefix(&arg, "--compiler-filter=")) {
+      options.compiler_filter = arg;
+    } else if (ConsumePrefix(&arg, "--use-profile=")) {
+      ParseBoolResult result = ParseBool(arg);
+      if (result == ParseBoolResult::kError) {
+        Usage(ART_FORMAT("Unrecognized --use-profile value: '{}'", arg));
+      }
+      options.use_profile = result == ParseBoolResult::kTrue;
+    } else if (ConsumePrefix(&arg, "--dex2oat-bin=")) {
+      options.dex2oat_bin = arg;
+    } else if (ConsumePrefix(&arg, "--android-root=")) {
+      ConsumeSuffix(&arg, "/");
+      options.android_root = arg;
+    } else if (ConsumePrefix(&arg, "--profile-file=")) {
+      options.profile_file = arg;
+    } else if (ConsumePrefix(&arg, "--instruction-set=")) {
+      options.instruction_set = arg;
     } else {
-      LOG(ERROR) << android::base::StringPrintf("Unrecognized argument: '%s'", argv[i]);
-      exit(EX_USAGE);
+      Usage(ART_FORMAT("Unrecognized argument: '{}'", argv[i]));
     }
   }
 
-  if (dir.empty()) {
-    LOG(ERROR) << "--output-dir must be specified";
-    exit(EX_USAGE);
+  if (options.output_dir.empty()) {
+    Usage("--output-dir must be specified");
   }
 
-  return art::GenerateBootImage(dir, compiler_filter);
+  if (options.dex2oat_bin.empty()) {
+    if (kIsTargetBuild) {
+      options.dex2oat_bin = GetCompilerExecutable();
+    } else {
+      Usage("--dex2oat-bin must be specified when running on host");
+    }
+  }
+
+  if (options.android_root.empty()) {
+    if (!kIsTargetBuild) {
+      Usage("--android-root must be specified when running on host");
+    }
+  }
+
+  if (options.use_profile && options.profile_file.empty()) {
+    if (kIsTargetBuild) {
+      options.profile_file = ART_FORMAT("{}/etc/boot-image.prof", GetArtRoot());
+    } else {
+      Usage("--profile-file must be specified when running on host and --use-profile is true");
+    }
+  }
+
+  if (options.instruction_set.empty()) {
+    if (kIsTargetBuild) {
+      options.instruction_set = GetInstructionSetString(kRuntimeISA);
+    } else {
+      Usage("--instruction-set must be specified when running on host");
+    }
+  }
+
+  return GenerateBootImage(options);
 }
+
+}  // namespace
+}  // namespace art
+
+int main(int argc, char** argv) { return art::Main(argc, argv); }
