571e77cc5ebc0a30027f59bc89d5f100de77d4a9
Prashant Dubey <prashantdubey@google.com>
2023-07-19 17:18:03 +0000

    Add helper script to package linux-x86 binaries
    Bug: 242716693
    Test: ./art/tools/build-linux-x86-host-tools.sh
    Change-Id: Ia4b58728d38720b2d3d4db4ee1196379b8629dca

---
+++ tools/build-linux-x86-host-tools.sh
@@ -0,0 +1,43 @@
+ #!/bin/bash
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+if [ -z "$OUT_DIR" ]; then
+  echo "OUT_DIR should be set for this script to run"
+  exit 1
+fi
+
+if [ -z "$DIST_DIR" ]; then
+  echo "DIST_DIR should be set when this script is run"
+  exit 1
+fi
+
+# Script should always be run from the root of the android tree
+cd $ANDROID_BUILD_TOP
+
+HOST_BINARIES=(
+  ${OUT_DIR}/host/linux-x86/bin/dex2oat64
+  ${OUT_DIR}/host/linux-x86/bin/dex2oatd64
+  ${OUT_DIR}/host/linux-x86/bin/dex2oat
+  ${OUT_DIR}/host/linux-x86/bin/dex2oatd
+  ${OUT_DIR}/host/linux-x86/bin/deapexer
+  ${OUT_DIR}/host/linux-x86/bin/debugfs_static
+)
+
+# Build and zip some binaries for the linux-x86 host
+build/soong/soong_ui.bash --make-mode USE_HOST_MUSL=true BUILD_HOST_static=true ${HOST_BINARIES}
+prebuilts/build-tools/linux-x86/bin/soong_zip -o ${DIST_DIR}/host_binaries.zip -j ${HOST_BINARIES[*]/#/-f }
