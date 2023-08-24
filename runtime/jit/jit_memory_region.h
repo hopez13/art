c41737e103104045e7cc22040f0dec4b3817e48a
David Srbecky <dsrbecky@google.com>
2023-08-24 14:08:33 +0100

    Move HasShouldDeoptimizeFlag from method header to CodeInfo.
    We don't have room for the extra bit if the code size is greater than 1GB.
    
    Bug: 288215378
    Bug: 288833213
    Bug: 297093110
    Test: ./art/test.py -b --host --64
    Change-Id: I0070b270c7fe03f2f6fbb26223e78de41305c547

--- runtime/jit/jit_memory_region.h
+++ runtime/jit/jit_memory_region.h
@@ -90,9 +90,7 @@ class JitMemoryRegion {
   // Returns pointer to copied code (within reserved_code region; after OatQuickMethodHeader).
   const uint8_t* CommitCode(ArrayRef<const uint8_t> reserved_code,
                             ArrayRef<const uint8_t> code,
-                            const uint8_t* stack_map,
-                            bool has_should_deoptimize_flag)
-      REQUIRES(Locks::jit_lock_);
+                            const uint8_t* stack_map) REQUIRES(Locks::jit_lock_);
 
   // Emit roots and stack map into the memory pointed by `roots_data` (despite it being const).
   bool CommitData(ArrayRef<const uint8_t> reserved_data,
