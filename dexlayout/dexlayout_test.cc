1b484a4c125352c01ca4939ccfe0cc3ad0fc2381
Martin Stjernholm <mast@google.com>
2023-08-31 21:59:41 +0100

    Disable cdex at build time.
    Code cleanup will come later.
    
    Test: banchan com.android.art && m
    Test: atest art_dexlayout_tests art_dexdiag_tests \
            art_standalone_dex2oat_tests art_standalone_dex2oat_cts_tests
    Bug: 256664509
    Change-Id: I3079ba4d4d09ca5edbd30593210451eebf98b924

--- dexlayout/dexlayout_test.cc
+++ dexlayout/dexlayout_test.cc
@@ -225,10 +225,9 @@ static const char kDuplicateCodeItemInputDex[] =
     "AHAAAAACAAAAAwAAAIwAAAADAAAAAQAAAJgAAAAFAAAABAAAAKQAAAAGAAAAAQAAAMQAAAABIAAA"
     "AwAAAOQAAAACIAAABwAAACQBAAADIAAAAwAAAFYBAAAAIAAAAQAAAGUBAAAAEAAAAQAAAHgBAAA=";
 
-// Returns the default compact dex option for dexlayout based on kDefaultCompactDexLevel.
+// Returns the default compact dex option for dexlayout.
 static std::vector<std::string> DefaultCompactDexOption() {
-  return (kDefaultCompactDexLevel == CompactDexLevel::kCompactDexLevelFast) ?
-      std::vector<std::string>{"-x", "fast"} : std::vector<std::string>{"-x", "none"};
+  return {"-x", "none"};
 }
 
 static void WriteBase64ToFile(const char* base64, File* file) {
@@ -549,17 +548,25 @@ class DexLayoutTest : public CommonArtTest {
 
 
 TEST_F(DexLayoutTest, FullPlainOutput) {
+  // TODO(b/256664509): Clean this up.
+  GTEST_SKIP() << "Compact dex is disabled";
+#if 0
   // Disable test on target.
   TEST_DISABLED_FOR_TARGET();
   std::string error_msg;
   ASSERT_TRUE(FullPlainOutputExec(&error_msg)) << error_msg;
+#endif
 }
 
 TEST_F(DexLayoutTest, DexFileOutput) {
+  // TODO(b/256664509): Clean this up.
+  GTEST_SKIP() << "Compact dex is disabled";
+#if 0
   // Disable test on target.
   TEST_DISABLED_FOR_TARGET();
   std::string error_msg;
   ASSERT_TRUE(DexFileOutputExec(&error_msg)) << error_msg;
+#endif
 }
 
 TEST_F(DexLayoutTest, DexFileLayout) {
