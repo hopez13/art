08533567a8b2854120fd5ea06f488b885fdbc8c4
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2023-09-05 04:30:03 +0000

    Revert "Refactor OatQuickMethodHeader for assembly stubs."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/042c4b67-a041-4394-afb5-705008bdca81). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I0c04939ef7254cb9d254700a1da50a8bc8764ae5

--- runtime/interpreter/mterp/nterp.h
+++ runtime/interpreter/mterp/nterp.h
@@ -17,7 +17,6 @@
 #ifndef ART_RUNTIME_INTERPRETER_MTERP_NTERP_H_
 #define ART_RUNTIME_INTERPRETER_MTERP_NTERP_H_
 
-#include "base/array_ref.h"
 #include "base/globals.h"
 
 extern "C" void* artNterpAsmInstructionStart[];
@@ -34,8 +33,6 @@ bool IsNterpSupported();
 bool CanRuntimeUseNterp();
 const void* GetNterpEntryPoint();
 const void* GetNterpWithClinitEntryPoint();
-ArrayRef<const uint8_t> NterpWithClinitImpl();
-ArrayRef<const uint8_t> NterpImpl();
 
 constexpr uint16_t kNterpHotnessValue = 0;
 
