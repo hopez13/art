fa81e8789b8bec1e5db7598b5663280e42057bfc
Ruben Ayrapetyan <ruben.ayrapetyan@arm.com>
2023-09-26 10:56:38 +0100

    16k: Use kPageSize instead of n * KB.
    Test: Same as for I5430741a8494b340ed7fd2d8692c41a59ad9c530.
          The whole patches chain was tested as a whole.
    Author: Branislav Rankov <branislav.rankov@arm.com>
    Change-Id: I10deda35d4694169606159706334a1a7524a196f

--- runtime/thread.cc
+++ runtime/thread.cc
@@ -152,7 +152,7 @@ static constexpr size_t kSuspendTimeDuringFlip = 5'000;
 // of the stack (lowest memory).  The higher portion of the memory
 // is protected against reads and the lower is available for use while
 // throwing the StackOverflow exception.
-constexpr size_t kStackOverflowProtectedSize = 4 * kMemoryToolStackGuardSizeScale * KB;
+constexpr size_t kStackOverflowProtectedSize = kMemoryToolStackGuardSizeScale * kPageSize;
 
 static const char* kThreadNameDuringStartup = "<native thread without managed peer>";
 
@@ -1344,12 +1344,15 @@ bool Thread::InitStackHwm() {
   tlsPtr_.stack_begin = reinterpret_cast<uint8_t*>(read_stack_base);
   tlsPtr_.stack_size = read_stack_size;
 
-  // The minimum stack size we can cope with is the overflow reserved bytes (typically
-  // 8K) + the protected region size (4K) + another page (4K).  Typically this will
-  // be 8+4+4 = 16K.  The thread won't be able to do much with this stack even the GC takes
-  // between 8K and 12K.
+  // The minimum stack size we can cope with is the overflow reserved bytes + the protected
+  // region size (one page) + another page size.
+  //
+  // The overflow reserved bytes is typically 8K if page size is 4K and one page size otherwise.
+  //
+  // On systems with 4K page size, typically the minimum stack size will be 8+4+4 = 16K.
+  // The thread won't be able to do much with this stack: even the GC takes between 8K and 12K.
   uint32_t min_stack = GetStackOverflowReservedBytes(kRuntimeISA) + kStackOverflowProtectedSize
-    + 4 * KB;
+    + kPageSize;
   if (read_stack_size <= min_stack) {
     // Note, as we know the stack is small, avoid operations that could use a lot of stack.
     LogHelper::LogLineLowStack(__PRETTY_FUNCTION__,
