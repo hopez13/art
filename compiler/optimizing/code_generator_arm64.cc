3614bd31d4287c326d39e6843e58d53055912352
Mythri Alle <mythria@google.com>
2023-10-19 13:51:56 +0000

    NOT FOR COMMIT: test overhead of always-on-profile check
    A minimal cl to just check the overhead of the check
    
    Change-Id: I43403778390e567834975208e88f0803bd4a5a97

--- compiler/optimizing/code_generator_arm64.cc
+++ compiler/optimizing/code_generator_arm64.cc
@@ -1034,6 +1034,39 @@ void CodeGeneratorARM64::EmitJumpTables() {
 }
 
 void CodeGeneratorARM64::Finalize() {
+#if (INLINE_CODE == 0)
+  // TODO(mythria): Implement this using slow paths.
+  ThreadOffset64 entrypoint_offset = GetThreadOffset<kArm64PointerSize>(kQuickRecordTraceEvent);
+  if (record_trace_entry_end.IsBound()) {
+    CHECK(!record_trace_entry.IsBound());
+    __ Bind(&record_trace_entry);
+    if (HasEmptyFrame()) {
+      __ Mov(x9, lr);
+      __ Ldr(lr, MemOperand(tr, entrypoint_offset.Int32Value()));
+      __ Blr(lr);
+      __ Mov(lr, x9);
+    } else {
+      __ Ldr(lr, MemOperand(tr, entrypoint_offset.Int32Value()));
+      __ Blr(lr);
+    }
+    __ B(&record_trace_entry_end);
+  }
+
+  if (record_trace_exit_end.IsBound()) {
+    __ Bind(&record_trace_exit);
+    if (HasEmptyFrame()) {
+      __ Mov(x9, lr);
+      __ Ldr(lr, MemOperand(tr, entrypoint_offset.Int32Value()));
+      __ Blr(lr);
+      __ Mov(lr, x9);
+    } else {
+      __ Ldr(lr, MemOperand(tr, entrypoint_offset.Int32Value()));
+      __ Blr(lr);
+    }
+    __ B(&record_trace_exit_end);
+  }
+#endif
+
   EmitJumpTables();
 
   // Emit JIT baker read barrier slow paths.
@@ -1306,6 +1339,61 @@ void CodeGeneratorARM64::MaybeIncrementHotness(bool is_frame_entry) {
   }
 }
 
+#if (INLINE_CODE == 1)
+void CodeGeneratorARM64::RecordTraceEvent(bool method_entry) {
+  if (!kAlwaysOnProfile) {
+    return;
+  }
+
+  vixl::aarch64::Label done, update_entry;
+  MacroAssembler* masm = GetVIXLAssembler();
+  __ Cmp(mr, 0x1100);
+  __ B(ne, &done);
+  UseScratchRegisterScope temps(masm);
+  Register index = temps.AcquireX();
+  Register addr = temps.AcquireX();
+  size_t trace_buffer_index_addr =
+    Thread::TraceBufferIndexOffset<kArm64PointerSize>().SizeValue();
+  __ Ldr(index, MemOperand(tr, trace_buffer_index_addr));
+  __ Ldr(addr, MemOperand(tr, Thread::TraceBufferPtrOffset<kArm64PointerSize>().SizeValue()));
+  __ Cmp(index, addr);
+  __ B(gt, &update_entry);
+  __ ComputeAddress(index, MemOperand(addr, (kPerThreadBufSize - 1) << TIMES_8));
+  __ Bind(&update_entry);
+  if (method_entry) {
+    __ Str(kArtMethodRegister, MemOperand(index, -8, PostIndex));
+  } else {
+    Register ts = addr;
+    __ Mrs(ts, (SystemRegister)SYS_CNTVCT_EL0);
+    __ Orr(ts, ts, Operand(1));
+    __ Str(ts, MemOperand(index, -8, PostIndex));
+  }
+  __ Str(index, MemOperand(tr, Thread::TraceBufferIndexOffset<kArm64PointerSize>().SizeValue()));
+  __ Bind(&done);
+}
+#else
+void CodeGeneratorARM64::RecordTraceEvent(bool method_entry) {
+  if (!kAlwaysOnProfile) {
+    return;
+  }
+
+  MacroAssembler* masm = GetVIXLAssembler();
+  UseScratchRegisterScope temps(masm);
+  Register addr = temps.AcquireX();
+  __ Ldr(addr, MemOperand(tr, Thread::TraceBufferPtrOffset<kArm64PointerSize>().SizeValue()));
+  if (method_entry) {
+    CHECK(!record_trace_entry_end.IsBound());
+    __ Cbnz(addr, &record_trace_entry);
+    __ Bind(&record_trace_entry_end);
+  } else {
+    __ Cbnz(addr, &record_trace_exit);
+    if (!record_trace_exit_end.IsBound()) {
+      __ Bind(&record_trace_exit_end);
+    }
+  }
+}
+#endif
+
 void CodeGeneratorARM64::GenerateFrameEntry() {
   MacroAssembler* masm = GetVIXLAssembler();
 
@@ -1419,6 +1507,7 @@ void CodeGeneratorARM64::GenerateFrameEntry() {
       __ Str(wzr, MemOperand(sp, GetStackOffsetOfShouldDeoptimizeFlag()));
     }
   }
+  RecordTraceEvent(true);
   MaybeIncrementHotness(/* is_frame_entry= */ true);
   MaybeGenerateMarkingRegisterCheck(/* code= */ __LINE__);
 }
@@ -1452,7 +1541,10 @@ void CodeGeneratorARM64::GenerateFrameExit() {
     }
     GetAssembler()->cfi().AdjustCFAOffset(-frame_size);
   }
+
+  RecordTraceEvent(false);
   __ Ret();
+
   GetAssembler()->cfi().RestoreState();
   GetAssembler()->cfi().DefCFAOffset(GetFrameSize());
 }
