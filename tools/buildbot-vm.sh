baea4e4f4351fa5337dd944992fee9d67418d281
Jaeheon Yi <jaeheon@google.com>
2023-11-02 23:53:43 -0700

    riscv64: update vm img to mantic minotaur
    This release has the V extension.
    
    Test: manual run of `buildbot-vm.sh boot`
    
    Bug: 283082047
    Change-Id: Ib43815a7b6d954cc52f33c74b1a913ee2c2c4d48

--- tools/buildbot-vm.sh
+++ tools/buildbot-vm.sh
@@ -45,21 +45,24 @@ if [[ $action = create ]]; then
 
     # sudo apt install qemu-system-<arch> qemu-efi cloud-image-utils
 
-    # Get the cloud image for Ubunty 22.04 (Jammy)
-    wget "http://cloud-images.ubuntu.com/releases/22.04/release/$ART_TEST_VM_IMG"
-
     if [[ "$TARGET_ARCH" = "riscv64" ]]; then
-        # Get U-Boot for Ubuntu 22.04 (Jammy)
+        # Get the cloud image for Ubunty 23.10 (Minotaur)
+        wget "http://cloud-images.ubuntu.com/releases/23.10/release/$ART_TEST_VM_IMG"
+
+        # Get U-Boot for Ubuntu 23.10 (Minotaur)
         get_stable_binary \
-            u/u-boot/u-boot-qemu_2022.01+dfsg-2ubuntu2.3_all.deb \
+            u/u-boot/u-boot-qemu_2023.07+dfsg-1ubuntu2_all.deb \
             usr/lib/u-boot/qemu-riscv64_smode/uboot.elf
 
-        # Get OpenSBI for Ubuntu 22.04 (Jammy)
+        # Get OpenSBI for Ubuntu 23.10 (Minotaur)
         get_stable_binary \
-            o/opensbi/opensbi_1.3-1ubuntu0.22.04.1_all.deb \
+            o/opensbi/opensbi_1.3-1ubuntu0.23.04.1_all.deb \
             usr/lib/riscv64-linux-gnu/opensbi/generic/fw_jump.elf
 
     elif [[ "$TARGET_ARCH" = "arm64" ]]; then
+        # Get the cloud image for Ubunty 22.04 (Jammy)
+        wget "http://cloud-images.ubuntu.com/releases/22.04/release/$ART_TEST_VM_IMG"
+
         # Get EFI (ARM64) for Ubuntu 22.04 (Jammy)
         get_stable_binary \
             e/edk2/qemu-efi-aarch64_2022.02-3ubuntu0.22.04.1_all.deb \
@@ -103,10 +106,13 @@ elif [[ $action = boot ]]; then
 (
     cp "$(dirname $0)/user-data.img" "$ART_TEST_VM_DIR/user-data.img"
     cd "$ART_TEST_VM_DIR"
+    # Keep -cpu flag aligned with 'host/libs/vm_manager/qemu_manager.cpp' in
+    # 'device/google/cuttlefish'.
     if [[ "$TARGET_ARCH" = "riscv64" ]]; then
         (qemu-system-riscv64 \
             -m 16G \
             -smp 8 \
+            -cpu rv64,v=true,elen=64,vlen=128,zba=true,zbb=true,zbs=true \
             -M virt \
             -nographic \
             -bios fw_jump.elf \
