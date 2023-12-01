62d55426e28071a95de90551d6676c48a1907d7f
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2023-12-01 10:23:10 +0000

    Revert "Add a pass to bg-dexopt to dexopt apps with small profil..."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/dc352553-0fde-4a1b-949f-a7d39ce5c07d). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I528d96f58c0c746902d793e82b871f5c468087d2

--- libartservice/service/java/com/android/server/art/Dexopter.java
+++ libartservice/service/java/com/android/server/art/Dexopter.java
@@ -562,12 +562,9 @@ public abstract class Dexopter<DexInfoType extends DetailedDexInfo> {
             @Nullable ProfilePath referenceProfile) throws RemoteException {
         OutputProfile output = buildOutputProfile(dexInfo, false /* isPublic */);
 
-        var options = new MergeProfileOptions();
-        options.forceMerge = (mParams.getFlags() & ArtFlags.FLAG_FORCE_MERGE_PROFILE) != 0;
-
         try {
             if (mInjector.getArtd().mergeProfiles(getCurProfiles(dexInfo), referenceProfile, output,
-                        List.of(dexInfo.dexPath()), options)) {
+                        List.of(dexInfo.dexPath()), new MergeProfileOptions())) {
                 return ProfilePath.tmpProfilePath(output.profilePath);
             }
         } catch (ServiceSpecificException e) {
