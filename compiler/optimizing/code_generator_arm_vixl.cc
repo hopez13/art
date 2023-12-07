7283c0d5e98dedb3b0827a300124a6badadac7c4
Nicolas Geoffray <ngeoffray@google.com>
2023-12-07 14:19:45 +0000

    Don't lookup the profiling info in EnqueueOptimizedCompilation.
    This avoids one use of the jit lock in mutator threads.
    
    Test: test.py
    Bug: 315300060
    Change-Id: I9506bd60794bc8d9f86c3d23ff5ed53a2cf36fce

--- compiler/optimizing/code_generator_arm_vixl.cc
+++ compiler/optimizing/code_generator_arm_vixl.cc
@@ -970,7 +970,9 @@ class MethodEntryExitHooksSlowPathARMVIXL : public SlowPathCodeARMVIXL {
 
 class CompileOptimizedSlowPathARMVIXL : public SlowPathCodeARMVIXL {
  public:
-  CompileOptimizedSlowPathARMVIXL() : SlowPathCodeARMVIXL(/* instruction= */ nullptr) {}
+  CompileOptimizedSlowPathARMVIXL(vixl32::Register counter)
+      : SlowPathCodeARMVIXL(/* instruction= */ nullptr),
+        counter_(counter) {}
 
   void EmitNativeCode(CodeGenerator* codegen) override {
     uint32_t entry_point_offset =
@@ -980,6 +982,7 @@ class CompileOptimizedSlowPathARMVIXL : public SlowPathCodeARMVIXL {
     // Note: we don't record the call here (and therefore don't generate a stack
     // map), as the entrypoint should never be suspended.
     __ Blx(lr);
+    __ Mov(counter_, ProfilingInfo::GetOptimizeThreshold());
     __ B(GetExitLabel());
   }
 
@@ -988,6 +991,8 @@ class CompileOptimizedSlowPathARMVIXL : public SlowPathCodeARMVIXL {
   }
 
  private:
+  vixl32::Register counter_;
+
   DISALLOW_COPY_AND_ASSIGN(CompileOptimizedSlowPathARMVIXL);
 };
 
@@ -2289,20 +2294,21 @@ void CodeGeneratorARMVIXL::MaybeIncrementHotness(bool is_frame_entry) {
   }
 
   if (GetGraph()->IsCompilingBaseline() && !Runtime::Current()->IsAotCompiler()) {
-    SlowPathCodeARMVIXL* slow_path = new (GetScopedAllocator()) CompileOptimizedSlowPathARMVIXL();
-    AddSlowPath(slow_path);
     ProfilingInfo* info = GetGraph()->GetProfilingInfo();
     DCHECK(info != nullptr);
     DCHECK(!HasEmptyFrame());
     uint32_t address = reinterpret_cast32<uint32_t>(info);
     UseScratchRegisterScope temps(GetVIXLAssembler());
     vixl32::Register tmp = temps.Acquire();
+    SlowPathCodeARMVIXL* slow_path =
+        new (GetScopedAllocator()) CompileOptimizedSlowPathARMVIXL(tmp);
+    AddSlowPath(slow_path);
     __ Mov(lr, address);
     __ Ldrh(tmp, MemOperand(lr, ProfilingInfo::BaselineHotnessCountOffset().Int32Value()));
     __ Adds(tmp, tmp, -1);
     __ B(cc, slow_path->GetEntryLabel());
-    __ Strh(tmp, MemOperand(lr, ProfilingInfo::BaselineHotnessCountOffset().Int32Value()));
     __ Bind(slow_path->GetExitLabel());
+    __ Strh(tmp, MemOperand(lr, ProfilingInfo::BaselineHotnessCountOffset().Int32Value()));
   }
 }
 
