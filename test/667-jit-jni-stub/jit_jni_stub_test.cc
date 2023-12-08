a11461275b12c0a4f7afa352537b5489441c26dc
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2023-12-08 08:17:04 +0000

    Revert "Remove logic of polling liveness of compiled code."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/b8e8a0e4-e79f-4343-9f35-57e5ab1cc7ed). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Ia7528fdf17d5d9a41688ea20ab5289509b7049b5

--- test/667-jit-jni-stub/jit_jni_stub_test.cc
+++ test/667-jit-jni-stub/jit_jni_stub_test.cc
@@ -25,6 +25,17 @@
 
 namespace art {
 
+// Local class declared as a friend of JitCodeCache so that we can access its internals.
+class JitJniStubTestHelper {
+ public:
+  static bool isNextJitGcFull(Thread* self) REQUIRES_SHARED(Locks::mutator_lock_) {
+    CHECK(Runtime::Current()->GetJit() != nullptr);
+    jit::JitCodeCache* cache = Runtime::Current()->GetJit()->GetCodeCache();
+    MutexLock mu(self, *Locks::jit_lock_);
+    return cache->ShouldDoFullCollection();
+  }
+};
+
 // Calls through to a static method with signature "()V".
 extern "C" JNIEXPORT
 void Java_Main_callThrough(JNIEnv* env, jclass, jclass klass, jstring methodName) {
@@ -40,15 +51,13 @@ void Java_Main_jitGc(JNIEnv*, jclass) {
   CHECK(Runtime::Current()->GetJit() != nullptr);
   jit::JitCodeCache* cache = Runtime::Current()->GetJit()->GetCodeCache();
   ScopedObjectAccess soa(Thread::Current());
-  cache->InvalidateAllCompiledCode();
   cache->GarbageCollectCache(Thread::Current());
 }
 
 extern "C" JNIEXPORT
 jboolean Java_Main_isNextJitGcFull(JNIEnv*, jclass) {
-  // Because we invalidate all compiled code above, we currently always do a
-  // full GC.
-  return true;
+  ScopedObjectAccess soa(Thread::Current());
+  return JitJniStubTestHelper::isNextJitGcFull(soa.Self());
 }
 
 }  // namespace art
