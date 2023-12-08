a40f3ea098f9aeb2c32ba4a1235b538af2a5d89d
Santiago Aboy Solanes <solanes@google.com>
2023-12-08 14:01:44 +0000

    Revert "Disable write-barrier elimination pass"
    This reverts commit 5a3271d7caafefd10a20f5a5db09d2c178838b76.
    
    Reason for revert:
    This CL has two fixes (codegen not doing a null check if a write
    barrier is being relied on, and codegen not recomputing
    skipping write barriers), regression tests, a new
    runtime check which runs in debug mode for the CC GC to ensure
    that the card table is set correctly for skipped write barriers,
    and new compile time (graph checker) tests to ensure graph
    consistency.
    
    This patchset updates the WriteBarrierKind to be
    {emit being relied on, emit not being relied on, dont emit},
    which leaves the null check skip implementation to codegen.
    
    Test 2247- is removed from knownfailures.json but still skipped
    in MTS due to SLO requirements.
    
    Test: art/test/testrunner/testrunner.py --host --64 --optimizing -b
    Bug: 301833859
    Bug: 310755375
    Bug: 260843353
    Change-Id: I025597e284b2765986e2091538680ee629fb5ae7

--- compiler/optimizing/write_barrier_elimination.cc
+++ compiler/optimizing/write_barrier_elimination.cc
@@ -21,9 +21,6 @@
 #include "base/scoped_arena_containers.h"
 #include "optimizing/nodes.h"
 
-// TODO(b/310755375, solanes): Disable WBE while we investigate crashes.
-constexpr bool kWBEEnabled = false;
-
 namespace art HIDDEN {
 
 class WBEVisitor final : public HGraphVisitor {
@@ -58,7 +55,7 @@ class WBEVisitor final : public HGraphVisitor {
       DCHECK(it->second->AsInstanceFieldSet()->GetWriteBarrierKind() !=
              WriteBarrierKind::kDontEmit);
       DCHECK_EQ(it->second->GetBlock(), instruction->GetBlock());
-      it->second->AsInstanceFieldSet()->SetWriteBarrierKind(WriteBarrierKind::kEmitNoNullCheck);
+      it->second->AsInstanceFieldSet()->SetWriteBarrierKind(WriteBarrierKind::kEmitBeingReliedOn);
       instruction->SetWriteBarrierKind(WriteBarrierKind::kDontEmit);
       MaybeRecordStat(stats_, MethodCompilationStat::kRemovedWriteBarrier);
     } else {
@@ -84,7 +81,7 @@ class WBEVisitor final : public HGraphVisitor {
       DCHECK(it->second->IsStaticFieldSet());
       DCHECK(it->second->AsStaticFieldSet()->GetWriteBarrierKind() != WriteBarrierKind::kDontEmit);
       DCHECK_EQ(it->second->GetBlock(), instruction->GetBlock());
-      it->second->AsStaticFieldSet()->SetWriteBarrierKind(WriteBarrierKind::kEmitNoNullCheck);
+      it->second->AsStaticFieldSet()->SetWriteBarrierKind(WriteBarrierKind::kEmitBeingReliedOn);
       instruction->SetWriteBarrierKind(WriteBarrierKind::kDontEmit);
       MaybeRecordStat(stats_, MethodCompilationStat::kRemovedWriteBarrier);
     } else {
@@ -112,8 +109,7 @@ class WBEVisitor final : public HGraphVisitor {
       DCHECK(it->second->IsArraySet());
       DCHECK(it->second->AsArraySet()->GetWriteBarrierKind() != WriteBarrierKind::kDontEmit);
       DCHECK_EQ(it->second->GetBlock(), instruction->GetBlock());
-      // We never skip the null check in ArraySets so that value is already set.
-      DCHECK(it->second->AsArraySet()->GetWriteBarrierKind() == WriteBarrierKind::kEmitNoNullCheck);
+      it->second->AsArraySet()->SetWriteBarrierKind(WriteBarrierKind::kEmitBeingReliedOn);
       instruction->SetWriteBarrierKind(WriteBarrierKind::kDontEmit);
       MaybeRecordStat(stats_, MethodCompilationStat::kRemovedWriteBarrier);
     } else {
@@ -156,10 +152,8 @@ class WBEVisitor final : public HGraphVisitor {
 };
 
 bool WriteBarrierElimination::Run() {
-  if (kWBEEnabled) {
-    WBEVisitor wbe_visitor(graph_, stats_);
-    wbe_visitor.VisitReversePostOrder();
-  }
+  WBEVisitor wbe_visitor(graph_, stats_);
+  wbe_visitor.VisitReversePostOrder();
   return true;
 }
 
