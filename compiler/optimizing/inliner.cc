75a90cd6b29fbc4dc85a2e816ad860d278b27659
Nicolas Geoffray <ngeoffray@google.com>
2023-12-17 19:34:50 +0000

    Reland "Move the construction of ProfilingInfo in the compiler."
    This reverts commit 9fedb9f473fd77f31285203f5baa9533b8e21ce6.
    
    Reason for reland:
    - Use CodeGenerator::IsImplementedIntrinsic in IsInlineCacheUseful, to
      match inliner behavior.
    - Address some missing type propagation opportunities in aosp/2880687
    - Be robust when there is a missing inline cache.
    
    Test: test.py
    
    Change-Id: Ib6e4a624174d6891a0fd425af88a9c16e09afa99

--- compiler/optimizing/inliner.cc
+++ compiler/optimizing/inliner.cc
@@ -665,9 +665,14 @@ HInliner::InlineCacheType HInliner::GetInlineCacheJIT(
     return kInlineCacheNoData;
   }
 
-  Runtime::Current()->GetJit()->GetCodeCache()->CopyInlineCacheInto(
-      *profiling_info->GetInlineCache(invoke_instruction->GetDexPc()),
-      classes);
+  InlineCache* cache = profiling_info->GetInlineCache(invoke_instruction->GetDexPc());
+  if (cache == nullptr) {
+    // This shouldn't happen, but be robust, warn about it, and return that we
+    // don't have any inline cache data.
+    LOG(WARNING) << "No inline cache found for " << caller->PrettyMethod();
+    return kInlineCacheNoData;
+  }
+  Runtime::Current()->GetJit()->GetCodeCache()->CopyInlineCacheInto(*cache, classes);
   return GetInlineCacheType(*classes);
 }
 
