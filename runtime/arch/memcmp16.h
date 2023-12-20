04b7fea8a8ca1583552a23d972b0c1e420ac8ac6
Dmitrii Ishcheikin <ishcheikin@google.com>
2023-12-20 15:18:47 +0000

    Hide libart-runtime symbols
    This change only adds HIDDEN attribute to art namespace in source and
    header files.
    Files that have exported symbols will be hanled in a separate CL.
    
    Bug: 260881207
    Test: presubmit
    Test: abtd app_compat_drm
    Test: abtd app_compat_top_100
    Test: abtd app_compat_banking
    Change-Id: I76439334c7d153c860d1df00db68540f799a4125

--- runtime/arch/memcmp16.h
+++ runtime/arch/memcmp16.h
@@ -20,6 +20,8 @@
 #include <cstddef>
 #include <cstdint>
 
+#include "base/macros.h"
+
 // memcmp16 support.
 //
 // This can either be optimized assembly code, in which case we expect a function __memcmp16,
@@ -50,7 +52,7 @@ static inline int32_t MemCmp16(const uint16_t* s0, const uint16_t* s1, size_t co
 extern "C" int32_t memcmp16_generic_static(const uint16_t* s0, const uint16_t* s1, size_t count);
 #endif
 
-namespace art {
+namespace art HIDDEN {
 
 namespace testing {
 
