1fe2f5c49196c4cf90d28b16f46ebf6780239ada
Jiakai Zhang <jiakaiz@google.com>
2023-12-21 11:32:21 +0000

    Throttle the creation of ScheduledExecutorService in Debouncer.
    Looks like ScheduledExecutorService creates a thread during
    initialization, rather than when the actual action is run, so the
    debouncer ends up creating too many threads.
    
    This CL fixes the problem by caching the executor until the actual task
    is run.
    
    Bug: 317234694
    Test: atest ArtServiceTests
    Change-Id: I4d688da7f466ac98c1d4302a390f7aa5aee58830

--- libartservice/service/java/com/android/server/art/Debouncer.java
+++ libartservice/service/java/com/android/server/art/Debouncer.java
@@ -19,6 +19,8 @@ package com.android.server.art;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
 
+import com.android.internal.annotations.GuardedBy;
+
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
@@ -32,12 +34,26 @@ import java.util.function.Supplier;
 public class Debouncer {
     @NonNull private Supplier<ScheduledExecutorService> mScheduledExecutorFactory;
     private final long mIntervalMs;
-    @Nullable private ScheduledFuture<?> mCurrentTask = null;
+    @GuardedBy("this") @Nullable private ScheduledFuture<?> mCurrentTask = null;
+    @GuardedBy("this") @Nullable private ScheduledExecutorService mExecutor = null;
 
     public Debouncer(
             long intervalMs, @NonNull Supplier<ScheduledExecutorService> scheduledExecutorFactory) {
         mScheduledExecutorFactory = scheduledExecutorFactory;
         mIntervalMs = intervalMs;
+    }
+
+    private void runTask(@NonNull Runnable command, @NonNull ScheduledExecutorService executor) {
+        synchronized (this) {
+            // In rare cases, at this point, another task may have been scheduled on the same
+            // executor, but that's okay. Either that task won't pass the check below or it will be
+            // cancelled, so the same executor won't be shut down twice.
+            if (mExecutor == executor) {
+                mExecutor.shutdown();
+                mExecutor = null;
+            }
+        }
+        command.run();
     }
 
     /**
@@ -48,8 +64,10 @@ public class Debouncer {
         if (mCurrentTask != null) {
             mCurrentTask.cancel(false /* mayInterruptIfRunning */);
         }
-        ScheduledExecutorService executor = mScheduledExecutorFactory.get();
-        mCurrentTask = executor.schedule(command, mIntervalMs, TimeUnit.MILLISECONDS);
-        executor.shutdown();
+        if (mExecutor == null) {
+            mExecutor = mScheduledExecutorFactory.get();
+        }
+        mCurrentTask = mExecutor.schedule(
+                () -> runTask(command, mExecutor), mIntervalMs, TimeUnit.MILLISECONDS);
     }
 }
