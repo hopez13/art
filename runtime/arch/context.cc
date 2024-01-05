069e6c0fcb5d187e15bd848cfb5872e89e565093
Chris Jones <christopher.jones@arm.com>
2024-01-05 16:30:57 +0000

    Refactor C++ entrypoints
    Refactor all C++ entrypoint function (those that can be called from
    assembly entrypoints) declarations into a new header file. Now all
    C++ entrypoint functions have a single declaration, inside a header
    that can be included as needed. This removes the need for additional
    declarations to be made which, if not type checked, could be
    mismatched with the function definition.
    
    A macro is used to declare each entrypoint inside the header but also
    allows operations to be done on all C++ entrypoints similar to
    assembly entrypoints with its macro in quick_entrypoints_list.h.
    
    Note: linkers, unlike compilers, do not typically type check
    function declarations against defintions, e.g: when linking between
    two libraries. This can cause mismatches which are not caught when
    building and therefore become difficult to debug later.
    
    This patch aims to unify all C++ entrypoint function declarations,
    i.e: those C++ functions called from assembly entrypoints.
    By doing this any unnecessary and potentially conflicting
    declarations can be removed to ensure a single source of truth.
    
    Author: Chris Jones <christopher.jones@arm.com>
    
    Test: test.py --host --target
    Change-Id: I5b0de39eed96f666e7bb4205906c5d9369615aa8

--- runtime/arch/context.cc
+++ runtime/arch/context.cc
@@ -22,6 +22,8 @@ Context* Context::Create() {
   return new RuntimeContextType;
 }
 
+// Copy the GPRs and FPRs from the given thread's context to the given buffers. This function
+// expects that a long jump (art_quick_do_long_jump) is called afterwards.
 extern "C" void artContextCopyForLongJump(Thread* self, uintptr_t* gprs, uintptr_t* fprs) {
   self->GetLongJumpContext()->CopyContextTo(gprs, fprs);
 }
