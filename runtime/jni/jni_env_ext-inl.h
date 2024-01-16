f35c48effbe9748aa9a2f2bf760ca8b9b13bcd02
Vladimir Marko <vmarko@google.com>
2024-01-16 09:43:46 +0100

    Move previous segment state to `LocalReferenceTable`.
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing
    Bug: 172332525
    Change-Id: Ied2bc55ac8f3a473adf10bf48bd96b086764f9cd

--- runtime/jni/jni_env_ext-inl.h
+++ runtime/jni/jni_env_ext-inl.h
@@ -27,9 +27,9 @@ namespace art {
 template<typename T>
 inline T JNIEnvExt::AddLocalReference(ObjPtr<mirror::Object> obj) {
   std::string error_msg;
-  IndirectRef ref = locals_.Add(local_ref_cookie_, obj, &error_msg);
+  IndirectRef ref = locals_.Add(obj, &error_msg);
   if (UNLIKELY(ref == nullptr)) {
-    // This is really unexpected if we allow resizing local IRTs...
+    // This is really unexpected if we allow resizing LRTs...
     LOG(FATAL) << error_msg;
     UNREACHABLE();
   }
