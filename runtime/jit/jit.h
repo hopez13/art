74707904b2bab675cbe2d42e96719118689f8002
Nicolas Geoffray <ngeoffray@google.com>
2024-02-05 15:33:34 +0000

    In ForceJitCompile in tests, do pattern match first.
    If successful, we won't be able to check the entrypoint, so just return.
    
    Test: test.py
    Bug: 218263452
    Change-Id: I5047ab8ab377b6130008ce85c7b8686c3c8bfee9

--- runtime/jit/jit.h
+++ runtime/jit/jit.h
@@ -519,6 +519,8 @@ class Jit {
   EXPORT void MaybeEnqueueCompilation(ArtMethod* method, Thread* self)
       REQUIRES_SHARED(Locks::mutator_lock_);
 
+  EXPORT static bool TryPatternMatch(ArtMethod* method, CompilationKind compilation_kind);
+
  private:
   Jit(JitCodeCache* code_cache, JitOptions* options);
 
