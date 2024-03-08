dfbf5cd61be331f7ed0a078cd8a5831b449367b6
Santiago Aboy Solanes <solanes@google.com>
2024-03-08 10:26:24 +0000

    Update odrefresh to use the full CLC
    Test: m test-art-host-gtest-art_runtime_tests64
    Test: Manually compiled in both the new way, and the backwards
          compatible way and made sure we are successfully loading
          the oat files
    Bug: 293298276
    Change-Id: I6cc99044544f32f99ffe6720861bb66fac4cf2f0

--- runtime/oat/oat.h
+++ runtime/oat/oat.h
@@ -45,7 +45,7 @@ class EXPORT PACKED(4) OatHeader {
  public:
   static constexpr std::array<uint8_t, 4> kOatMagic { { 'o', 'a', 't', '\n' } };
   // Last oat version changed reason: store resolved MethodType-s in .bss.
-  static constexpr std::array<uint8_t, 4> kOatVersion{{'2', '4', '1', '\0'}};
+  static constexpr std::array<uint8_t, 4> kOatVersion{{'2', '4', '2', '\0'}};
 
   static constexpr const char* kDex2OatCmdLineKey = "dex2oat-cmdline";
   static constexpr const char* kDebuggableKey = "debuggable";
