aead14ea7a62c5850435c9e54194390ac9dfd243
Vladimir Marko <vmarko@google.com>
2024-04-18 13:21:15 +0000

    Optimizing: Treat app image objects as non-movable.
    Treat app image objects similar to boot image objects and
    avoid unnecessary read barriers for app image `HLoadClass`
    and `HInstanceOf` checks with app image `HLoadClass` input.
    
    Extend other optimizations to treat app image classes the
    same way as boot image classes even though this remains
    mostly dormant because we currently do not initialize app
    image classes with class initializers; the experimental
    flag `--initialize-app-image-classes` is false by default.
    
    Test: m test-art-host-gtest
    Test: testrunner.py --host --optimizing --speed-profile
    Bug: 38313278
    Change-Id: I359dd8897f6d128213602f5731d40edace298ab8

---
+++ test/732-checker-app-image/src/Main.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println($noinline$getAppImageClass().getName());
+        System.out.println($noinline$getNonAppImageClass().getName());
+
+        $noinline$callAppImageClassNop();
+        $noinline$callAppImageClassWithClinitNop();
+        $noinline$callNonAppImageClassNop();
+    }
+
+    /// CHECK-START: java.lang.Class Main.$noinline$getAppImageClass() builder (after)
+    /// CHECK:            LoadClass load_kind:BssEntry in_image:true
+    public static Class<?> $noinline$getAppImageClass() {
+        return AppImageClass.class;
+    }
+
+    /// CHECK-START: java.lang.Class Main.$noinline$getNonAppImageClass() builder (after)
+    /// CHECK:            LoadClass load_kind:BssEntry in_image:false
+    public static Class<?> $noinline$getNonAppImageClass() {
+        return NonAppImageClass.class;
+    }
+
+    /// CHECK-START: void Main.$noinline$callAppImageClassNop() builder (after)
+    /// CHECK:            InvokeStaticOrDirect clinit_check:none
+
+    /// CHECK-START: void Main.$noinline$callAppImageClassNop() builder (after)
+    /// CHECK-NOT:        LoadClass
+    /// CHECK-NOT:        ClinitCheck
+
+    /// CHECK-START: void Main.$noinline$callAppImageClassNop() inliner (after)
+    /// CHECK-NOT:        LoadClass
+    /// CHECK-NOT:        ClinitCheck
+    /// CHECK-NOT:        InvokeStaticOrDirect
+    public static void $noinline$callAppImageClassNop() {
+        AppImageClass.$inline$nop();
+    }
+
+    /// CHECK-START: void Main.$noinline$callAppImageClassWithClinitNop() builder (after)
+    /// CHECK:            LoadClass load_kind:BssEntry in_image:true
+    /// CHECK:            ClinitCheck
+    /// CHECK:            InvokeStaticOrDirect clinit_check:explicit
+
+    /// CHECK-START: void Main.$noinline$callAppImageClassWithClinitNop() inliner (after)
+    /// CHECK:            LoadClass load_kind:BssEntry in_image:true
+    /// CHECK:            ClinitCheck
+
+    /// CHECK-START: void Main.$noinline$callAppImageClassWithClinitNop() inliner (after)
+    /// CHECK-NOT:        InvokeStaticOrDirect
+    public static void $noinline$callAppImageClassWithClinitNop() {
+        AppImageClassWithClinit.$inline$nop();
+    }
+
+    /// CHECK-START: void Main.$noinline$callNonAppImageClassNop() builder (after)
+    /// CHECK:            LoadClass load_kind:BssEntry in_image:false
+    /// CHECK:            ClinitCheck
+    /// CHECK:            InvokeStaticOrDirect clinit_check:explicit
+
+    /// CHECK-START: void Main.$noinline$callNonAppImageClassNop() inliner (after)
+    /// CHECK:            LoadClass load_kind:BssEntry in_image:false
+    /// CHECK:            ClinitCheck
+
+    /// CHECK-START: void Main.$noinline$callNonAppImageClassNop() inliner (after)
+    /// CHECK-NOT:        InvokeStaticOrDirect
+    public static void $noinline$callNonAppImageClassNop() {
+        NonAppImageClass.$inline$nop();
+    }
+}
+
+class AppImageClass {  // Included in the profile.
+    public static void $inline$nop() {}
+}
+
+class AppImageClassWithClinit {
+    static boolean doThrow = false;
+    static {
+        if (doThrow) {
+            throw new Error();
+        }
+    }
+
+    public static void $inline$nop() {}
+}
+
+class NonAppImageClass {  // Not included in the profile.
+    public static void $inline$nop() {}
+}
