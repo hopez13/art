84687c8fab49b00a0243c8988a75d5e883c47c9b
Jiakai Zhang <jiakaiz@google.com>
2024-04-24 15:07:29 +0100

    Update file GC for Pre-reboot Dexopt.
    - Keep ".staged" files if Pre-reboot Dexopt has run.
    - Remove ".staged" files if Pre-reboot Dexopt has not run.
    
    Bug: 311377497
    Test: atest ArtServiceTests
    Test: m test-art-host-gtest-art_artd_tests
    Change-Id: If57e5b6af036ed02ba6f5671dee985dc6d554023

--- libartservice/service/java/com/android/server/art/ArtManagerLocal.java
+++ libartservice/service/java/com/android/server/art/ArtManagerLocal.java
@@ -1089,8 +1089,9 @@ public final class ArtManagerLocal {
                     runtimeArtifactsToKeep.addAll(artifactLists.runtimeArtifacts());
                 }
             }
-            return mInjector.getArtd().cleanup(
-                    profilesToKeep, artifactsToKeep, vdexFilesToKeep, runtimeArtifactsToKeep);
+            return mInjector.getArtd().cleanup(profilesToKeep, artifactsToKeep, vdexFilesToKeep,
+                    runtimeArtifactsToKeep,
+                    SdkLevel.isAtLeastV() && mInjector.getPreRebootDexoptJob().hasStarted());
         } catch (RemoteException e) {
             Utils.logArtdException(e);
             return 0;
