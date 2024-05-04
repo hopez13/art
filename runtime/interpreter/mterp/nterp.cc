27784bcbae447864cf2b2339629d9a60a08c90f3
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-05-04 16:04:47 +0000

    Revert "Adjust JIT thresholds."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/2e1b7b8c-1e91-4c58-b640-7d2188e4d162). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I6cb4e3ed4eaecdcdf36e1cab886b8a52523c93ae

--- runtime/interpreter/mterp/nterp.cc
+++ runtime/interpreter/mterp/nterp.cc
@@ -118,9 +118,8 @@ void CheckNterpAsmConstants() {
 inline void UpdateHotness(ArtMethod* method) REQUIRES_SHARED(Locks::mutator_lock_) {
   // The hotness we will add to a method when we perform a
   // field/method/class/string lookup.
-  bool increase_hotness_for_ui = Runtime::Current()->InJankPerceptibleProcessState() &&
-       Thread::Current()->IsJitSensitiveThread();
-  method->UpdateCounter(increase_hotness_for_ui ? 0x6ff : 0xf);
+  constexpr uint16_t kNterpHotnessLookup = 0xff;
+  method->UpdateCounter(kNterpHotnessLookup);
 }
 
 template<typename T>
