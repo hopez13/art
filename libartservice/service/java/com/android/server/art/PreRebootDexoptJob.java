a88f04c373264a0e35622d89a0dcc9756ccf07e2
Jiakai Zhang <jiakaiz@google.com>
2024-05-23 13:26:34 +0100

    Skipped already dexopted dex files on resuming.
    Bug: 311377497
    Test: TBD
    Change-Id: Ice8d8d67292697728e95ee88699c08307e0e13fe

--- libartservice/service/java/com/android/server/art/PreRebootDexoptJob.java
+++ libartservice/service/java/com/android/server/art/PreRebootDexoptJob.java
@@ -27,6 +27,8 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.os.Build;
 import android.os.CancellationSignal;
+import android.os.RemoteException;
+import android.os.ServiceSpecificException;
 import android.os.SystemProperties;
 import android.provider.DeviceConfig;
 
@@ -132,7 +134,17 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
      */
     public @ScheduleStatus int onUpdateReady(@Nullable String otaSlot) {
         unschedule();
-        mSerializedExecutor.execute(() -> mInjector.getStatsReporter().reset());
+        mSerializedExecutor.execute(() -> {
+            mInjector.getStatsReporter().reset();
+            if (hasStarted()) {
+                try {
+                    mInjector.getArtd().cleanUpPreRebootStagedFiles();
+                } catch (ServiceSpecificException | RemoteException e) {
+                    AsLog.e("Failed to clean up obsolete Pre-reboot staged files", e);
+                }
+                markHasStarted(false);
+            }
+        });
         updateOtaSlot(otaSlot);
         return schedule();
     }
@@ -204,8 +216,8 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
 
         String otaSlot = mOtaSlot;
         var cancellationSignal = mCancellationSignal = new CancellationSignal();
-        markHasStarted();
         mRunningJob = new CompletableFuture().supplyAsync(() -> {
+            markHasStarted(true);
             try {
                 mInjector.getPreRebootDriver().run(
                         otaSlot, cancellationSignal, mInjector.getStatsReporter());
@@ -269,8 +281,8 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
         return SystemProperties.getBoolean("dalvik.vm.pre-reboot.has-started", false /* def */);
     }
 
-    private void markHasStarted() {
-        ArtJni.setProperty("dalvik.vm.pre-reboot.has-started", "true");
+    private void markHasStarted(boolean value) {
+        ArtJni.setProperty("dalvik.vm.pre-reboot.has-started", String.valueOf(value));
     }
 
     /**
@@ -301,6 +313,11 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
         @NonNull
         public PreRebootStatsReporter getStatsReporter() {
             return mStatsReporter;
+        }
+
+        @NonNull
+        public IArtd getArtd() {
+            return ArtdRefCache.getInstance().getArtd();
         }
     }
 }
