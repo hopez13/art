5fcc15aa505a3996e6323b0d6ec773665864c1e1
Jiakai Zhang <jiakaiz@google.com>
2024-05-24 10:29:09 +0100

    Fixed missing end time on previous runs.
    Before this change, there was a bug with metrics reporting. When a job
    was rerun, the end time of previous runs were unexpectedly cleared.
    
    The root cause is that, when a job was cancelled and rerun,
    `recordJobEnded` was called by a reporter inside chroot, and then
    `recordJobStarted` was called by a reporter outside chroot. The latter
    didn't have the job end time.
    
    After this change, both `recordJobEnded` and `recordJobStarted` are
    called by a reporter outside chroot. This change also brings a bonus:
    the job duration now includes the time spend on teardown.
    
    Bug: 336239721
    Bug: 311377497
    Test: Run Pre-reboot Dexopt twice.
    Change-Id: Iaf8f574087e2ebbc99aef1004d862ae2777e80c7

--- libartservice/service/java/com/android/server/art/PreRebootDexoptJob.java
+++ libartservice/service/java/com/android/server/art/PreRebootDexoptJob.java
@@ -135,7 +135,7 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
     public @ScheduleStatus int onUpdateReady(@Nullable String otaSlot) {
         unschedule();
         mSerializedExecutor.execute(() -> {
-            mInjector.getStatsReporter().reset();
+            mInjector.getStatsReporter().delete();
             if (hasStarted()) {
                 try {
                     mInjector.getArtd().cleanUpPreRebootStagedFiles();
@@ -225,8 +225,7 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
         mRunningJob = new CompletableFuture().supplyAsync(() -> {
             markHasStarted(true);
             try {
-                mInjector.getPreRebootDriver().run(
-                        otaSlot, cancellationSignal, mInjector.getStatsReporter());
+                mInjector.getPreRebootDriver().run(otaSlot, cancellationSignal);
             } catch (RuntimeException e) {
                 AsLog.e("Fatal error", e);
             } finally {
@@ -299,11 +298,9 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
     @VisibleForTesting
     public static class Injector {
         @NonNull private final Context mContext;
-        @NonNull private final PreRebootStatsReporter mStatsReporter;
 
         Injector(@NonNull Context context) {
             mContext = context;
-            mStatsReporter = new PreRebootStatsReporter();
         }
 
         @NonNull
@@ -318,7 +315,7 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
 
         @NonNull
         public PreRebootStatsReporter getStatsReporter() {
-            return mStatsReporter;
+            return new PreRebootStatsReporter();
         }
 
         @NonNull
