21eb32ffcdfb63caaef705349b6ec59619ba0bb8
Jiakai Zhang <jiakaiz@google.com>
2024-05-31 12:26:16 +0100

    Make Pre-reboot Dexopt synchronous for OTA.
    Bug: 311377497
    Test: atest ArtServiceTests:com.android.server.art.PreRebootDexoptJobTest --iterations 10
    Test: -
      1. adb shell pm art on-ota-staged --slot=<slot>
      2. Press Ctrl+C.
    Test: -
      1. adb shell pm art on-ota-staged --slot=<slot>
      2. adb shell killall cmd sh adbd
    Test: -
      1. system/update_engine/scripts/update_device.py out/dist/aosp_cf_x86_64_phone-ota-*.zip
      2. Wait for update_engine to enter the postinstall stage.
      3. adb shell update_engine_client --cancel
    Change-Id: Ie993febbeeaa1f7dada486acd5bf4f3613cae4c3

--- libartservice/service/java/com/android/server/art/PreRebootDexoptJob.java
+++ libartservice/service/java/com/android/server/art/PreRebootDexoptJob.java
@@ -77,6 +77,9 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
     /** The slot that contains the OTA update, "_a" or "_b", or null for a Mainline update. */
     @GuardedBy("this") @Nullable private String mOtaSlot = null;
 
+    /** Whether to map/unmap snapshots. Only applicable to an OTA update. */
+    @GuardedBy("this") private boolean mMapSnapshotsForOta = false;
+
     // Mutations to the global state of Pre-reboot Dexopt, including mounts, staged files, and
     // stats, should only be done when there is no job running and the `this` lock is held, or by
     // the job itself.
@@ -121,7 +124,7 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
     @Override
     public synchronized boolean onStopJob(@NonNull JobParameters params) {
         if (mIsRunningJobKnownByJobScheduler) {
-            cancelGivenLocked(mRunningJob);
+            cancelGivenLocked(mRunningJob, false /* expectInterrupt */);
             mIsRunningJobKnownByJobScheduler = false;
         }
         // "true" means to execute again with the default retry policy.
@@ -138,12 +141,38 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
         cancelAnyLocked();
         resetLocked();
         updateOtaSlotLocked(otaSlot);
+        mMapSnapshotsForOta = true;
         return scheduleLocked();
+    }
+
+    /**
+     * Same as above, but starts the job immediately, instead of going through the job scheduler.
+     *
+     * @return The future of the job, or null if Pre-reboot Dexopt is not enabled.
+     */
+    @Nullable
+    public synchronized CompletableFuture<Void> onUpdateReadyStartNow(@Nullable String otaSlot) {
+        cancelAnyLocked();
+        resetLocked();
+        updateOtaSlotLocked(otaSlot);
+        // Don't map snapshots when running synchronously. `update_engine` maps snapshots for us.
+        mMapSnapshotsForOta = false;
+        if (!isEnabled()) {
+            return null;
+        }
+        mInjector.getStatsReporter().recordJobScheduled(false /* isAsync */);
+        return startLocked();
     }
 
     public synchronized void test() {
         cancelAnyLocked();
         mInjector.getPreRebootDriver().test();
+    }
+
+    /** @see #cancelGivenLocked */
+    public synchronized void cancelGiven(
+            @NonNull CompletableFuture<Void> job, boolean expectInterrupt) {
+        cancelGivenLocked(job, expectInterrupt);
     }
 
     @VisibleForTesting
@@ -202,7 +231,7 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
 
         if (result == JobScheduler.RESULT_SUCCESS) {
             AsLog.i("Pre-reboot Dexopt Job scheduled");
-            mInjector.getStatsReporter().recordJobScheduled();
+            mInjector.getStatsReporter().recordJobScheduled(true /* isAsync */);
             return ArtFlags.SCHEDULE_SUCCESS;
         } else {
             AsLog.i("Failed to schedule Pre-reboot Dexopt Job");
@@ -236,11 +265,12 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
         Utils.check(mRunningJob == null);
 
         String otaSlot = mOtaSlot;
+        boolean mapSnapshotsForOta = mMapSnapshotsForOta;
         var cancellationSignal = mCancellationSignal = new CancellationSignal();
         mRunningJob = new CompletableFuture().runAsync(() -> {
             markHasStarted(true);
             try {
-                mInjector.getPreRebootDriver().run(otaSlot, cancellationSignal);
+                mInjector.getPreRebootDriver().run(otaSlot, mapSnapshotsForOta, cancellationSignal);
             } catch (RuntimeException e) {
                 AsLog.e("Fatal error", e);
             } finally {
@@ -268,9 +298,12 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
      *
      * When this method exits, it's guaranteed that the given job is not running, but another job
      * might be running.
+     *
+     * @param expectInterrupt if true, this method returns immediately when the thread is
+     *         interrupted, with no guarantee on the job state
      */
     @GuardedBy("this")
-    private void cancelGivenLocked(@NonNull CompletableFuture<Void> job) {
+    private void cancelGivenLocked(@NonNull CompletableFuture<Void> job, boolean expectInterrupt) {
         while (mRunningJob == job) {
             if (!mCancellationSignal.isCanceled()) {
                 mCancellationSignal.cancel();
@@ -279,6 +312,9 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
             try {
                 this.wait();
             } catch (InterruptedException e) {
+                if (expectInterrupt) {
+                    return;
+                }
                 AsLog.wtf("Interrupted", e);
             }
         }
@@ -337,6 +373,10 @@ public class PreRebootDexoptJob implements ArtServiceJobInterface {
             return false;
         }
         return true;
+    }
+
+    public boolean isAsyncForOta() {
+        return SystemProperties.getBoolean("dalvik.vm.pr_dexopt_async_for_ota", false /* def */);
     }
 
     @GuardedBy("this")
