ce8327818c3e0e6f5f0c502386c946e17e1505ff
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2024-06-24 14:30:59 +0000

    Revert "Only increase the hotness for UI thread after startup."
    This is an Android Culprit Assistant revert, created as part of a culprit search session (http://go/aca-get/405cc2b3-7bbd-4a51-b750-81faacc37c30). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: Ieb90d568f17ae52f633c7fe57f6e64adf1ede5af
    Signed-off-by: boq-android-culprit-assistant@system.gserviceaccount.com

--- runtime/interpreter/mterp/nterp.cc
+++ runtime/interpreter/mterp/nterp.cc
@@ -118,10 +118,8 @@ void CheckNterpAsmConstants() {
 inline void UpdateHotness(ArtMethod* method) REQUIRES_SHARED(Locks::mutator_lock_) {
   // The hotness we will add to a method when we perform a
   // field/method/class/string lookup.
-  Runtime* runtime = Runtime::Current();
-  bool increase_hotness_for_ui = runtime->GetStartupCompleted() &&
-      runtime->InJankPerceptibleProcessState() &&
-      Thread::Current()->IsJitSensitiveThread();
+  bool increase_hotness_for_ui = Runtime::Current()->InJankPerceptibleProcessState() &&
+       Thread::Current()->IsJitSensitiveThread();
   method->UpdateCounter(increase_hotness_for_ui ? 0x6ff : 0xf);
 }
 
