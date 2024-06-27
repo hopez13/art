7c3d4e59dda2a80d431f8803da62aab93538a2ca
Anton Romanov <anton.romanov@syntacore.com>
2024-06-27 17:46:39 +0500

    riscv64: Extend Shl+Add optimization for many Adds
    Extend instruction_simplifier_riscv64 so it can optimize not only one
    non environment Add use of Shl<1|2|3>.
    
    Replaces every Add use of Shl<1|2|3> with Riscv64ShiftAdd even if there
    are other uses. If Shl has no any uses then remove it.
    
    Even if there is not only an Add among Shl uses such an extension gives
    a gain. sh<1|2|3>add takes one clock cycle therefore 2 independent shifts
    (e.g. slli and sh<1|2|3>add) can go parallel on a dual-issue processor.
    
    By compiling with dex2oat all the methods of applications below I got:
    Facebook: +20% (+9) cases
    TikTok:   +15% (+4) cases of the optimization.
    
    Test: art/test/testrunner/testrunner.py --target --64 --ndebug --optimizing
    
    Change-Id: I2c194ad74f1b8c60f8c926894c389af61333ec28

--- compiler/optimizing/instruction_simplifier_riscv64.cc
+++ compiler/optimizing/instruction_simplifier_riscv64.cc
@@ -41,51 +41,60 @@ class InstructionSimplifierRiscv64Visitor final : public HGraphVisitor {
     }
   }
 
-  bool TryReplaceShiftAddWithOneInstruction(HShl* shl, HAdd* add) {
+  // Replace Add which has Shl with distance of 1 or 2 or 3 with Riscv64ShiftAdd
+  bool TryReplaceAddsWithShiftAdds(HShl* shl) {
     // There is no reason to replace Int32 Shl+Add with ShiftAdd because of
     // additional sign-extension required.
     if (shl->GetType() != DataType::Type::kInt64) {
       return false;
     }
 
-    if (!shl->GetRight()->IsIntConstant()) {
+    if (!shl->GetRight()->IsConstant()) {
       return false;
     }
+
+    // The bytecode does not permit the shift distance to come from a wide variable
+    DCHECK(shl->GetRight()->IsIntConstant());
 
     const int32_t distance = shl->GetRight()->AsIntConstant()->GetValue();
-    if (distance != 1 && distance != 2 && distance != 3) {
+    if ((distance & ~0x3) != 0) {
       return false;
     }
 
-    if (!shl->HasOnlyOneNonEnvironmentUse()) {
-      return false;
+    bool replaced = false;
+
+    for (const HUseListNode<HInstruction*>& use : shl->GetUses()) {
+      HInstruction* user = use.GetUser();
+
+      if (!user->IsAdd()) {
+        continue;
+      }
+      HAdd* add = user->AsAdd();
+      HInstruction* left = add->GetLeft();
+      HInstruction* right = add->GetRight();
+      DCHECK_EQ(add->GetType(), DataType::Type::kInt64)
+          << "Replaceable Add must be the same 64 bit type as the input";
+
+      // If to replace Add which has 2 the same Shl<1|2|3> inputs with Riscv64ShiftAdd
+      // which has this Shl<1|2|3> as an input then any processor will not be able to
+      // execute these shifts parallel which is the purpose of the replace below.
+      if (left == right) {
+        continue;
+      }
+
+      auto* const add_other_input = left == shl ? right : left;
+      auto* const shift_add = new (GetGraph()->GetAllocator())
+          HRiscv64ShiftAdd(shl->GetLeft(), add_other_input, distance);
+
+      add->GetBlock()->ReplaceAndRemoveInstructionWith(add, shift_add);
+      replaced = true;
     }
 
-    auto* const add_other_input = add->GetLeft() == shl ? add->GetRight() : add->GetLeft();
-    auto* const shift_add = new (GetGraph()->GetAllocator())
-        HRiscv64ShiftAdd(shl->GetLeft(), add_other_input, distance);
-
-    DCHECK_EQ(add->GetType(), DataType::Type::kInt64)
-        << "Riscv64ShiftAdd replacement should have the same 64 bit type";
-    add->GetBlock()->ReplaceAndRemoveInstructionWith(add, shift_add);
-    shl->GetBlock()->RemoveInstruction(shl);
-
-    return true;
-  }
-
-  // Replace code looking like
-  //    SHL tmp, a, 1 or 2 or 3
-  //    ADD dst, tmp, b
-  // with
-  //    Riscv64ShiftAdd dst, a, b
-  void VisitAdd(HAdd* add) override {
-    auto* const left = add->GetLeft();
-    auto* const right = add->GetRight();
-    if (left->IsShl() && TryReplaceShiftAddWithOneInstruction(left->AsShl(), add)) {
-      return;
-    } else if (right->IsShl() && TryReplaceShiftAddWithOneInstruction(right->AsShl(), add)) {
-      return;
+    if (!shl->HasUses()) {
+      shl->GetBlock()->RemoveInstruction(shl);
     }
+
+    return replaced;
   }
 
   void VisitAnd(HAnd* inst) override {
@@ -96,6 +105,17 @@ class InstructionSimplifierRiscv64Visitor final : public HGraphVisitor {
 
   void VisitOr(HOr* inst) override {
     if (TryMergeNegatedInput(inst)) {
+      RecordSimplification();
+    }
+  }
+
+  // Replace code looking like
+  //    SHL tmp, a, 1 or 2 or 3
+  //    ADD dst, tmp, b
+  // with
+  //    Riscv64ShiftAdd dst, a, b
+  void VisitShl(HShl* inst) override {
+    if (TryReplaceAddsWithShiftAdds(inst)) {
       RecordSimplification();
     }
   }
